6151c486f92229d54a02a6d077ec44f49f58bbcf
David <david.lund@knowit.se>
2021-12-01 20:33:33 +0100

    Done Day 1 Sonar Sweep

---
+++ AdventCode2021/AdventCode2021/SonarSweeper.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace AdventCode2021
+{
+    public class SonarSweeper
+    {
+        public int SimpleIncreases(List<int> depths)
+        {
+            int increases = 0;
+            for (int ii = 1; ii < depths.Count; ii++)
+            {
+                if (depths[ii - 1] < depths[ii])
+                {
+                    increases++;
+                }
+            }
+            return increases;
+        }
+
+        public List<int> ToSumList(List<int> depths)
+        {
+            List<int> sums = new List<int>();
+
+            int sum = 0;
+            for (int ii = 0; ii < depths.Count - 2; ii++)
+            {
+                sum = depths[ii] + depths[ii + 1] + depths[ii + 2];
+                sums.Add(sum);
+            }
+            return sums;
+        }
+    }
+}
--- AdventCode2021/DailyXunitTests/Day01SonarSweepUnitTests.cs
+++ AdventCode2021/DailyXunitTests/Day01SonarSweepUnitTests.cs
@@ -8,56 +8,65 @@ namespace DailyXunitTests
 {
     public class Day01SonarSweepUnitTests
     {
+        private List<int> testList;
+
+        public Day01SonarSweepUnitTests()
+        {
+            testList = new List<int>()
+            {
+                199,
+                200,
+                208,
+                210,
+                200,
+                207,
+                240,
+                269,
+                260,
+                263
+            };
+        }
+
         [Fact]
         public void Day01_Puzzle1_OK()
         {
+            var sut = new SonarSweeper();
             var depths = Utils.ReadIntsFromFile("Day01.txt");
             int expected = 9105;
-            Assert.Equal(expected, depths[depths.Count-1]);
+            Assert.Equal(expected, depths[depths.Count - 1]);
 
-            int increases = 0;
-            for (int ii = 1; ii < depths.Count; ii++)
-            {
-                if (depths[ii-1] < depths[ii])
-                {
-                    increases++;
-                }
-            }
+            int increases = sut.SimpleIncreases(depths);
 
             Assert.Equal(1692, increases);
+        }
+
+        [Fact]
+        public void CreateSsumList_OK()
+        {
+            var sut = new SonarSweeper();
+            int expected = 263;
+            Assert.Equal(expected, testList[testList.Count - 1]);
+
+            var sums = sut.ToSumList(testList);
+            Assert.Equal(8, sums.Count);
+            Assert.Equal(792, sums[sums.Count - 1]);
+            int increases = sut.SimpleIncreases(sums);
+            Assert.Equal(5, increases);
         }
 
         [Fact]
         public void Day01_Puzzle2_OK()
         {
+            var sut = new SonarSweeper();
             var depths = Utils.ReadIntsFromFile("Day01.txt");
             int expected = 9105;
             Assert.Equal(expected, depths[depths.Count - 1]);
 
-            int increases = 0;
-            for (int ii = 1; ii < depths.Count+2; ii++)
-            {
-                if (Sum(depths, ii - 1) < Sum(depths, ii))
-                {
-                    increases++;
-                }
-            }
+            var sums = sut.ToSumList(depths);
+            int increases = sut.SimpleIncreases(sums);
 
-            Assert.Equal(1692, increases);
+            Assert.Equal(1724, increases);
         }
 
-        public int Sum(List<int> depths, int index)
-        {
-            int sum = 0;
-            for (int ii = 0; ii < 3; ii++)
-            {
-                int iiNow = index - ii;
-                if (iiNow >= 0 && iiNow < depths.Count)
-                {
-                    sum += depths[iiNow];
-                }
-            }
-            return sum;
-        }
     }
 }
