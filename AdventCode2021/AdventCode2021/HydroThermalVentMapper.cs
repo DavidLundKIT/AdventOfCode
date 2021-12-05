f9975051a825cc3205ebb2b342b3526e0e6bb021
David <david.lund@knowit.se>
2021-12-05 12:33:11 +0100

    Day 5 puzzle 1

---
+++ AdventCode2021/AdventCode2021/HydroThermalVentMapper.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventCode2021
+{
+    public class HydroThermalVentMapper
+    {
+        public List<Vent> Vents { get; set; }
+        public Dictionary<string, int> MapHV { get; set; }
+
+        public HydroThermalVentMapper(string[] lines)
+        {
+            Vents = new List<Vent>();
+            foreach (var line in lines)
+            {
+                Vents.Add(new Vent(line));
+            }
+        }
+
+        public int MapVentsHV()
+        {
+            MapHV = new Dictionary<string, int>();
+            foreach (var vent in Vents)
+            {
+                var points = vent.GetPointsHV();
+                foreach (var point in points)
+                {
+                    string key = point.ToString();
+                    if (MapHV.ContainsKey(key))
+                    {
+                        MapHV[key] += 1;
+                    }
+                    else
+                    {
+                        MapHV.Add(key, 1);
+                    }
+                }
+            }
+
+            return MapHV.Values.Where(v => v > 1).Count();
+        }
+    }
+
+    public class Vent
+    {
+        public Point A { get; set; }
+        public Point B { get; set; }
+
+        public Vent(string line)
+        {
+            var vals = line.Split(" -> ", StringSplitOptions.RemoveEmptyEntries);
+            A = new Point(vals[0]);
+            B = new Point(vals[1]);
+        }
+
+        public List<Point> GetPointsHV()
+        {
+            List<Point> list = new List<Point>();
+            if (A.X == B.X)
+            {
+                int ymin = Math.Min(A.Y, B.Y);
+                int ymax = Math.Max(A.Y, B.Y);
+                for (int y = ymin; y <= ymax; y++)
+                {
+                    list.Add(new Point(A.X, y));
+                }
+            }
+            if (A.Y == B.Y)
+            {
+                int xmin = Math.Min(A.X, B.X);
+                int xmax = Math.Max(A.X, B.X);
+                for (int x = xmin; x <= xmax; x++)
+                {
+                    list.Add(new Point(x, A.Y));
+                }
+            }
+            return list;
+        }
+    }
+
+    public class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Point(string line)
+        {
+            var vals = line.Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+            X = int.Parse(vals[0]);
+            Y = int.Parse(vals[1]);
+        }
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public override string ToString()
+        {
+            return $"{X},{Y}";
+        }
+    }
+}
---
+++ AdventCode2021/DailyXunitTests/Day05HydroThermalVentureTests.cs
@@ -0,0 +1,29 @@
+﻿using AdventCode2021;
+using Xunit;
+
+namespace DailyXunitTests
+{
+    public class Day05HydroThermalVentureTests
+    {
+        [Fact]
+        public void Read_Horizontal_and_Vertical_Vents_ok()
+        {
+            var lines = Utils.ReadLinesFromFile("Day05Test.txt");
+            Assert.Equal(10, lines.Length);
+            var sut = new HydroThermalVentMapper(lines);
+            Assert.Equal(10, sut.Vents.Count);
+            Assert.Equal(5, sut.MapVentsHV());
+        }
+
+        [Fact]
+        public void Day05_Puzzle1_Ok()
+        {
+            var lines = Utils.ReadLinesFromFile("Day05.txt");
+            Assert.Equal(500, lines.Length);
+            var sut = new HydroThermalVentMapper(lines);
+            Assert.Equal(500, sut.Vents.Count);
+            Assert.Equal(7468, sut.MapVentsHV());
+        }
+
+    }
+}
