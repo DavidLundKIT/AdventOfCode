4ecba1211021d7917932082da7417fa883e16ccd
David Lund <david.lund@knowit.se>
2021-12-06 20:22:48 +0100

    Completed day 6 Lanternfish!

---
+++ AdventCode2021/AdventCode2021/LanternFishModeller.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventCode2021
+{
+    public class LanternFishModeller
+    {
+        public List<int> Fish { get; set; }
+        public long[] FishPerDay { get; set; }
+
+        public LanternFishModeller(int[] fishdata)
+        {
+            Fish = new List<int>(fishdata);
+            FishPerDay = new long[9];
+            for (int i = 0; i <= 8; i++)
+            {
+                //if (Fish.Any(f => f == i))
+                int num = Fish.Where(f => f == i).Count();
+                FishPerDay[i] = num;
+                //else
+            }
+        }
+
+        public void DoDay()
+        {
+            int countFish = Fish.Count;
+
+            for (int ii = countFish - 1; ii >= 0; ii--)
+            {
+                int fish = Fish[ii];
+                if (fish == 0)
+                {
+                    fish = 6;
+                    Fish.Add(8);
+                }
+                else
+                {
+                    fish--;
+                }
+                Fish[ii] = fish;
+            }
+        }
+        public void DoDay2()
+        {
+            long saveFish = FishPerDay[0];
+
+            for (int ii = 1; ii < 9; ii++)
+            {
+                FishPerDay[ii - 1] = FishPerDay[ii];
+            }
+            FishPerDay[6] += saveFish;
+            FishPerDay[8] = saveFish;
+        }
+
+        public long FishCount()
+        {
+            return FishPerDay.Sum();
+        }
+    }
+}
---
+++ AdventCode2021/DailyXunitTests/Day06LanternFishTests.cs
@@ -0,0 +1,106 @@
+﻿using AdventCode2021;
+using Xunit;
+
+namespace DailyXunitTests
+{
+    public class Day06LanternFishTests
+    {
+        public int[] TestData { get; set; }
+        public int[] Data { get; set; }
+
+        public Day06LanternFishTests()
+        {
+            TestData = new int[] { 3, 4, 3, 1, 2 };
+            Data = new int[] { 1, 3, 4, 1, 5, 2, 1, 1, 1, 1, 5, 1, 5, 1, 1, 1, 1, 3, 1, 1, 1, 1, 1, 1, 1, 2, 1, 5, 1, 1,
+                1, 1, 1, 4, 4, 1, 1, 4, 1, 1, 2, 3, 1, 5, 1, 4, 1, 2, 4, 1, 1, 1, 1, 1, 1, 1, 1, 2, 5, 3, 3, 5, 1, 1, 1,
+                1, 4, 1, 1, 3, 1, 1, 1, 2, 3, 4, 1, 1, 5, 1, 1, 1, 1, 1, 2, 1, 3, 1, 3, 1, 2, 5, 1, 1, 1, 1, 5, 1, 5, 5,
+                1, 1, 1, 1, 3, 4, 4, 4, 1, 5, 1, 1, 4, 4, 1, 1, 1, 1, 3, 1, 1, 1, 1, 1, 1, 3, 2, 1, 4, 1, 1, 4, 1, 5, 5,
+                1, 2, 2, 1, 5, 4, 2, 1, 1, 5, 1, 5, 1, 3, 1, 1, 1, 1, 1, 4, 1, 2, 1, 1, 5, 1, 1, 4, 1, 4, 5, 3, 5, 5, 1,
+                2, 1, 1, 1, 1, 1, 3, 5, 1, 2, 1, 2, 1, 3, 1, 1, 1, 1, 1, 4, 5, 4, 1, 3, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 5,
+                1, 1, 1, 5, 1, 1, 4, 1, 5, 2, 4, 1, 1, 1, 2, 1, 1, 4, 4, 1, 2, 1, 1, 1, 1, 5, 3, 1, 1, 1, 1, 4, 1, 4, 1,
+                1, 1, 1, 1, 1, 3, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 4, 1, 1, 1, 1, 1,
+                1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 5, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+
+        }
+
+        [Fact]
+        public void Test_CheckInput_OK()
+        {
+            int len = TestData.Length;
+            Assert.Equal(5, len);
+
+            var sut = new LanternFishModeller(TestData);
+            for (int day = 0; day < 18; day++)
+            {
+                sut.DoDay();
+            }
+            len = sut.Fish.Count;
+            Assert.Equal(26, len);
+        }
+
+        [Theory]
+        [InlineData(18, 26)]
+        [InlineData(80, 5934)]
+        public void Test_DoDays_old_OK(int days, long expected)
+        {
+            int len = TestData.Length;
+            Assert.Equal(5, len);
+
+            var sut = new LanternFishModeller(TestData);
+            for (int day = 0; day < days; day++)
+            {
+                sut.DoDay();
+            }
+            long actual = (long)sut.Fish.Count;
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Day06_Puzzle1_OK()
+        {
+            int len = Data.Length;
+            Assert.Equal(300, len);
+
+            var sut = new LanternFishModeller(Data);
+            for (int day = 0; day < 80; day++)
+            {
+                sut.DoDay();
+            }
+            len = sut.Fish.Count;
+            Assert.Equal(386755, len);
+        }
+
+        [Theory]
+        [InlineData(18, 26)]
+        [InlineData(80, 5934)]
+        [InlineData(256, 26984457539)]
+        public void Test_DoDays_New_OK(int days, long expected)
+        {
+            int len = TestData.Length;
+            Assert.Equal(5, len);
+
+            var sut = new LanternFishModeller(TestData);
+            for (int day = 0; day < days; day++)
+            {
+                sut.DoDay2();
+            }
+            long actual = sut.FishCount();
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Day06_Puzzle2_OK()
+        {
+            int len = Data.Length;
+            Assert.Equal(300, len);
+
+            var sut = new LanternFishModeller(Data);
+            for (int day = 0; day < 256; day++)
+            {
+                sut.DoDay2();
+            }
+            long actual = sut.FishCount();
+            Assert.Equal(1732731810807, actual);
+        }
+    }
+}
