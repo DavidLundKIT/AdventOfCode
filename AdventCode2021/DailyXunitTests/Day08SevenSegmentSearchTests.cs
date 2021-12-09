7ae93997ea30b18f74476a8352b55447607c64e8
David Lund <david.lund@knowit.se>
2021-12-09 09:20:56 +0100

    Set up for day 8 and 9.

---
+++ AdventCode2021/DailyXunitTests/Day08SevenSegmentSearchTests.cs
@@ -0,0 +1,25 @@
+﻿using AdventCode2021;
+using Xunit;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DailyXunitTests
+{
+    public class Day08SevenSegmentSearchTests
+    {
+        [Fact]
+        public void Test_ReadSegments_Ok()
+        {
+            var lines = Utils.ReadLinesFromFile("Day08Test.txt");
+            Assert.Equal(10, lines.Length);
+        }
+
+        [Fact]
+        public void Day08_Puzzle1_Ok()
+        {
+            var lines = Utils.ReadLinesFromFile("Day08.txt");
+            Assert.Equal(200, lines.Length);
+        }
+    }
+}
---
+++ AdventCode2021/DailyXunitTests/Day09SmokeBasinTests.cs
@@ -0,0 +1,37 @@
+﻿using AdventCode2021;
+using Xunit;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DailyXunitTests
+{
+    public class Day09SmokeBasinTests
+    {
+        public string[] TestLines { get; set; }
+        public Day09SmokeBasinTests()
+        {
+            TestLines = new string[]
+            {
+                "2199943210",
+                "3987894921",
+                "9856789892",
+                "8767896789",
+                "9899965678"
+            };
+        }
+
+        [Fact]
+        public void Test_ReadSegments_Ok()
+        {
+            Assert.Equal(5, TestLines.Length);
+        }
+
+        [Fact]
+        public void Day09_Puzzle1_Ok()
+        {
+            var lines = Utils.ReadLinesFromFile("Day09.txt");
+            Assert.Equal(100, lines.Length);
+        }
+    }
+}
