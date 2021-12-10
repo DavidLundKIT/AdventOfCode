315695133380481b14602499ef6f42b304b3455a
David Lund <david.lund@knowit.se>
2021-12-10 23:25:29 +0100

    Completed day 10 Syntax checker

---
+++ AdventCode2021/AdventCode2021/SyntaxChecker.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventCode2021
+{
+    public class SyntaxChecker
+    {
+        public string[] Lines { get; set; }
+        public Stack<char> ChunkStack { get; set; }
+        public List<long> CompScores { get; set; }
+
+        public SyntaxChecker(string[] lines)
+        {
+            ChunkStack = new Stack<char>();
+            CompScores = new List<long>();
+            Lines = lines;
+        }
+
+        public long CheckSyntaxScore()
+        {
+            long score = 0;
+            foreach (var line in Lines)
+            {
+                score += SyntaxScore(line);
+            }
+            return score;
+        }
+
+        public long GetCompScore()
+        {
+            CompScores.Sort();
+            int idx = CompScores.Count / 2;
+            long midvalue = CompScores.Skip(idx).Take(1).ToList()[0];
+            return midvalue;
+        }
+
+        public long SyntaxScore(string line)
+        {
+            long score = 0;
+            char chpopped;
+            ChunkStack.Clear();
+
+            foreach (var ch in line.ToCharArray())
+            {
+                switch (ch)
+                {
+                    case '(':
+                        ChunkStack.Push(ch);
+                        break;
+                    case ')':
+                        chpopped = ChunkStack.Pop();
+                        if (chpopped != '(')
+                        {
+                            // bad chunk
+                            score += 3;
+                        }
+                        break;
+                    case '[':
+                        ChunkStack.Push(ch);
+                        break;
+                    case ']':
+                        chpopped = ChunkStack.Pop();
+                        if (chpopped != '[')
+                        {
+                            // bad chunk
+                            score += 57;
+                        }
+                        break;
+                    case '{':
+                        ChunkStack.Push(ch);
+                        break;
+                    case '}':
+                        chpopped = ChunkStack.Pop();
+                        if (chpopped != '{')
+                        {
+                            // bad chunk
+                            score += 1197;
+                        }
+                        break;
+                    case '<':
+                        ChunkStack.Push(ch);
+                        break;
+                    case '>':
+                        chpopped = ChunkStack.Pop();
+                        if (chpopped != '<')
+                        {
+                            // bad chunk
+                            score += 25137;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+                if (score > 0)
+                    break;
+            }
+            if (score == 0)
+            {
+                // an incomplete row
+                long compScore = 0;
+                while (ChunkStack.Count > 0)
+                {
+                    char ch = ChunkStack.Pop();
+                    switch (ch)
+                    {
+                        case '(':
+                            compScore = 5 * compScore + 1;
+                            break;
+                        case '[':
+                            compScore = 5 * compScore + 2;
+                            break;
+                        case '{':
+                            compScore = 5 * compScore + 3;
+                            break;
+                        case '<':
+                            compScore = 5 * compScore + 4;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                CompScores.Add(compScore);
+            }
+            return score;
+        }
+    }
+}
---
+++ AdventCode2021/DailyXunitTests/Day10SyntaxScoringTests.cs
@@ -0,0 +1,56 @@
+﻿using AdventCode2021;
+using Xunit;
+
+namespace DailyXunitTests
+{
+    public class Day10SyntaxScoringTests
+    {
+        [Fact]
+        public void Test_readlines_SyntaxScore_ok()
+        {
+            var lines = Utils.ReadLinesFromFile("Day10Test.txt");
+            Assert.Equal(10, lines.Length);
+
+            var sut = new SyntaxChecker(lines);
+            long actual = sut.CheckSyntaxScore();
+
+            Assert.Equal(26397, actual);
+        }
+
+        [Fact]
+        public void Test_readlines_CompletionScore_ok()
+        {
+            var lines = Utils.ReadLinesFromFile("Day10Test.txt");
+            Assert.Equal(10, lines.Length);
+
+            var sut = new SyntaxChecker(lines);
+            long actual = sut.CheckSyntaxScore();
+            actual = sut.GetCompScore();
+            Assert.Equal(288957, actual);
+        }
+
+        [Fact]
+        public void Day10_Puzzle1_Ok()
+        {
+            var lines = Utils.ReadLinesFromFile("Day10.txt");
+            Assert.Equal(106, lines.Length);
+
+            var sut = new SyntaxChecker(lines);
+            long actual = sut.CheckSyntaxScore();
+
+            Assert.Equal(323691, actual);
+        }
+
+        [Fact]
+        public void Day10_Puzzle2_Ok()
+        {
+            var lines = Utils.ReadLinesFromFile("Day10.txt");
+            Assert.Equal(106, lines.Length);
+
+            var sut = new SyntaxChecker(lines);
+            long actual = sut.CheckSyntaxScore();
+            actual = sut.GetCompScore();
+            Assert.Equal(2858785164, actual);
+        }
+    }
+}
