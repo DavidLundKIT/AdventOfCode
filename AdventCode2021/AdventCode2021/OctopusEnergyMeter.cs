ed7f8ae3b725a092b6e3517dab4eb5dd37b8e593
David <david.lund@knowit.se>
2021-12-11 15:27:30 +0100

    Finished day 11 Dumbo Octopi

---
+++ AdventCode2021/AdventCode2021/OctopusEnergyMeter.cs
@@ -0,0 +1,109 @@
+﻿namespace AdventCode2021
+{
+    public class OctopusEnergyMeter
+    {
+        public const int KGrid = 10;
+        public int TotalFlashes { get; set; }
+        public Octopus[,] OctopusGrid { get; set; }
+        public int Synchronized { get; set; }
+
+        public OctopusEnergyMeter(string[] lines)
+        {
+            OctopusGrid = new Octopus[KGrid, KGrid];
+
+            for (int y = 0; y < KGrid; y++)
+            {
+                var chs = lines[y].ToCharArray();
+                for (int x = 0; x < KGrid; x++)
+                {
+                    OctopusGrid[x, y] = new Octopus(x, y, (int)(chs[x] - '0'), OctopusGrid);
+                }
+            }
+            TotalFlashes = 0;
+            Synchronized = int.MaxValue;
+        }
+
+        public void IncreaseEnergy()
+        {
+            for (int i = 0; i < KGrid; i++)
+            {
+                for (int j = 0; j < KGrid; j++)
+                {
+                    OctopusGrid[i, j].Inc();
+                }
+            }
+        }
+
+        public int FindOctopusFlashes()
+        {
+            int flashes = 0;
+            for (int i = 0; i < KGrid; i++)
+            {
+                for (int j = 0; j < KGrid; j++)
+                {
+                    flashes += OctopusGrid[i, j].HasFlashed();
+                }
+            }
+            return flashes;
+        }
+
+        public void DoStep(int step)
+        {
+            IncreaseEnergy();
+            int flashes = FindOctopusFlashes();
+            if (flashes == 100)
+                Synchronized = step;
+            TotalFlashes += flashes;
+        }
+    }
+
+    public class Octopus
+    {
+        public int Energy { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public bool Flashed { get; set; }
+        public Octopus[,] Octopi { get; set; }
+        public Octopus(int x, int y, int energy, Octopus[,] octopi)
+        {
+            X = x;
+            Y = y;
+            Energy = energy;
+            Flashed = false;
+            Octopi = octopi;
+        }
+
+        public void Inc()
+        {
+            Energy++;
+            if (Energy == 10 && !Flashed)
+            {
+                Flashed = true;
+                for (int x = X - 1; x <= X + 1; x++)
+                {
+                    for (int y = Y - 1; y <= Y + 1; y++)
+                    {
+                        Energize(x, y);
+                    }
+                }
+            }
+        }
+
+        public int HasFlashed()
+        {
+            if (!Flashed)
+                return 0;
+            Flashed = false;
+            Energy = 0;
+            return 1;
+        }
+
+        public void Energize(int x, int y)
+        {
+            if (0 <= x && x <= 9 && 0 <= y && y <= 9)
+            {
+                Octopi[x, y].Inc();
+            }
+        }
+    }
+}
---
+++ AdventCode2021/DailyXunitTests/Day11DumboOctopusTests.cs
@@ -0,0 +1,76 @@
+﻿using AdventCode2021;
+using Xunit;
+
+namespace DailyXunitTests
+{
+    public class Day11DumboOctopusTests
+    {
+        [Fact]
+        public void Test_Read_Octupi_OK()
+        {
+            var lines = Utils.ReadLinesFromFile("Day11Test.txt");
+            Assert.Equal(10, lines.Length);
+            var sut = new OctopusEnergyMeter(lines);
+            Assert.Equal(5, sut.OctopusGrid[0, 0].Energy);
+            Assert.Equal(6, sut.OctopusGrid[9, 9].Energy);
+            for (int i = 0; i < 10; i++)
+            {
+                sut.DoStep(i);
+            }
+            Assert.Equal(204, sut.TotalFlashes);
+            for (int i = 0; i < 90; i++)
+            {
+                sut.DoStep(i);
+            }
+            Assert.Equal(1656, sut.TotalFlashes);
+        }
+
+        [Fact]
+        public void Day11_Puzzle1_OK()
+        {
+            var lines = Utils.ReadLinesFromFile("Day11.txt");
+            Assert.Equal(10, lines.Length);
+            var sut = new OctopusEnergyMeter(lines);
+            Assert.Equal(4, sut.OctopusGrid[0, 0].Energy);
+            Assert.Equal(4, sut.OctopusGrid[9, 9].Energy);
+
+            for (int i = 0; i < 100; i++)
+            {
+                sut.DoStep(i);
+            }
+            Assert.Equal(1741, sut.TotalFlashes);
+        }
+
+        [Fact]
+        public void Test_OctopusSynchronization_OK()
+        {
+            var lines = Utils.ReadLinesFromFile("Day11Test.txt");
+            Assert.Equal(10, lines.Length);
+            var sut = new OctopusEnergyMeter(lines);
+
+            int step = 0;
+            do
+            {
+                sut.DoStep(step++);
+            } while (sut.Synchronized == int.MaxValue);
+            Assert.Equal(195, sut.Synchronized + 1);
+        }
+
+        [Fact]
+        public void Day11_Puzzle2_OK()
+        {
+            var lines = Utils.ReadLinesFromFile("Day11.txt");
+            Assert.Equal(10, lines.Length);
+            var sut = new OctopusEnergyMeter(lines);
+            Assert.Equal(4, sut.OctopusGrid[0, 0].Energy);
+            Assert.Equal(4, sut.OctopusGrid[9, 9].Energy);
+
+            int step = 0;
+            do
+            {
+                sut.DoStep(step++);
+            } while (sut.Synchronized == int.MaxValue);
+            Assert.Equal(440, sut.Synchronized + 1);
+        }
+    }
+}
