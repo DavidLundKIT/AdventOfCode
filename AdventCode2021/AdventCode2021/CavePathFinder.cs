a8b4a20087e67faabd62bb0aa70a1be70921749f
David Lund <david.lund@knowit.se>
2021-12-12 13:56:04 +0100

    Nothing working on Day12 so far. The man is lost!

---
+++ AdventCode2021/AdventCode2021/CavePathFinder.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventCode2021
+{
+    public class CavePathFinder
+    {
+        public Dictionary<string, List<string>> MapNodes { get; set; }
+        public Stack<string> Path;
+        public Dictionary<string, int> Paths;
+
+        public CavePathFinder(string[] lines)
+        {
+            MapNodes = new Dictionary<string, List<string>>();
+            Paths = new Dictionary<string, int>();
+            Path = new Stack<string>();
+
+            foreach (var line in lines)
+            {
+                var parts = line.Split('-', StringSplitOptions.RemoveEmptyEntries);
+                AddNodePair(parts[0], parts[1]);
+                AddNodePair(parts[1], parts[0]);
+            }
+        }
+
+        public void AddNodePair(string a, string b)
+        {
+            if (!MapNodes.ContainsKey(a))
+            {
+                // need to set things up
+                var nodes = new List<string>();
+                MapNodes.Add(a, nodes);
+            }
+            MapNodes[a].Add(b);
+        }
+
+        public bool FindPath(string node)
+        {
+            string lcNode = node.ToLower();
+            if (Path.Contains(node))
+            {
+                // only uppercase can be visited multiple times
+                if (String.Equals(node, lcNode, StringComparison.InvariantCulture))
+                {
+                    // in the path and lower case, stop here.
+                    return false;
+                }
+            }
+            
+            // we can go here again
+            Path.Push(node);
+
+            if (node == "end")
+            {
+                // found a path
+                AddPathToPaths();
+                Path.Pop();
+                return false;
+            }
+
+            foreach (var nodeNext in MapNodes[node])
+            {
+                FindPath(nodeNext);
+            }
+            return false;
+        }
+
+        public void AddPathToPaths()
+        {
+            // hit end make the path
+            var nodes = Path.ToArray();
+            var path = string.Join(",", nodes);
+            if (Paths.ContainsKey(path))
+                Paths[path]++;
+            else
+                Paths.Add(path, 1);
+        }
+    }
+}
---
+++ AdventCode2021/DailyXunitTests/Day12PassagePathingTests.cs
@@ -0,0 +1,46 @@
+﻿using AdventCode2021;
+using Xunit;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DailyXunitTests
+{
+    public class Day12PassagePathingTests
+    {
+        [Fact]
+        public void Day12_Test1_OK()
+        {
+            var lines = Utils.ReadLinesFromFile("Day12Test1.txt");
+            Assert.Equal(7, lines.Length);
+            var sut = new CavePathFinder(lines);
+            //var ret = sut.FindPath("start");
+
+            Assert.Equal(10, sut.Paths.Count);
+        }
+
+        [Fact]
+        public void Day12_Test2_OK()
+        {
+            var lines = Utils.ReadLinesFromFile("Day12Test2.txt");
+            Assert.Equal(10, lines.Length);
+            var sut = new CavePathFinder(lines);
+        }
+
+        [Fact]
+        public void Day12_Test3_OK()
+        {
+            var lines = Utils.ReadLinesFromFile("Day12test3.txt");
+            Assert.Equal(18, lines.Length);
+            var sut = new CavePathFinder(lines);
+        }
+
+        [Fact]
+        public void Day12_Puzzle1_OK()
+        {
+            var lines = Utils.ReadLinesFromFile("Day12.txt");
+            Assert.Equal(23, lines.Length);
+            var sut = new CavePathFinder(lines);
+        }
+    }
+}
