583bba475abc63f040d9687327b8bfc992e370f9
David Lund <david.lund@knowit.se>
2021-12-14 09:14:01 +0100

    Set up for Day 14

---
+++ AdventCode2021/AdventCode2021/Polymerizer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventCode2021
+{
+    public class Polymerizer
+    {
+        public string Template { get; set; }
+
+        public Dictionary<string,string> Converter { get; set; }
+
+        public Polymerizer(string[] lines)
+        {
+            Converter = new Dictionary<string, string>();
+            Template = lines[0];
+
+            for (int i = 2; i < lines.Length; i++)
+            {
+                var parts = lines[i].Split(" -> ", StringSplitOptions.RemoveEmptyEntries);
+                Converter.Add(parts[0], parts[1]);
+            }
+        }
+    }
+}
---
+++ AdventCode2021/DailyXunitTests/Day14ExtendedPolymerisationTests.cs
@@ -0,0 +1,31 @@
+﻿using AdventCode2021;
+using Xunit;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DailyXunitTests
+{
+    public class Day14ExtendedPolymerisationTests
+    {
+        [Fact]
+        public void Day14_TestReadPolymers_OK()
+        {
+            var lines = Utils.ReadLinesFromFile("Day14Test.txt");
+
+            Assert.Equal(18, lines.Length);
+            var sut = new Polymerizer(lines);
+            Assert.Equal("NNCB", sut.Template);
+        }
+
+        [Fact]
+        public void Day14_Puzzle1_OK()
+        {
+            var lines = Utils.ReadLinesFromFile("Day14.txt");
+
+            Assert.Equal(102, lines.Length);
+            var sut = new Polymerizer(lines);
+            Assert.Equal("KOKHCCHNKKFHBKVVHNPN", sut.Template);
+        }
+    }
+}
