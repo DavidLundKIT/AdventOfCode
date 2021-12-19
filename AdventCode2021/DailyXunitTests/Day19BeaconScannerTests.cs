9d9f08227ae435fd6b6163513503fea04973ebb1
David <david.lund@knowit.se>
2021-12-19 23:39:17 +0100

    Prep Day 19.

---
+++ AdventCode2021/DailyXunitTests/Day19BeaconScannerTests.cs
@@ -0,0 +1,18 @@
+﻿using AdventCode2021;
+using Xunit;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DailyXunitTests
+{
+    public class Day19BeaconScannerTests
+    {
+        [Fact]
+        public void Day19_Puzzle1_OK()
+        {
+            var lines = Utils.ReadLinesFromFile("Day19.txt");
+            Assert.Equal(777, lines.Length);
+        }
+    }
+}
