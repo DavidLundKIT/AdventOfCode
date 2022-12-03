9ee6870fc24ffec295662e11f61b71629adfb3a7
David Lund <david.lund@knowit.se>
2022-12-03 11:23:25 +0100

    Solution day 3 of Advent of code 2022

---
+++ AdventCode2022/AdventCode2022/Day03RucksackReorg.cs
@@ -0,0 +1,112 @@
+﻿namespace AdventCode2022
+{
+    public class Day03RucksackReorg
+    {
+        [Fact]
+        public void ReadInDataFile_OK()
+        {
+            var things = Utils.ReadLinesFromFile("Day03.txt");
+            int expected = 300;
+            int actual = things.Length;
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("vJrwpWtwJgWrhcsFMMfFFhFp", 'p', 16)]
+        [InlineData("jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL", 'L', 38)]
+        [InlineData("PmmdzqPrVvPwwTWBwg", 'P', 42)]
+        [InlineData("wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn", 'v', 22)]
+        [InlineData("ttgJtRGJQctTZtZT", 't', 20)]
+        [InlineData("CrZsJsPPZsGzwwsLwLmpwMDw", 's', 19)]
+        public void FindItemInRucksackAndPrio(string rucksack, char expectedItem, int expected)
+        {
+            char actualItem = FindItemInRucksack(rucksack);
+            Assert.Equal(expectedItem, actualItem);
+            int actual = CalcPriority(actualItem);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void RucksackReorg_Part1_OK()
+        {
+            var rucksacks = Utils.ReadLinesFromFile("Day03.txt");
+            int expected = 300;
+            int actual = rucksacks.Length;
+            Assert.Equal(expected, actual);
+
+            int sumPrios = 0;
+            foreach (var rucksack in rucksacks)
+            {
+                char item = FindItemInRucksack(rucksack);
+                int prio = CalcPriority(item);
+                sumPrios += prio;
+            }
+            Assert.Equal(7581, sumPrios);
+        }
+
+        [Theory]
+        [InlineData(new string[] { "vJrwpWtwJgWrhcsFMMfFFhFp", "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL", "PmmdzqPrVvPwwTWBwg" },'r', 18)]
+        [InlineData(new string[] { "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn", "ttgJtRGJQctTZtZT", "CrZsJsPPZsGzwwsLwLmpwMDw" }, 'Z', 52)]
+        public void FindCommonItemInRucksackAndPrio(string[] rucksacks, char expectedItem, int expected)
+        {
+            char actualItem = FindCommonItemInRucsacks(rucksacks);
+            Assert.Equal(expectedItem, actualItem);
+            int actual = CalcPriority(actualItem);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void RucksackReorg_Part2_OK()
+        {
+            var rucksacks = Utils.ReadLinesFromFile("Day03.txt");
+            int expected = 300;
+            int actual = rucksacks.Length;
+            Assert.Equal(expected, actual);
+
+            int sumPrios = 0;
+            for (int ii = 0; ii < rucksacks.Length; ii+=3)
+            {
+                var sacks = rucksacks.Skip(ii).Take(3);
+                char item = FindCommonItemInRucsacks(sacks.ToArray());
+                int prio = CalcPriority(item);
+                sumPrios += prio;
+            }
+            Assert.Equal(2525, sumPrios);
+        }
+
+        public char FindCommonItemInRucsacks(string[] rucksacks)
+        {
+            HashSet<char> h1 = new HashSet<char>(rucksacks[0]);
+            HashSet<char> h2 = new HashSet<char>(rucksacks[1]);
+            HashSet<char> h3 = new HashSet<char>(rucksacks[2]);
+            h1.IntersectWith(h2);
+            h1.IntersectWith(h3);
+            return h1.Single();
+        }
+
+        public char FindItemInRucksack(string rucksack)
+        {
+            string side1 = rucksack.Substring(0, rucksack.Length / 2);
+            string side2 = rucksack.Substring(rucksack.Length / 2);
+            Assert.Equal(rucksack.Length, side1.Length + side2.Length);
+            Assert.Equal(side1.Length, side2.Length);
+            foreach (var item in side1)
+            {
+                if (side2.Contains(item, StringComparison.InvariantCulture))
+                {
+                    return item;
+                }
+            }
+            return ' ';
+        }
+
+        public int CalcPriority(char item)
+        {
+            if (char.IsLower(item))
+            {
+                return item - 'a' + 1;
+            }
+            return item - 'A' + 27;
+        }
+    }
+}
