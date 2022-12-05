0861863a68f6dde9d80a28374ec8ce2c89dc8743
David Lund <david.lund@knowit.se>
2022-12-05 18:28:25 +0100

    Answers to Day 5 2022 but no automatic build of supply crate stacks.

--- AdventCode2022/AdventCode2022/Day05SupplyStacksTests.cs
+++ AdventCode2022/AdventCode2022/Day05SupplyStacksTests.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AdventCode2022.Models;
 
 namespace AdventCode2022
 {
@@ -22,12 +18,124 @@ namespace AdventCode2022
         [InlineData("move 3 from 1 to 3", 3, 1, 3)]
         [InlineData("move 2 from 2 to 1", 2, 2, 1)]
         [InlineData("move 1 from 1 to 2", 1, 1, 2)]
-        public void ParseCommands_OK(string cmd, int amount, int from, int to) 
+        public void ParseCommands_OK(string cmd, int amount, int from, int to)
         {
+            var sut = new SupplyCrane(3);
             var parts = cmd.Split(new char[] { ' ' });
             Assert.Equal(amount, int.Parse(parts[1]));
             Assert.Equal(from, int.Parse(parts[3]));
             Assert.Equal(to, int.Parse(parts[5]));
         }
+
+        [Fact]
+        public void ApplyCommands_OK()
+        {
+            var cmds = new string[] {
+                "move 1 from 2 to 1","move 3 from 1 to 3","move 2 from 2 to 1", "move 1 from 1 to 2"
+            };
+
+            var sut = new SupplyCrane(3);
+
+            foreach (var cmd in cmds)
+            {
+                sut.DoCmd(cmd);
+            }
+            Assert.Equal('C', sut.Stacks[0].Peek());
+            Assert.Equal('M', sut.Stacks[1].Peek());
+            Assert.Equal('Z', sut.Stacks[2].Peek());
+            var actual = sut.PeekStackTops();
+            Assert.Equal("CMZ", actual);
+        }
+
+        [Fact]
+        public void ApplyCommandsFromFile_OK()
+        {
+            var cmds = Utils.ReadLinesFromFile("Day05test.txt");
+
+            var sut = new SupplyCrane(3);
+
+            foreach (var cmd in cmds)
+            {
+                if (cmd.StartsWith("move"))
+                    sut.DoCmd(cmd);
+            }
+            Assert.Equal('C', sut.Stacks[0].Peek());
+            Assert.Equal('M', sut.Stacks[1].Peek());
+            Assert.Equal('Z', sut.Stacks[2].Peek());
+            var actual = sut.PeekStackTops();
+            Assert.Equal("CMZ", actual);
+        }
+
+        [Fact]
+        public void SupplyStacks_Part1_OK()
+        {
+            var cmds = Utils.ReadLinesFromFile("Day05.txt");
+
+            var sut = new SupplyCrane(9);
+
+            foreach (var cmd in cmds)
+            {
+                if (cmd.StartsWith("move"))
+                    sut.DoCmd(cmd);
+            }
+            var actual = sut.PeekStackTops();
+            Assert.Equal("MQTPGLLDN", actual);
+        }
+
+        [Fact]
+        public void ApplyCommands_9001_OK()
+        {
+            var cmds = new string[] {
+                "move 1 from 2 to 1","move 3 from 1 to 3","move 2 from 2 to 1", "move 1 from 1 to 2"
+            };
+
+            var sut = new SupplyCrane(3);
+
+            foreach (var cmd in cmds)
+            {
+                sut.DoCmd9001(cmd);
+            }
+            Assert.Equal('M', sut.Stacks[0].Peek());
+            Assert.Equal('C', sut.Stacks[1].Peek());
+            Assert.Equal('D', sut.Stacks[2].Peek());
+            var actual = sut.PeekStackTops();
+            Assert.Equal("MCD", actual);
+        }
+
+        [Fact]
+        public void ApplyCommands_9001_FromFile_OK()
+        {
+            var cmds = Utils.ReadLinesFromFile("Day05test.txt");
+
+            var sut = new SupplyCrane(3);
+
+            foreach (var cmd in cmds)
+            {
+                if (cmd.StartsWith("move"))
+                    sut.DoCmd9001(cmd);
+            }
+            Assert.Equal('M', sut.Stacks[0].Peek());
+            Assert.Equal('C', sut.Stacks[1].Peek());
+            Assert.Equal('D', sut.Stacks[2].Peek());
+            var actual = sut.PeekStackTops();
+            Assert.Equal("MCD", actual);
+        }
+
+        [Fact]
+        public void SupplyStacks_Crane9001_Part2_OK()
+        {
+            var cmds = Utils.ReadLinesFromFile("Day05.txt");
+
+            var sut = new SupplyCrane(9);
+
+            foreach (var cmd in cmds)
+            {
+                if (cmd.StartsWith("move"))
+                    sut.DoCmd9001(cmd);
+            }
+            var actual = sut.PeekStackTops();
+            Assert.Equal("LVZPSTTCZ", actual);
+        }
+
     }
 }
--- AdventCode2022/AdventCode2022/Models/SupplyCrane.cs
+++ AdventCode2022/AdventCode2022/Models/SupplyCrane.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text;
 
 namespace AdventCode2022.Models
 {
@@ -14,17 +9,72 @@ namespace AdventCode2022.Models
         public SupplyCrane(int stackSize)
         {
             Stacks = new List<Stack<char>>(stackSize);
-            if (3 == stackSize)
+            if (9 == stackSize)
             {
                 // test
-                Stacks[0] = new Stack<char>(new [] { 'N', 'Z' });
-                Stacks[1] = new Stack<char>(new[] { 'N', 'Z' });
-                Stacks[2] = new Stack<char>(new[] { 'N', 'Z' });
-            }
-            else 
-            { 
+                Stacks.Add(new Stack<char>(new[] { 'Z', 'J', 'N', 'W', 'P', 'S' }));
+                Stacks.Add(new Stack<char>(new[] { 'G', 'S', 'T' }));
+                Stacks.Add(new Stack<char>(new[] { 'V', 'Q', 'R', 'L', 'H' }));
 
+                Stacks.Add(new Stack<char>(new[] { 'V', 'S', 'T', 'D' }));
+                Stacks.Add(new Stack<char>(new[] { 'Q', 'Z', 'T', 'D', 'B', 'M', 'J' }));
+                Stacks.Add(new Stack<char>(new[] { 'M', 'W', 'T', 'J', 'D', 'C', 'Z', 'L' }));
+
+                Stacks.Add(new Stack<char>(new[] { 'L', 'P', 'M', 'W', 'G', 'T', 'J' }));
+                Stacks.Add(new Stack<char>(new[] { 'N', 'G', 'M', 'T', 'B', 'F', 'Q', 'H' }));
+                Stacks.Add(new Stack<char>(new[] { 'R', 'D', 'G', 'C', 'P', 'B', 'Q', 'W' }));
             }
+            else
+            {
+                Stacks.Add(new Stack<char>(new[] { 'Z', 'N' }));
+                Stacks.Add(new Stack<char>(new[] { 'M', 'C', 'D' }));
+                Stacks.Add(new Stack<char>(new[] { 'P' }));
+            }
+        }
+
+        public void DoCmd(string cmd)
+        {
+            var parts = cmd.Split(new char[] { ' ' });
+            int amount= int.Parse(parts[1]);
+            int from = int.Parse(parts[3]);
+            int to = int.Parse(parts[5]);
+
+            for (int item = 0; item < amount; item++)
+            {
+                var crate = Stacks[from - 1].Pop();
+                Stacks[to-1].Push(crate);
+            }
+        }
+
+        public void DoCmd9001(string cmd)
+        {
+            var parts = cmd.Split(new char[] { ' ' });
+            int amount = int.Parse(parts[1]);
+            int from = int.Parse(parts[3]);
+            int to = int.Parse(parts[5]);
+
+            var tempStack = new Stack<char>();
+            for (int item = 0; item < amount; item++)
+            {
+                var crate = Stacks[from - 1].Pop();
+                tempStack.Push(crate);
+            }
+
+            while (tempStack.Count> 0)
+            {
+                var crate = tempStack.Pop();
+                Stacks[to - 1].Push(crate);
+            }
+        }
+
+        public string PeekStackTops()
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < Stacks.Count; i++)
+            {
+                sb.Append(Stacks[i].Peek());
+            }
+            return sb.ToString();
         }
     }
 }
