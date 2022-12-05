31d423c76b2ff2a21f7b4ff52bf4190d02a9d618
David Lund <david.lund@knowit.se>
2022-12-05 08:57:53 +0100

    Save for later

--- AdventCode2022/AdventCode2022/Day05SupplyStacksTests.cs
+++ AdventCode2022/AdventCode2022/Day05SupplyStacksTests.cs
@@ -17,5 +17,17 @@ namespace AdventCode2022
             Assert.Equal(expected, actual);
         }
 
+        [Theory]
+        [InlineData("move 1 from 2 to 1", 1, 2, 1)]
+        [InlineData("move 3 from 1 to 3", 3, 1, 3)]
+        [InlineData("move 2 from 2 to 1", 2, 2, 1)]
+        [InlineData("move 1 from 1 to 2", 1, 1, 2)]
+        public void ParseCommands_OK(string cmd, int amount, int from, int to) 
+        {
+            var parts = cmd.Split(new char[] { ' ' });
+            Assert.Equal(amount, int.Parse(parts[1]));
+            Assert.Equal(from, int.Parse(parts[3]));
+            Assert.Equal(to, int.Parse(parts[5]));
+        }
     }
 }
---
+++ AdventCode2022/AdventCode2022/Models/SupplyCrane.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventCode2022.Models
+{
+    public class SupplyCrane
+    {
+        public List<Stack<char>> Stacks { get; set; }
+
+        public SupplyCrane(int stackSize)
+        {
+            Stacks = new List<Stack<char>>(stackSize);
+            if (3 == stackSize)
+            {
+                // test
+                Stacks[0] = new Stack<char>(new [] { 'N', 'Z' });
+                Stacks[1] = new Stack<char>(new[] { 'N', 'Z' });
+                Stacks[2] = new Stack<char>(new[] { 'N', 'Z' });
+            }
+            else 
+            { 
+
+            }
+        }
+    }
+}
