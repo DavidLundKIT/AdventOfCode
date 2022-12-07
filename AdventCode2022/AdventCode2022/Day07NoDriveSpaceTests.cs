e6edbb6d3834056a0c91e558a4191ade150c2d06
David Lund <david.lund@knowit.se>
2022-12-07 08:44:56 +0100

    Only boiler plate for day 7 so far

---
+++ AdventCode2022/AdventCode2022/Day07NoDriveSpaceTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventCode2022
+{
+    public class Day07NoDriveSpaceTests
+    {
+        [Fact]
+        public void ReadInDataFile_OK()
+        {
+            var things = Utils.ReadLinesFromFile("Day07.txt");
+            int expected = 1060;
+            int actual = things.Length;
+            Assert.Equal(expected, actual);
+        }
+    }
+}
