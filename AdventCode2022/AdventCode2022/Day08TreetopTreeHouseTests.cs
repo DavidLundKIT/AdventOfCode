acddccf32d88ac0d8bef2410274313da4dc76d69
David Lund <david.lund@knowit.se>
2022-12-10 13:27:33 +0100

    Day 8 2022 done plus boilder plate day 9

--- AdventCode2022/AdventCode2022/Day08TreetopTreeHouseTests.cs
+++ AdventCode2022/AdventCode2022/Day08TreetopTreeHouseTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AdventCode2022.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,11 +12,72 @@ namespace AdventCode2022
         [Fact]
         public void ReadInDataFile_OK()
         {
-            var things = Utils.ReadIntSquareFromFile("Day08.txt");
+            var trees = Utils.ReadIntSquareFromFile("Day08.txt");
             int expected = 99;
-            int actual = things.Count;
+            int actual = trees.Count;
             Assert.Equal(expected, actual);
         }
 
+        [Fact]
+        public void ReadTestDataFile_OK()
+        {
+            var trees = Utils.ReadIntSquareFromFile("Day08test.txt");
+            int expected = 5;
+            int actual = trees.Count;
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void FindVisibleTrees_Test_OK()
+        {
+            var trees = Utils.ReadIntSquareFromFile("Day08test.txt");
+            int expected = 21;
+            var sut = new TreeSpotter(trees);
+            int actual = sut.FindVisibleTrees();
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void FindVisibleTrees_Part1_OK()
+        {
+            var trees = Utils.ReadIntSquareFromFile("Day08.txt");
+            int expected = 1543;
+            var sut = new TreeSpotter(trees);
+            int actual = sut.FindVisibleTrees();
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void BestViewTreeScore_Test_OK()
+        {
+            var trees = Utils.ReadIntSquareFromFile("Day08test.txt");
+            var sut = new TreeSpotter(trees);
+
+            int actual = sut.BestViewScore(2, 1);
+            Assert.Equal(4, actual);
+
+            actual = sut.BestViewScore(2, 3);
+            Assert.Equal(8, actual);
+        }
+
+        [Fact]
+        public void FindBestViewTreeScore_Test_OK()
+        {
+            var trees = Utils.ReadIntSquareFromFile("Day08test.txt");
+            var sut = new TreeSpotter(trees);
+
+            int actual = sut.FindBestViewScore();
+            Assert.Equal(8, actual);
+        }
+
+        [Fact]
+        public void FindBestViewTreeScore_Part2_OK()
+        {
+            var trees = Utils.ReadIntSquareFromFile("Day08.txt");
+            var sut = new TreeSpotter(trees);
+
+            int actual = sut.FindBestViewScore();
+            Assert.Equal(595080, actual);
+        }
     }
 }
---
+++ AdventCode2022/AdventCode2022/Models/TreeSpotter.cs
@@ -0,0 +1,190 @@
+﻿namespace AdventCode2022.Models
+{
+    public class TreeSpotter
+    {
+        public List<List<int>> Trees { get; set; }
+        public Dictionary<string, int> VisibleTrees { get; set; }
+
+        public TreeSpotter(List<List<int>> trees)
+        {
+            Trees = trees;
+            VisibleTrees = new Dictionary<string, int>();
+        }
+
+        public int FindVisibleTrees()
+        {
+            int tallest = 0;
+            VisibleTrees.Clear();
+
+            for (int yIndex = 0; yIndex < Trees.Count; yIndex++)
+            {
+                tallest = VisibleFromLeft(yIndex);
+                tallest = VisibleFromRight(yIndex);
+            }
+            for (int xIndex = 0; xIndex < Trees[0].Count; xIndex++)
+            {
+                tallest = VisibleFromTop(xIndex);
+                tallest = VisibleFromBottom(xIndex);
+            }
+
+            return VisibleTrees.Count;
+        }
+
+        public int FindBestViewScore()
+        {
+            int bestView = 0;
+            VisibleTrees.Clear();
+
+            for (int yIndex = 0; yIndex < Trees.Count; yIndex++)
+            {
+                for (int xIndex = 0; xIndex < Trees[0].Count; xIndex++)
+                {
+                    bestView = BestViewScore(xIndex, yIndex);
+                }
+            }
+
+            return VisibleTrees.Values.Max();
+        }
+
+        public int BestViewScore(int xIndex, int yIndex)
+        {
+            int thisTree = Trees[yIndex][xIndex];
+            string treeKey = MakeTreeKey(xIndex, yIndex);
+            VisibleTrees.Add(treeKey, 1);
+
+            // Left
+            int treesSeen = 0;
+            for (int i = xIndex - 1; i >= 0; i--)
+            {
+                int treeNow = Trees[yIndex][i];
+                if (treeNow < thisTree)
+                {
+                    treesSeen++;
+                }
+                else if (treeNow >= thisTree)
+                {
+                    treesSeen++;
+                    break;
+                }
+            }
+            VisibleTrees[treeKey] *= treesSeen;
+
+            // Right
+            treesSeen = 0;
+            for (int i = xIndex +1; i < Trees[yIndex].Count; i++)
+            {
+                int treeNow = Trees[yIndex][i];
+                if (treeNow < thisTree)
+                {
+                    treesSeen++;
+                }
+                else if (treeNow >= thisTree)
+                {
+                    treesSeen++;
+                    break;
+                }
+            }
+            VisibleTrees[treeKey] *= treesSeen;
+
+            // Top
+            treesSeen = 0;
+            for (int i = yIndex - 1; i >= 0; i--)
+            {
+                int treeNow = Trees[i][xIndex];
+                if (treeNow < thisTree)
+                {
+                    treesSeen++;
+                }
+                else if (treeNow >= thisTree)
+                {
+                    treesSeen++;
+                    break;
+                }
+            }
+            VisibleTrees[treeKey] *= treesSeen;
+
+            // Bottom
+            treesSeen = 0;
+            for (int i = yIndex + 1; i < Trees.Count; i++)
+            {
+                int treeNow = Trees[i][xIndex];
+                if (treeNow < thisTree)
+                {
+                    treesSeen++;
+                }
+                else if (treeNow >= thisTree)
+                {
+                    treesSeen++;
+                    break;
+                }
+            }
+            VisibleTrees[treeKey] *= treesSeen;
+            return VisibleTrees[treeKey];
+        }
+
+        int IsTallestTree(int xIndex, int yIndex, int tallestTree)
+        {
+            if (Trees[yIndex][xIndex] > tallestTree)
+            {
+                // visible
+                string treeKey = MakeTreeKey(xIndex, yIndex);
+                if (!VisibleTrees.ContainsKey(treeKey))
+                {
+                    VisibleTrees.Add(treeKey, 1);
+                }
+                else
+                {
+                    VisibleTrees[treeKey] += 1;
+                }
+                tallestTree = Trees[yIndex][xIndex];
+            }
+
+            return tallestTree;
+        }
+
+        public int VisibleFromBottom(int xIndex)
+        {
+            int tallestTree = -1;
+            for (int yIndex = Trees.Count - 1; yIndex >= 0; yIndex--)
+            {
+                tallestTree = IsTallestTree(xIndex, yIndex, tallestTree);
+            }
+            return tallestTree;
+        }
+
+        public int VisibleFromTop(int xIndex)
+        {
+            int tallestTree = -1;
+            for (int yIndex = 0; yIndex < Trees.Count; yIndex++)
+            {
+                tallestTree = IsTallestTree(xIndex, yIndex, tallestTree);
+            }
+            return tallestTree;
+        }
+
+        public int VisibleFromLeft(int yIndex)
+        {
+            int tallestTree = -1;
+            for (int xIndex = 0; xIndex < Trees[yIndex].Count; xIndex++)
+            {
+                tallestTree = IsTallestTree(xIndex, yIndex, tallestTree);
+            }
+            return tallestTree;
+        }
+
+        public int VisibleFromRight(int yIndex)
+        {
+            int tallestTree = -1;
+            for (int xIndex = Trees[yIndex].Count - 1; xIndex >= 0; xIndex--)
+            {
+                tallestTree = IsTallestTree(xIndex, yIndex, tallestTree);
+            }
+            return tallestTree;
+        }
+
+        public string MakeTreeKey(int xIndex, int yIndex)
+        {
+            return $"({xIndex}, {yIndex})";
+        }
+    }
+}
