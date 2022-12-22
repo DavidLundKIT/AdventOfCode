bc41237c9f8b026ad4a4191806828542b8574353
David Lund <david.lund@knowit.se>
2022-12-22 09:57:01 +0100

    Saving so far Day 20. must work

---
+++ AdventCode2022/AdventCode2022/Day20GrovePositioningSystemTests.cs
@@ -0,0 +1,47 @@
+﻿using AdventCode2022.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventCode2022
+{
+    public class Day20GrovePositioningSystemTests
+    {
+        public List<int> TestData { get; set; } = new List<int> { 1, 2, -3, 3, -2, 0, 4 };
+
+        [Fact]
+        public void ReadInDataFile_OK()
+        {
+            var lines = Utils.ReadIntsFromFile("Day20.txt");
+            int actual = lines.Count;
+            Assert.Equal(5000, actual);
+
+            var gps = new GrovePositioningSystem(lines);
+
+            Assert.Equal(5000, gps.OrderList.Count);
+        }
+
+        [Fact]
+        public void FindGroveCoords_Test_OK()
+        {
+            var gps = new GrovePositioningSystem(TestData);
+
+            Assert.Equal(7, gps.OrderList.Count);
+
+            for (int i = 0; i < 7; i++)
+            {
+                gps.DoMove(i);
+            }
+
+            List<int> expected = new List<int>() { 1, 2, -3, 4, 0, 3, -2 };
+            var actual = gps.OrderList.Select(n => n.Item2).ToList();
+            var results = expected.Zip(actual).Select(p => p.First== p.Second);
+            foreach (var result in results)
+            {
+                Assert.True(result);
+            }
+        }
+    }
+}
---
+++ AdventCode2022/AdventCode2022/Models/GrovePositioningSystem.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace AdventCode2022.Models
+{
+    public class GrovePositioningSystem
+    {
+        public List<int> OriginalOrder { get; set; }
+        public LinkedList<Tuple<int, int>> OrderList { get; set; }
+
+        public GrovePositioningSystem(List<int> originalOrder)
+        {
+            OriginalOrder = originalOrder;
+            OrderList = new LinkedList<Tuple<int, int>>();
+
+            for (int i = 0; i < originalOrder.Count; i++)
+            {
+                OrderList.AddLast(new Tuple<int, int>(i, originalOrder[i]));
+            }
+            // The numbers are not unique
+            //var uniqueNumbers = new Dictionary<long, long>();
+            //foreach (var item in originalOrder)
+            //{
+            //    uniqueNumbers.Add(item, 1);
+            //}
+        }
+
+        public void DoMove(int step)
+        {
+            // step 1 is at index 0 and remember to wrap
+            int index = step % OriginalOrder.Count;
+
+            var tNow = new Tuple<int, int>(index, OriginalOrder[index]);
+            var nodeNow = OrderList.Find(tNow);
+            if (nodeNow.Value.Item2 < 0)
+            {
+                LinkedListNode<Tuple<int, int>> nodeNew = nodeNow.Previous ?? nodeNow.List.Last;
+                for (int i = 0; i > nodeNow.Value.Item2; i--)
+                {
+                    nodeNew = nodeNew.Previous ?? nodeNew.List.Last;
+                }
+                OrderList.Remove(nodeNow);
+                OrderList.AddAfter(nodeNew, nodeNow);
+            }
+            else if (nodeNow.Value.Item2 > 0)
+            {
+                LinkedListNode<Tuple<int, int>> nodeNew = nodeNow.Next ?? nodeNow.List.First;
+                for (int i = 0; i < nodeNow.Value.Item2; i++)
+                {
+                    nodeNew = nodeNew.Next ?? nodeNew.List.First;
+                }
+                OrderList.Remove(nodeNow);
+                OrderList.AddBefore(nodeNew, nodeNow);
+            }
+            else
+            {
+                Debug.WriteLine($"Node value is {nodeNow.Value.Item1}, {nodeNow.Value.Item2}");
+            }
+        }
+    }
+}
