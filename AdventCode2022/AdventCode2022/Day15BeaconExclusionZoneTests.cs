7fc1c807bb0fb71512b10c51bdb71acab616a07c
David Lund <david.lund@knowit.se>
2023-01-06 18:00:38 +0100

    Day 15 2022 part2 finding all out of range pts.

--- AdventCode2022/AdventCode2022/Day15BeaconExclusionZoneTests.cs
+++ AdventCode2022/AdventCode2022/Day15BeaconExclusionZoneTests.cs
@@ -88,5 +88,71 @@ namespace AdventCode2022
 
             Assert.Equal(0, actual);
         }
+
+        [Fact(Skip ="Not much smaller cube")]
+        public void BeaconFindFrequency_FindBounds_Part2_OK()
+        {
+            var lines = Utils.ReadLinesFromFile("Day15.txt");
+            long actual = lines.Length;
+            Assert.Equal(27, actual);
+            var sut = new BeaconSensorCalculator(lines);
+            actual = sut.SensorBeaconDict.Count;
+            Assert.Equal(27, actual);
+
+            long xMin = Math.Min(sut.SensorBeaconDict.Keys.Min(t => t.Item1), sut.SensorBeaconDict.Values.Min(t => t.Item1));
+            long xMax = Math.Max(sut.SensorBeaconDict.Keys.Max(t => t.Item1), sut.SensorBeaconDict.Values.Max(t => t.Item1));
+            long yMin = Math.Min(sut.SensorBeaconDict.Keys.Min(t => t.Item2), sut.SensorBeaconDict.Values.Min(t => t.Item2));
+            long yMax = Math.Max(sut.SensorBeaconDict.Keys.Max(t => t.Item2), sut.SensorBeaconDict.Values.Max(t => t.Item2));
+            //actual = sut.FindFrequency(0, 4000000);
+
+            Assert.Equal(0, actual);
+        }
+
+        //[Fact()]
+        //public void BeaconFindFrequency_FindOpenSpace_Test_OK()
+        //{
+        //    var lines = Utils.ReadLinesFromFile("Day15test.txt");
+        //    long actual = lines.Length;
+        //    Assert.Equal(14, actual);
+        //    var sut = new BeaconSensorCalculator(lines);
+        //    actual = sut.SensorBeaconDict.Count;
+        //    Assert.Equal(14, actual);
+
+        //    sut.FindPointsBetweenSensorsSingle();
+
+        //    Assert.Equal(0, actual);
+        //}
+
+        [Fact()]
+        public void BeaconFindFrequency_FindOutOfRangeByOne_Test_OK()
+        {
+            var lines = Utils.ReadLinesFromFile("Day15test.txt");
+            long actual = lines.Length;
+            Assert.Equal(14, actual);
+            var sut = new BeaconSensorCalculator(lines);
+            actual = sut.SensorBeaconDict.Count;
+            Assert.Equal(14, actual);
+
+            actual = sut.FindPointsBetweenSensors(0, 20);
+
+            Assert.Equal(56000011, actual);
+        }
+
+        [Fact(Skip ="Takes 3.5 - 3.6 minutes")]
+        public void BeaconFindFrequency_FindOutOfRangeByOne_Part2_OK()
+        {
+            var lines = Utils.ReadLinesFromFile("Day15.txt");
+            long actual = lines.Length;
+            Assert.Equal(27, actual);
+            var sut = new BeaconSensorCalculator(lines);
+            actual = sut.SensorBeaconDict.Count;
+            Assert.Equal(27, actual);
+
+            actual = sut.FindPointsBetweenSensors(0, 4000000);
+            // 7.1 minutes with out removing sensed points
+            // 4.0 minutes with removing points after sensing points in second loop
+            // 3.5 minutes with sensing and removing points in the first loop too. Not much gain.
+            Assert.Equal(11583882601918, actual);
+        }
     }
 }
--- AdventCode2022/AdventCode2022/Models/BeaconSensorCalculator.cs
+++ AdventCode2022/AdventCode2022/Models/BeaconSensorCalculator.cs
@@ -1,4 +1,4 @@
-﻿using System.Linq;
+﻿using System.Diagnostics;
 
 namespace AdventCode2022.Models
 {
@@ -67,6 +67,164 @@ namespace AdventCode2022.Models
             return Math.Abs(sensor.Item1 - beacon.Item1) + Math.Abs(sensor.Item2 - beacon.Item2);
         }
 
+        public void FindPointsOutOfRangeByOne(Tuple<long, long> sensor, Tuple<long, long> beacon, long vMin, long vMax)
+        {
+            long md = ManhattenDistance(sensor, beacon) + 1;
+            for (long i = 0; i <= md; ++i)
+            {
+                var t1 = new Tuple<long, long>(sensor.Item1 + i, sensor.Item2 + (md - i));
+                CheckQuadrant(t1, vMin, vMax);
+                var t2 = new Tuple<long, long>(sensor.Item1 - i, sensor.Item2 + (md - i));
+                CheckQuadrant(t2, vMin, vMax);
+                var t3 = new Tuple<long, long>(sensor.Item1 + i, sensor.Item2 - (md - i));
+                CheckQuadrant(t3, vMin, vMax);
+                var t4 = new Tuple<long, long>(sensor.Item1 - i, sensor.Item2 - (md - i));
+                CheckQuadrant(t4, vMin, vMax);
+            }
+        }
+
+        public void CheckQuadrant(Tuple<long, long> tNow, long vMin, long vMax)
+        {
+            if (vMin <= tNow.Item1 && tNow.Item1 <= vMax
+                && vMin <= tNow.Item2 && tNow.Item2 <= vMax)
+            {
+                SensedPoints.TryAdd(tNow, 0);
+            }
+        }
+
+        public void MarkThoseInRange(Tuple<long, long> sensor, Tuple<long, long> beacon)
+        {
+            long md = ManhattenDistance(sensor, beacon);
+            foreach (var sp in SensedPoints.Keys)
+            {
+                if (SensedPoints[sp] == 0)
+                {
+                    // no one has marked this as sensed
+                    long dist = ManhattenDistance(sensor, sp);
+                    if (dist <= md)
+                    {
+                        SensedPoints[sp]++;
+                    }
+                }
+            }
+        }
+
+        public void RemoveSensedPoints()
+        {
+            var keysMarked = SensedPoints.Where(t => t.Value > 0).Select(t => t.Key);
+            foreach (var key in keysMarked)
+            {
+                SensedPoints.Remove(key);
+            }
+        }
+
+        /// <summary>
+        /// Working on the assumption that there is only one free point in the intersection of all sensors.
+        /// within the given square (vMin, vMax).
+        /// So for each sensor find all points just out of sensing range in the given square.
+        /// Then see if those points are seen by any of the sensors.
+        /// </summary>
+        public long FindPointsBetweenSensors(long vMin, long vMax)
+        {
+            SensedPoints.Clear();
+            foreach (var kvp in SensorBeaconDict)
+            {
+                var sensor = kvp.Key;
+                var beacon = kvp.Value;
+                MarkThoseInRange(sensor, beacon);
+                RemoveSensedPoints();
+                FindPointsOutOfRangeByOne(sensor, beacon, vMin, vMax);
+            }
+
+            Debug.WriteLine($"Points to be sensed: {SensedPoints.Count}");
+            foreach (var kvp in SensorBeaconDict)
+            {
+                var sensor = kvp.Key;
+                var beacon = kvp.Value;
+                MarkThoseInRange(sensor, beacon);
+                RemoveSensedPoints();
+            }
+
+            var kvpBeacon = SensedPoints.Where(kvp => kvp.Value == 0).Single();
+            return kvpBeacon.Key.Item1 * 4000000 + kvpBeacon.Key.Item2;
+        }
+
+        /// <summary>
+        /// Working on the assumption that there is only one free point in the intersection of all sensors.
+        /// So check 1-1 for the closest but not quite intersections.
+        /// Then see if those points are seen by any of the sensors.
+        /// </summary>
+        public void FindPointsBetweenSensorsSingle()
+        {
+            SensedPoints.Clear();
+            foreach (var kvpLeft in SensorBeaconDict)
+            {
+                int foundOpen = 0;
+                var sensorLeft = kvpLeft.Key;
+                var beaconLeft = kvpLeft.Value;
+                long distLeft = ManhattenDistance(sensorLeft, beaconLeft);
+
+                foreach (var kvpRight in SensorBeaconDict)
+                {
+                    var sensorRight = kvpRight.Key;
+                    var beaconRight = kvpRight.Value;
+                    if (sensorLeft == sensorRight)
+                    {
+                        // same sensor
+                        continue;
+                    }
+                    long distRight = ManhattenDistance(sensorRight, beaconRight);
+
+                    long distSensors = ManhattenDistance(sensorLeft, sensorRight);
+                    if (distSensors > distLeft + distRight)
+                    {
+                        // sensing ranges do not overlap
+                        long diff = distSensors - (distLeft + distRight);
+                        if (diff <= 2)
+                        {
+                            Debug.WriteLine($"diff is: {diff}");
+                            foundOpen++;
+                            // make the points.
+                            MakeAndAddPoints(sensorLeft, distLeft, sensorRight, distRight);
+                        }
+                    }
+                }
+                Debug.WriteLine($"Found open spot: {foundOpen}");
+            }
+            Debug.WriteLine("Done");
+        }
+
+        /// <summary>
+        /// Need to find the points in between the two sensors.
+        /// Not the center. Duh!
+        /// </summary>
+        /// <param name="sensorLeft"></param>
+        /// <param name="distLeft"></param>
+        /// <param name="sensorRight"></param>
+        /// <param name="distRight"></param>
+        public void MakeAndAddPoints(Tuple<long, long> sensorLeft, long distLeft, Tuple<long, long> sensorRight, long distRight)
+        {
+            // TODO David would this work if I could get the points between the range of the 2 sensors?
+            long xMin = Math.Min(sensorLeft.Item1, sensorRight.Item1);
+            long xMax = Math.Min(sensorLeft.Item1, sensorRight.Item1);
+            long yMin = Math.Min(sensorLeft.Item2, sensorRight.Item2);
+            long yMax = Math.Min(sensorLeft.Item2, sensorRight.Item2);
+            Tuple<long, long> tCenter = new Tuple<long, long>(xMin + Math.Abs(sensorLeft.Item1 - sensorRight.Item1) / 2, yMin + Math.Abs(sensorLeft.Item2 - sensorRight.Item2) / 2);
+            long mdl = ManhattenDistance(tCenter, sensorLeft);
+            long mdr = ManhattenDistance(tCenter, sensorRight);
+            if (mdl > distLeft && mdr > distRight)
+            {
+                SensedPoints.Add(tCenter, 1);
+            }
+        }
+
+        /// <summary>
+        /// Way too long 6.6 E13 tests x Number of sensors.
+        /// Previous version got bogged in prepping a test square.
+        /// </summary>
+        /// <param name="vMin"></param>
+        /// <param name="vMax"></param>
+        /// <returns></returns>
         public long FindFrequency(int vMin, int vMax)
         {
             bool sensed = false;
@@ -84,7 +242,7 @@ namespace AdventCode2022.Models
                         long dist = ManhattenDistance(sensor, tNow);
                         if (dist <= manhattenBeacon)
                         {
-                            sensed= true;
+                            sensed = true;
                             break;
                         }
                     }
@@ -96,55 +254,6 @@ namespace AdventCode2022.Models
                 }
             }
             return 0;
-        }
-
-        public long FindFrequencyFirstTry(long vMin, long vMax)
-        {
-            PrepSearchSquare(vMin, vMax);
-
-            foreach (var kvp in SensorBeaconDict)
-            {
-                var sensor = kvp.Key;
-                var beacon = kvp.Value;
-                long manhattenBeacon = ManhattenDistance(sensor, beacon);
-                long xMin = (sensor.Item1 - manhattenBeacon) < vMin ? vMin : sensor.Item1 - manhattenBeacon;
-                long xMax = (sensor.Item1 + manhattenBeacon) > vMax ? vMax : sensor.Item1 + manhattenBeacon;
-                long yMin = (sensor.Item2 - manhattenBeacon) < vMin ? vMin : sensor.Item2 - manhattenBeacon;
-                long yMax = (sensor.Item2 + manhattenBeacon) > vMax ? vMax : sensor.Item2 + manhattenBeacon;
-                for (long x = xMin; x <= xMax; x++)
-                {
-                    for (long y = yMin; y <= yMax; y++)
-                    {
-                        var tNow = new Tuple<long, long>(x, y);
-                        long dist = ManhattenDistance(sensor, tNow);
-                        if (dist <= manhattenBeacon)
-                        {
-                            SensedPoints[tNow] += 1;
-                        }
-                    }
-                }
-            }
-            var notSensed = SensedPoints.Where(k => k.Value == 0);
-            if (notSensed.Any())
-            {
-                var tFirst = notSensed.SingleOrDefault();
-                long freq = tFirst.Key.Item1 * 4000000 + tFirst.Key.Item2;
-                return freq;
-            }
-            return 0;
-        }
-
-        public void PrepSearchSquare(long vMin, long vMax)
-        {
-            SensedPoints.Clear();
-            for (long x = vMin; x <= vMax; x++)
-            {
-                for (long y = vMin; y <= vMax; y++)
-                {
-                    var tNow = new Tuple<long, long>(x, y);
-                    SensedPoints.Add(tNow, 0);
-                }
-            }
         }
     }
 }
