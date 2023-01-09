f577f031bf34b541b27796009a3999edd27dbf16
David Lund <david.lund@knowit.se>
2023-01-09 23:15:59 +0100

    Start of Day 22, 2022

---
+++ AdventCode2022/AdventCode2022/Day22MonkeyMapTests.cs
@@ -0,0 +1,40 @@
+﻿using AdventCode2022.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventCode2022
+{
+    public class Day22MonkeyMapTests
+    {
+        [Fact]
+        public void ReadInDataFile_OK()
+        {
+            var lines = Utils.ReadLinesFromFile("Day22.txt");
+            long actual = lines.Length;
+            Assert.Equal(202, actual);
+
+            var mmp = new MonkeyMapPassword(lines.ToList());
+
+            actual = mmp.Map.Count();
+            Assert.Equal(0, actual);
+        }
+
+        [Fact]
+        public void ReadInDataFile_Test_OK()
+        {
+            var lines = Utils.ReadLinesFromFile("Day22test.txt");
+            long actual = lines.Length;
+            Assert.Equal(14, actual);
+
+            var mmp = new MonkeyMapPassword(lines.ToList());
+
+            actual = mmp.Map.Count();
+            Assert.Equal(0, actual);
+        }
+
+
+    }
+}
---
+++ AdventCode2022/AdventCode2022/Models/MonkeyMapPassword.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventCode2022.Models
+{
+    public class MonkeyMapPassword
+    {
+        public Dictionary<Tuple<long, long>, char> Map { get; set; }
+        public string Instructions { get; set; }
+
+        public MonkeyMapPassword(List<string> lines)
+        {
+            Instructions = lines[lines.Count - 1];
+            lines.RemoveAt(lines.Count - 1);
+
+            Map = new Dictionary<Tuple<long, long>, char>();
+            for (int y = 0; y < lines.Count; y++)
+            {
+                var line = lines[y];
+                for (int x = 0; x < line.Length; x++)
+                {
+                    if (line[x] != ' ')
+                    {
+                        Map.Add(new Tuple<long, long>(x, y), line[x]);
+                    }
+                }
+            }
+        }
+
+    }
+}
