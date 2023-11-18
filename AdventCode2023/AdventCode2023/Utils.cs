575e9364a791d6f83bb27ea090d8920a6e26288e
David Lund <david.lund@knowit.se>
2023-11-18 19:49:00 +0100

    Prep for day 1 2023

---
+++ AdventCode2023/AdventCode2023/Utils.cs
@@ -0,0 +1,41 @@
+﻿namespace AdventCode2023
+{
+    public class Utils
+    {
+        public static string[] ReadLinesFromFile(string filename)
+        {
+            string baseDir = AppContext.BaseDirectory;
+            string filePath = $"{baseDir}\\Data\\{filename}";
+            string[] orbits = File.ReadAllLines(filePath);
+            return orbits;
+        }
+
+        public static List<int> ReadIntsFromFile(string filename)
+        {
+            List<string> lines = ReadLinesFromFile(filename).ToList();
+            return lines.Select(s => int.Parse(s)).ToList();
+        }
+
+        public static List<long> ReadLongsFromFile(string filename)
+        {
+            List<string> lines = ReadLinesFromFile(filename).ToList();
+            return lines.Select(s => long.Parse(s)).ToList();
+        }
+
+        public static List<List<int>> ReadIntSquareFromFile(string filename)
+        {
+            List<List<int>> lists = new List<List<int>>();
+            List<string> lines = ReadLinesFromFile(filename).ToList();
+            foreach (string line in lines)
+            {
+                var lNow = new List<int>();
+                for (int i = 0; i < line.Length; i++)
+                {
+                    lNow.Add(int.Parse(line.Substring(i, 1)));
+                }
+                lists.Add(lNow);
+            }
+            return lists;
+        }
+    }
+}
