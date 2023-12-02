b0e383603f0d0632245af173e97ebfe35b83d2a6
David Lund <david.lund@knowit.se>
2023-12-02 12:41:29 +0100

    Day 2 part 1

---
+++ AdventCode2023/AdventCode2023/Day02CubeConundrumUnitTests.cs
@@ -0,0 +1,63 @@
+﻿using AdventCode2023.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventCode2023
+{
+    public class Day02CubeConundrumUnitTests
+    {
+        private const int kMinBlueCubes = 14;
+        private const int kMinGreenCubes = 13;
+        private const int kMinRedCubes = 12;
+
+
+        [Fact]
+        public void ReadInDataFile_OK()
+        {
+            var games = Utils.ReadLinesFromFile("Day02.txt");
+            int expected = 100;
+            int actual = games.Length;
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green", 1, true)]
+        [InlineData("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue", 2, true)]
+        [InlineData("Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red", 3, false)]
+        [InlineData("Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red", 4, false)]
+        [InlineData("Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green", 5, true)]
+        public void ParseGames_Test_OK(string line, int gameNr, bool possible)
+        {
+            var game = new CubeConundrumGame(line);
+            Assert.Equal(gameNr, game.GameNr);
+
+            var actual = game.PossibleGame(kMinRedCubes, kMinBlueCubes, kMinGreenCubes);
+            Assert.Equal(actual, possible);
+        }
+
+        [Fact]
+        public void Day02_Part1_OK()
+        {
+            var lines = Utils.ReadLinesFromFile("Day02.txt");
+            int expected = 100;
+            int actual = lines.Length;
+            Assert.Equal(expected, actual);
+
+            List<CubeConundrumGame> games = new List<CubeConundrumGame>();
+            int sum = 0;
+            foreach (var line in lines)
+            {
+                var game = new CubeConundrumGame(line.Trim());
+                games.Add(game);
+                if (game.PossibleGame(kMinRedCubes, kMinBlueCubes, kMinGreenCubes))
+                {
+                    sum += game.GameNr;
+                }
+            }
+            Assert.Equal(1931, sum);
+        }
+    }
+}
---
+++ AdventCode2023/AdventCode2023/Models/CubeConundrumGame.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities.ObjectModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AdventCode2023.Models
+{
+    public class CubeConundrumGame
+    {
+        public int GameNr { get; set; }
+        public List<CubeHand> Hands { get; set; }
+
+        public CubeConundrumGame(string gameLine)
+        {
+            Hands = new List<CubeHand>();
+            var temp = gameLine.Split(':', StringSplitOptions.RemoveEmptyEntries);
+            GameNr = int.Parse(Regex.Match(temp[0], @"\d+").Value);
+
+            var hands = temp[1].Split(';', StringSplitOptions.RemoveEmptyEntries);
+            foreach (var hand in hands)
+            {
+                Hands.Add(new CubeHand(hand));
+            }
+        }
+
+        public bool PossibleGame(int redCubes, int blueCubes, int greenCubes)
+        {
+            foreach (var hand in Hands) 
+            {
+                if (!hand.Possible(redCubes, blueCubes, greenCubes))
+                    return false;
+            }
+            return true;
+        }
+    }
+
+    public class CubeHand
+    {
+        public int Blue { get; set; }
+        public int Green { get; set; }
+        public int Red { get; set; }
+
+        public CubeHand(string hand)
+        {
+            var cubes = hand.Split(',', StringSplitOptions.RemoveEmptyEntries);
+            foreach (var c in cubes)
+            {
+                int count = int.Parse(Regex.Match(c, @"\d+").Value);
+                if (c.Contains("blue"))
+                {
+                    Blue = count;
+                }
+                else if (c.Contains("green"))
+                {
+                    Green = count;
+                }
+                else if (c.Contains("red"))
+                {
+                    Red = count;
+                } else
+                {
+                    throw new Exception("Huh?");
+                }
+            }
+        }
+
+        public bool Possible(int redCubes, int blueCubes, int greenCubes)
+        {
+            return (Red <= redCubes && Blue <= blueCubes && Green <= greenCubes);
+        }
+    }
+}
