10403c96c9c21606d8fc15ee907c467a1e6de283
David Lund <david.lund@knowit.se>
2023-12-03 15:39:56 +0100

    Day 3 part 1 and 2 finally!

---
+++ AdventCode2023/AdventCode2023/Day03GearRatiosUnitTests.cs
@@ -0,0 +1,115 @@
+﻿using AdventCode2023.Models;
+using System.Text.RegularExpressions;
+
+namespace AdventCode2023
+{
+    public class Day03GearRatiosUnitTests
+    {
+        [Fact]
+        public void ReadInDataFile_OK()
+        {
+            var lines = Utils.ReadLinesFromFile("Day03.txt");
+            int expected = 140;
+            int actual = lines.Length;
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ReadInDataFile_Test_OK()
+        {
+            var lines = Utils.ReadLinesFromFile("Day03test.txt");
+            int expected = 11;
+            int actual = lines.Length;
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("467..114..", 2)]
+        [InlineData("...*......", 0)]
+        [InlineData("..35..633.", 2)]
+        [InlineData("......#...", 0)]
+        [InlineData("617*......", 1)]
+        [InlineData(".....+.58.", 1)]
+        [InlineData("..592.....", 1)]
+        [InlineData("......755.", 1)]
+        [InlineData("...$.*....", 0)]
+        [InlineData(".664.598..", 2)]
+        [InlineData("-664.598..", 2)]
+        public void TestRegex_OK(string line, int expected)
+        {
+            var matches = Regex.Matches(line, @"\d+");
+            Assert.NotNull(matches);
+            Assert.Equal(expected, matches.Count);
+            if (matches.Count > 0)
+            {
+                foreach (Match match in matches)
+                {
+                    int val = int.Parse(match.Value);
+                    Assert.True(val > 0);
+                }
+            }
+        }
+
+        [Fact]
+        public void FindPartsIn_Test_OK()
+        {
+            var lines = Utils.ReadLinesFromFile("Day03test.txt");
+            int expected = 11;
+            int actual = lines.Length;
+            Assert.Equal(expected, actual);
+
+            var sut = new SchematicScanner(lines);
+
+            sut.ScanSchematic();
+            Assert.Equal(8, sut.Parts.Count);
+
+            actual = sut.Parts.Sum();
+            Assert.Equal(4361, actual);
+        }
+
+        [Fact]
+        public void Day03_Part1_GearRatios_OK()
+        {
+            var lines = Utils.ReadLinesFromFile("Day03.txt");
+            int expected = 140;
+            int actual = lines.Length;
+            Assert.Equal(expected, actual);
+
+            var sut = new SchematicScanner(lines);
+            sut.ScanSchematic();
+            actual = sut.Parts.Sum();
+            Assert.Equal(540212, actual);
+        }
+
+        [Fact]
+        public void FindGearRatiosIn_Test_OK()
+        {
+            var lines = Utils.ReadLinesFromFile("Day03test.txt");
+            int expected = 11;
+            int actual = lines.Length;
+            Assert.Equal(expected, actual);
+
+            var sut = new SchematicScanner(lines);
+
+            sut.GetGearRatios();
+            Assert.Equal(3, sut.GearRatios.Count);
+
+            actual = sut.GearRatioSum();
+            Assert.Equal(467835, actual);
+        }
+
+        [Fact]
+        public void Day03_Part2_GearRatios_OK()
+        {
+            var lines = Utils.ReadLinesFromFile("Day03.txt");
+            int expected = 140;
+            int actual = lines.Length;
+            Assert.Equal(expected, actual);
+
+            var sut = new SchematicScanner(lines);
+            sut.GetGearRatios();
+            actual = sut.GearRatioSum();
+            Assert.Equal(87605697, actual);
+        }
+    }
+}
---
+++ AdventCode2023/AdventCode2023/Models/SchematicScanner.cs
@@ -0,0 +1,153 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventCode2023.Models
+{
+    public class SchematicScanner
+    {
+        public List<int> Parts { get; set; }
+        public Dictionary<Tuple<int, int>, GearRatio> GearRatios { get; set; }
+        public string[] Schematic { get; set; }
+        public int xMax { get; set; }
+        public int yMax { get; set; }
+
+        public SchematicScanner(string[] schematic)
+        {
+            Parts = new List<int>();
+            GearRatios = new Dictionary<Tuple<int, int>, GearRatio>();
+
+            Schematic = schematic;
+            yMax = schematic.Length;
+            xMax = schematic[0].Length;
+        }
+
+        public void ScanSchematic()
+        {
+            for (int iline = 0; iline < Schematic.Length; iline++)
+            {
+                ScanSchematicLine(Schematic[iline], iline);
+            }
+        }
+
+        public void ScanSchematicLine(string line, int iline)
+        {
+            var matches = Regex.Matches(line, @"\d+");
+            if (matches.Count > 0)
+            {
+                foreach (Match match in matches)
+                {
+                    int part = int.Parse(match.Value);
+                    if (isPart(match.Index, iline, match.Length))
+                    {
+                        Parts.Add(part);
+                    }
+                }
+            }
+        }
+
+        private bool isPart(int index, int iline, int length)
+        {
+            int x0 = (index - 1) < 0 ? 0 : index - 1;
+            int xN = (index + length + 1) < xMax ? index + length + 1 : xMax;
+            int y0 = (iline - 1) < 0 ? 0 : iline - 1;
+            int yN = (iline + 1) < yMax - 1 ? iline + 1 : yMax - 1;
+
+            for (int y = y0; y <= yN; y++)
+            {
+                for (int x = x0; x < xN; x++)
+                {
+                    char ch = Schematic[y][x];
+                    if (ch != '.' && !char.IsDigit(ch))
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public void GetGearRatios()
+        {
+            for (int iline = 0; iline < Schematic.Length; iline++)
+            {
+                FindGearRatio(Schematic[iline], iline);
+            }
+        }
+
+        public void FindGearRatio(string line, int iline)
+        {
+            var matches = Regex.Matches(line, @"\d+");
+            if (matches.Count > 0)
+            {
+                foreach (Match match in matches)
+                {
+                    int gear = int.Parse(match.Value);
+                    var pt = isGearPart(match.Index, iline, match.Length);
+                    if (pt != null)
+                    {
+                        if (GearRatios.ContainsKey(pt))
+                        {
+                            // second match
+                            GearRatios[pt].Gear2 = gear;
+                        }
+                        else
+                        {
+                            GearRatios.Add(pt, new GearRatio(gear));
+                        }
+                    }
+                }
+            }
+        }
+
+        public Tuple<int, int>? isGearPart(int index, int iline, int length)
+        {
+            int x0 = (index - 1) < 0 ? 0 : index - 1;
+            int xN = (index + length + 1) < xMax ? index + length + 1 : xMax;
+            int y0 = (iline - 1) < 0 ? 0 : iline - 1;
+            int yN = (iline + 1) < yMax - 1 ? iline + 1 : yMax - 1;
+
+            for (int y = y0; y <= yN; y++)
+            {
+                for (int x = x0; x < xN; x++)
+                {
+                    char ch = Schematic[y][x];
+                    if (ch == '*')
+                    {
+                        return new Tuple<int, int>(x, y);
+                    }
+                }
+            }
+            return null;
+        }
+
+        public int GearRatioSum()
+        {
+            int sum = 0;
+
+            if (GearRatios.Count > 0)
+            {
+                foreach (GearRatio gr in GearRatios.Values)
+                {
+                    sum += gr.Ratio();
+                }
+            }
+            return sum;
+        }
+    }
+
+    public class GearRatio
+    {
+        public int Gear1 { get; set; }
+        public int Gear2 { get; set; }
+
+        public GearRatio(int gear)
+        {
+            Gear1 = gear;
+            Gear2 = 0;
+        }
+
+        public int Ratio()
+        {
+            return Gear1 * Gear2;
+        }
+    }
+}
