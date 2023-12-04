e640fd11b4737866b32ecc446a63e993b9dfb287
David Lund <david.lund@knowit.se>
2023-12-04 20:36:57 +0100

    Day 4 finally

--- AdventCode2023/AdventCode2023/Day04ScratchCardsUnitTests.cs
+++ AdventCode2023/AdventCode2023/Day04ScratchCardsUnitTests.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AdventCode2023.Models;
 
 namespace AdventCode2023
 {
@@ -24,7 +20,57 @@ namespace AdventCode2023
             int expected = 6;
             int actual = cards.Length;
             Assert.Equal(expected, actual);
+
+            var sut = new ScratchCardEvaluator(cards);
+
+            actual = 0;
+            foreach (var card in sut.Cards)
+            {
+                actual += card.PointValue();
+            }
+            Assert.Equal(13, actual);
+
+            actual = sut.CalculateTotalCards();
+            Assert.Equal(30, actual);
         }
 
+        [Fact]
+        public void Day04_Part1_ScratchCards_OK()
+        {
+            var cards = Utils.ReadLinesFromFile("Day04.txt");
+            int expected = 203;
+            int actual = cards.Length;
+            Assert.Equal(expected, actual);
+
+            var sut = new ScratchCardEvaluator(cards);
+
+            actual = 0;
+            foreach (var card in sut.Cards)
+            {
+                actual += card.PointValue();
+            }
+            Assert.Equal(25174, actual);
+        }
+
+        [Fact]
+        public void Day04_Part2_ScratchCards_OK()
+        {
+            var cards = Utils.ReadLinesFromFile("Day04.txt");
+            int expected = 203;
+            int actual = cards.Length;
+            Assert.Equal(expected, actual);
+
+            var sut = new ScratchCardEvaluator(cards);
+
+            actual = 0;
+            foreach (var card in sut.Cards)
+            {
+                actual += card.PointValue();
+            }
+            Assert.Equal(25174, actual);
+
+            actual = sut.CalculateTotalCards();
+            Assert.Equal(6420979, actual);
+        }
     }
 }
--- AdventCode2023/AdventCode2023/Models/ScratchCardEvaluator.cs
+++ AdventCode2023/AdventCode2023/Models/ScratchCardEvaluator.cs
@@ -1,16 +1,77 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text.RegularExpressions;
 
 namespace AdventCode2023.Models
 {
     public class ScratchCardEvaluator
     {
-        public ScratchCardEvaluator(string line)
+        public string[] Lines { get; set; }
+        public List<ScratchCard> Cards { get; set; }
+        public ScratchCardEvaluator(string[] lines)
         {
-            
+            Lines = lines;
+            Cards = new List<ScratchCard>();
+            foreach (var line in lines)
+            {
+                Cards.Add(new ScratchCard(line));
+            }
+        }
+
+        public int CalculateTotalCards()
+        {
+            int totalCards = 0;
+            for (int iCard = 0; iCard < Cards.Count; iCard++)
+            {
+                var card = Cards[iCard];
+                int wonCards = card.MatchingNumbers();
+                if (wonCards > 0)
+                {
+                    for (int jCard = iCard + 1; jCard <= iCard + wonCards; jCard++)
+                    {
+                        Cards[jCard].Copies += card.Copies;
+                    }
+                }
+                totalCards += card.Copies;
+            }
+            return totalCards;
+        }
+    }
+
+    public class ScratchCard
+    {
+        public int Card { get; set; }
+        public HashSet<int> WinningNumbers { get; set; }
+        public HashSet<int> CardNumbers { get; set; }
+        public int Copies { get; set; }
+
+        public ScratchCard(string line)
+        {
+            var temp = line.Split(new char[] { ':', '|' }, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+            Card = int.Parse(Regex.Match(temp[0], @"\d+").Value);
+            WinningNumbers = ParseNumbers(temp[1]);
+            CardNumbers = ParseNumbers(temp[2]);
+            Copies = 1;
+        }
+
+        public HashSet<int> ParseNumbers(string numpart)
+        {
+            var numbers = new HashSet<int>(numpart.Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).Select(n => int.Parse(n)));
+            return numbers;
+        }
+
+        public int MatchingNumbers()
+        {
+            var hs = WinningNumbers.Intersect(CardNumbers);
+            return hs.Count();
+        }
+        public int PointValue()
+        {
+            var count = MatchingNumbers();
+            int points = 0;
+            if (count > 0)
+            {
+                points = Convert.ToInt32(Math.Pow(2, count - 1));
+            }
+            return points;
         }
     }
 }
