238dea4d4b41bd0c8c6d35619403705ed05c1f8a
David Lund <david.lund@knowit.se>
2023-12-05 21:46:41 +0100

    Day 5 part 1

--- AdventCode2023/AdventCode2023/Day05SeedFertilizerUnitTests.cs
+++ AdventCode2023/AdventCode2023/Day05SeedFertilizerUnitTests.cs
@@ -1,13 +1,98 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AdventCode2023.Models;
 
 namespace AdventCode2023
 {
     public class Day05SeedFertilizerUnitTests
     {
+        [Fact]
+        public void ReadInDataFile_OK()
+        {
+            var lines = Utils.ReadLinesFromFile("Day05.txt");
+            int expected = 254;
+            int actual = lines.Length;
+            Assert.Equal(expected, actual);
+
+            var sut = new SeedFertilizerMapper();
+            sut.ParseData(lines);
+            Assert.Equal(20, sut.Seeds.Count);
+            Assert.Equal(42, sut.HumidityLocation.Count);
+        }
+
+        [Fact]
+        public void ReadInDataFile_Test_OK()
+        {
+            var lines = Utils.ReadLinesFromFile("Day05test.txt");
+            int expected = 33;
+            int actual = lines.Length;
+            Assert.Equal(expected, actual);
+
+            var sut = new SeedFertilizerMapper();
+            sut.ParseData(lines);
+            Assert.Equal(4, sut.Seeds.Count);
+            Assert.Equal(2, sut.SeedSoil.Count);
+            Assert.Equal(3, sut.SoilFertilizer.Count);
+            Assert.Equal(4, sut.FertilizerWater.Count);
+            Assert.Equal(2, sut.WaterLight.Count);
+            Assert.Equal(3, sut.LightTemperature.Count);
+            Assert.Equal(2, sut.TemperatureHumidity.Count);
+            Assert.Equal(2, sut.HumidityLocation.Count);
+
+            long location = sut.FindNearestSeedLocation();
+            Assert.Equal(35, location);
+        }
+
+        [Theory]
+        [InlineData(79, 81)]
+        [InlineData(14, 14)]
+        [InlineData(55, 57)]
+        [InlineData(13, 13)]
+        public void SeedToSoil_Test_OK(long seed, long soil)
+        {
+            var lines = Utils.ReadLinesFromFile("Day05test.txt");
+            int expected = 33;
+            int actual = lines.Length;
+            Assert.Equal(expected, actual);
+
+            var sut = new SeedFertilizerMapper();
+            sut.ParseData(lines);
+            long actualSoil = sut.MapToDestination(seed, sut.SeedSoil);
+            Assert.Equal(soil, actualSoil);
+        }
+
+        [Theory]
+        [InlineData(79, 82)]
+        [InlineData(14, 43)]
+        [InlineData(55, 86)]
+        [InlineData(13, 35)]
+        public void SeedToLocation_Test_OK(long seed, long location)
+        {
+            var lines = Utils.ReadLinesFromFile("Day05test.txt");
+            int expected = 33;
+            int actual = lines.Length;
+            Assert.Equal(expected, actual);
+
+            var sut = new SeedFertilizerMapper();
+            sut.ParseData(lines);
+            long actualSoil = sut.FromSeedToLocation(seed);
+            Assert.Equal(location, actualSoil);
+        }
+
+        [Fact]
+        public void Day05_Part1_SeedFertilizer_OK()
+        {
+            var lines = Utils.ReadLinesFromFile("Day05.txt");
+            int expected = 254;
+            int actual = lines.Length;
+            Assert.Equal(expected, actual);
+
+            var sut = new SeedFertilizerMapper();
+            sut.ParseData(lines);
+            Assert.Equal(20, sut.Seeds.Count);
+            Assert.Equal(42, sut.HumidityLocation.Count);
+
+            long location = sut.FindNearestSeedLocation();
+            Assert.Equal(174137457, location);
+        }
 
     }
 }
---
+++ AdventCode2023/AdventCode2023/Models/SeedFertilizerMapper.cs
@@ -0,0 +1,194 @@
+﻿namespace AdventCode2023.Models
+{
+    public enum InDataSection
+    {
+        Seeds,
+        SeedSoil,
+        SoilFertilizer,
+        FertilizerWater,
+        WaterLight,
+        LightTemperature,
+        TemperatureHumidity,
+        HumidityLocation,
+        Done
+    }
+    public class SeedFertilizerMapper
+    {
+        public List<long> Seeds { get; set; }
+        public SortedList<long, SourceDestionationMapper> SeedSoil { get; set; }
+        public SortedList<long, SourceDestionationMapper> SoilFertilizer { get; set; }
+        public SortedList<long, SourceDestionationMapper> FertilizerWater { get; set; }
+        public SortedList<long, SourceDestionationMapper> WaterLight { get; set; }
+        public SortedList<long, SourceDestionationMapper> LightTemperature { get; set; }
+        public SortedList<long, SourceDestionationMapper> TemperatureHumidity { get; set; }
+        public SortedList<long, SourceDestionationMapper> HumidityLocation { get; set; }
+
+
+        public SeedFertilizerMapper()
+        {
+            Seeds = new List<long>();
+            SeedSoil = new SortedList<long, SourceDestionationMapper>();
+            SoilFertilizer = new SortedList<long, SourceDestionationMapper>();
+            FertilizerWater = new SortedList<long, SourceDestionationMapper>();
+            WaterLight = new SortedList<long, SourceDestionationMapper>();
+            LightTemperature = new SortedList<long, SourceDestionationMapper>();
+            TemperatureHumidity = new SortedList<long, SourceDestionationMapper>();
+            HumidityLocation = new SortedList<long, SourceDestionationMapper>();
+        }
+
+        public void ParseData(string[] lines)
+        {
+            SourceDestionationMapper map;
+            InDataSection mode = InDataSection.Seeds;
+
+            foreach (string line in lines)
+            {
+                if (string.IsNullOrWhiteSpace(line))
+                {
+                    mode = ChangeParseMode(mode);
+                    continue;
+                }
+                switch (mode)
+                {
+                    case InDataSection.Seeds:
+                        var temp = line.Split(':', StringSplitOptions.RemoveEmptyEntries);
+                        Seeds.AddRange(temp[1].Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).Select(n => long.Parse(n)));
+                        break;
+                    case InDataSection.SeedSoil:
+                        if (char.IsDigit(line[0]))
+                        {
+                            map = new SourceDestionationMapper(line);
+                            SeedSoil.Add(map.Source, map);
+                        }
+                        break;
+                    case InDataSection.SoilFertilizer:
+                        if (char.IsDigit(line[0]))
+                        {
+                            map = new SourceDestionationMapper(line);
+                            SoilFertilizer.Add(map.Source, map);
+                        }
+                        break;
+                    case InDataSection.FertilizerWater:
+                        if (char.IsDigit(line[0]))
+                        {
+                            map = new SourceDestionationMapper(line);
+                            FertilizerWater.Add(map.Source, map);
+                        }
+                        break;
+                    case InDataSection.WaterLight:
+                        if (char.IsDigit(line[0]))
+                        {
+                            map = new SourceDestionationMapper(line);
+                            WaterLight.Add(map.Source, map);
+                        }
+                        break;
+                    case InDataSection.LightTemperature:
+                        if (char.IsDigit(line[0]))
+                        {
+                            map = new SourceDestionationMapper(line);
+                            LightTemperature.Add(map.Source, map);
+                        }
+                        break;
+                    case InDataSection.TemperatureHumidity:
+                        if (char.IsDigit(line[0]))
+                        {
+                            map = new SourceDestionationMapper(line);
+                            TemperatureHumidity.Add(map.Source, map);
+                        }
+                        break;
+                    case InDataSection.HumidityLocation:
+                        if (char.IsDigit(line[0]))
+                        {
+                            map = new SourceDestionationMapper(line);
+                            HumidityLocation.Add(map.Source, map);
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        public long MapToDestination(long source, SortedList<long, SourceDestionationMapper> maps)
+        {
+            foreach (var map in maps)
+            {
+
+                if (map.Key <= source && source <= map.Key + map.Value.Range - 1)
+                {
+                    // in the range
+                    return (source - map.Value.Source) + map.Value.Destination;
+                }
+            }
+            // no match so to itself
+            return source;
+        }
+
+        public long FromSeedToLocation(long seed)
+        {
+            long soil = MapToDestination(seed, SeedSoil);
+            long fert = MapToDestination(soil, SoilFertilizer);
+            long water = MapToDestination(fert, FertilizerWater);
+            long light = MapToDestination(water, WaterLight);
+            long temp = MapToDestination(light, LightTemperature);
+            long humid = MapToDestination(temp, TemperatureHumidity);
+            long location = MapToDestination(humid, HumidityLocation);
+            return location;
+        }
+
+        public long FindNearestSeedLocation()
+        {
+            long nearest = long.MaxValue;
+
+            foreach (var seed in Seeds)
+            {
+                long location = FromSeedToLocation(seed);
+                nearest = location < nearest ? location : nearest;
+            }
+            return nearest;
+        }
+
+        public InDataSection ChangeParseMode(InDataSection modeNow)
+        {
+            switch (modeNow)
+            {
+                case InDataSection.Seeds:
+                    return InDataSection.SeedSoil;
+                case InDataSection.SeedSoil:
+                    return InDataSection.SoilFertilizer;
+                case InDataSection.SoilFertilizer:
+                    return InDataSection.FertilizerWater;
+                case InDataSection.FertilizerWater:
+                    return InDataSection.WaterLight;
+                case InDataSection.WaterLight:
+                    return InDataSection.LightTemperature;
+                case InDataSection.LightTemperature:
+                    return InDataSection.TemperatureHumidity;
+                case InDataSection.TemperatureHumidity:
+                    return InDataSection.HumidityLocation;
+                case InDataSection.HumidityLocation:
+                    return InDataSection.Done;
+                default:
+                    throw new ArgumentOutOfRangeException("huh? modeNow");
+            }
+        }
+
+
+    }
+
+    public class SourceDestionationMapper
+    {
+        public long Destination { get; set; }
+        public long Source { get; set; }
+        public long Range { get; set; }
+
+        public SourceDestionationMapper(string line)
+        {
+            var temp = line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+            Destination = long.Parse(temp[0]);
+            Source = long.Parse(temp[1]);
+            Range = long.Parse(temp[2]);
+        }
+    }
+
+}
