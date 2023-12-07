dff5545dc2ae69742f5ca81e4159f4a8c086df6e
David Lund <david.lund@knowit.se>
2023-12-07 08:16:09 +0100

    Template only Day 7

---
+++ AdventCode2023/AdventCode2023/DayO7CamelCardsUnitTests.cs
@@ -0,0 +1,30 @@
+﻿using AdventCode2023.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventCode2023
+{
+    public class DayO7CamelCardsUnitTests
+    {
+        [Fact]
+        public void ReadInDataFile_OK()
+        {
+            var lines = Utils.ReadLinesFromFile("Day07.txt");
+            int expected = 1000;
+            int actual = lines.Length;
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ReadInDataFile_Test_OK()
+        {
+            var lines = Utils.ReadLinesFromFile("Day07test.txt");
+            int expected = 5;
+            int actual = lines.Length;
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ AdventCode2023/AdventCode2023/Models/CamelCardHandEvaluator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventCode2023.Models
+{
+    public class CamelCardHandEvaluator
+    {
+        public List<CamelCardHand> Hands { get; set; }
+
+        public CamelCardHandEvaluator(string[] lines)
+        {
+            Hands = new List<CamelCardHand>();
+            foreach (string line in lines)
+            {
+                Hands.Add(new CamelCardHand(line));
+            }
+        }
+
+        public long TotalWinnings()
+        {
+            long totalWinnings = 0;
+
+            Hands.Sort();
+            for (int rank  = 0; rank < Hands.Count; ++rank)
+            {
+                totalWinnings += (rank + 1) * Hands[rank].Bid;
+            }
+            return totalWinnings;
+        }
+    }
+
+    public class CamelCardHand: IComparable<CamelCardHand>
+    {
+        public string Hand { get; set; }
+        public long Bid { get; set; }
+
+        public CamelCardHand(string line)
+        {
+            var temp = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            Hand = temp[0];
+            Bid = long.Parse(temp[1]);
+        }
+
+        public int CompareTo(CamelCardHand? other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
