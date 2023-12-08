2ec0830872a7277f3a36a3e16156c6854f6f305e
David Lund <david.lund@knowit.se>
2023-12-08 19:07:26 +0100

    Day 7 part 1

--- AdventCode2023/AdventCode2023/DayO7CamelCardsUnitTests.cs
+++ AdventCode2023/AdventCode2023/DayO7CamelCardsUnitTests.cs
@@ -25,6 +25,47 @@ namespace AdventCode2023
             int expected = 5;
             int actual = lines.Length;
             Assert.Equal(expected, actual);
+
+            var sut = new CamelCardHandEvaluator(lines);
+
+            sut.Hands.Sort();
+            Assert.Equal("32T3K", sut.Hands[0].Hand);
+            Assert.Equal("KTJJT", sut.Hands[1].Hand);
+            Assert.Equal("KK677", sut.Hands[2].Hand);
+            Assert.Equal("T55J5", sut.Hands[3].Hand);
+            Assert.Equal("QQQJA", sut.Hands[4].Hand);
+
+            long totalWinnings = sut.TotalWinnings();
+            Assert.Equal(6440, totalWinnings);
+        }
+
+        [Theory]
+        [InlineData("32T3K 1", CamelCardHandType.OnePair)]
+        [InlineData("KK677 1", CamelCardHandType.TwoPair)]
+        [InlineData("QQQJA 1", CamelCardHandType.ThreeOfAKind)]
+        [InlineData("QQQQJ 1", CamelCardHandType.FourOfAKind)]
+        [InlineData("QQQQQ 1", CamelCardHandType.FiveOfAKind)]
+        [InlineData("J55J5 1", CamelCardHandType.FullHouse)]
+        [InlineData("32T38K 1", CamelCardHandType.HighCard)]
+        public void EvaluateHands_OK(string hand, CamelCardHandType expectedHandType)
+        {
+            var sut = new CamelCardHand(hand);
+
+            Assert.Equal(expectedHandType, sut.HandType);
+        }
+
+        [Fact]
+        public void Day07_Part1_CamelCards_OK()
+        {
+            var lines = Utils.ReadLinesFromFile("Day07.txt");
+            int expected = 1000;
+            int actual = lines.Length;
+            Assert.Equal(expected, actual);
+
+            var sut = new CamelCardHandEvaluator(lines);
+
+            long totalWinnings = sut.TotalWinnings();
+            Assert.Equal(252052080, totalWinnings);
         }
     }
 }
--- AdventCode2023/AdventCode2023/Models/CamelCardHandEvaluator.cs
+++ AdventCode2023/AdventCode2023/Models/CamelCardHandEvaluator.cs
@@ -1,11 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AdventCode2023.Models
 {
+    public enum CamelCardHandType
+    {
+        HighCard = 1,
+        OnePair = 2,
+        TwoPair = 3,
+        ThreeOfAKind = 4,
+        FullHouse = 5,
+        FourOfAKind = 6,
+        FiveOfAKind = 7
+    }
+
     public class CamelCardHandEvaluator
     {
         public List<CamelCardHand> Hands { get; set; }
@@ -24,7 +36,7 @@ namespace AdventCode2023.Models
             long totalWinnings = 0;
 
             Hands.Sort();
-            for (int rank  = 0; rank < Hands.Count; ++rank)
+            for (int rank = 0; rank < Hands.Count; ++rank)
             {
                 totalWinnings += (rank + 1) * Hands[rank].Bid;
             }
@@ -32,21 +44,110 @@ namespace AdventCode2023.Models
         }
     }
 
-    public class CamelCardHand: IComparable<CamelCardHand>
+    public class CamelCardHand : IComparable<CamelCardHand>
     {
         public string Hand { get; set; }
         public long Bid { get; set; }
+        public CamelCardHandType HandType { get; set; }
+        public Dictionary<char, int> Cards { get; set; }
 
         public CamelCardHand(string line)
         {
             var temp = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
             Hand = temp[0];
             Bid = long.Parse(temp[1]);
+            Cards = ParseHand();
+            HandType = EvaluateHand();
+        }
+
+        public Dictionary<char, int> ParseHand()
+        {
+            var cards = new Dictionary<char, int>();
+
+            foreach (char ch in Hand)
+            {
+                if (cards.ContainsKey(ch))
+                {
+                    cards[ch] += 1;
+                }
+                else
+                {
+                    cards.Add(ch, 1);
+                }
+            }
+            return cards;
+        }
+
+        public CamelCardHandType EvaluateHand()
+        {
+            int cc2;
+            switch (Cards.Count)
+            {
+                case 1:
+                    return CamelCardHandType.FiveOfAKind;
+                case 2:
+                    cc2 = Cards.Values.First();
+                    if (cc2 == 1 || cc2 == 4)
+                        return CamelCardHandType.FourOfAKind;
+                    else 
+                        //(cc2== 2 || cc2==3)
+                        return CamelCardHandType.FullHouse;
+                    throw new Exception("On card count of 2");
+                case 3:
+                    cc2 = Cards.Values.Where(c => c == 2).ToList().Count;
+                    if (cc2 == 2)
+                        return CamelCardHandType.TwoPair;
+                    return CamelCardHandType.ThreeOfAKind;
+                case 4:
+                    return CamelCardHandType.OnePair;
+                case 5:
+                    return CamelCardHandType.HighCard;
+                default:
+                    break;
+            }
+            throw new Exception("Hand type not matched");
+        }
+
+        /// <summary>
+        ///  A, K, Q, J, T, 9, 8, 7, 6, 5, 4, 3, or 2
+        /// </summary>
+        /// <param name="ch"></param>
+        /// <returns></returns>
+        public int CardValue(char ch)
+        {
+            switch(ch)
+            {
+                case 'A': 
+                    return 14;
+                case 'K':
+                    return 13;
+                case 'Q':
+                    return 12;
+                case 'J':
+                    return 11;
+                case 'T':
+                    return 10;
+                default: 
+                    return (int) ch - (int)'0';
+            }
         }
 
         public int CompareTo(CamelCardHand? other)
         {
-            throw new NotImplementedException();
+            if (other == null) 
+                return 1;
+
+            int cmp = ((int)HandType).CompareTo((int)other.HandType);
+            if (cmp != 0) 
+                return cmp;
+            // high card compare
+            for (int i = 0; i < Hand.Length; ++ i)
+            {
+                cmp = CardValue(Hand[i]).CompareTo(CardValue(other.Hand[i]));
+                if (cmp != 0)
+                    return cmp;
+            }
+            return 0;
         }
     }
 
