80d445ef5433cb263eb2430a44b5a8de01d2e6e8
David Lund <david.lund@knowit.se>
2023-12-10 21:48:00 +0100

    Day 10 part 1

---
+++ AdventCode2023/AdventCode2023/Day10PipeMazeUnitTests.cs
@@ -0,0 +1,48 @@
+﻿using AdventCode2023.Models;
+
+namespace AdventCode2023
+{
+    public class Day10PipeMazeUnitTests
+    {
+        [Fact]
+        public void ReadInDataFile_OK()
+        {
+            var lines = Utils.ReadLinesFromFile("Day10.txt");
+            int expectedLineCount = 140;
+            int actual = lines.Length;
+            Assert.Equal(expectedLineCount, actual);
+        }
+
+        [Theory]
+        [InlineData("Day10test1.txt", 5, 1, 1, 4)]
+        [InlineData("Day10test2.txt", 5, 0, 2, 8)]
+        public void TestMazes_OK(string path, int expectedlines, int expectedX, int expectedY, int steps)
+        {
+            var lines = Utils.ReadLinesFromFile(path);
+            int actual = lines.Length;
+            Assert.Equal(expectedlines, actual);
+
+            var sut = new PipeMazeWalker(lines);
+            Assert.Equal(expectedX, sut.Start.Item1);
+            Assert.Equal(expectedY, sut.Start.Item2);
+
+            actual = sut.WalkPipeMaze();
+            Assert.Equal(actual, steps);
+        }
+
+        [Fact]
+        public void Day10_Part1_PipeMaze_OK()
+        {
+            var lines = Utils.ReadLinesFromFile("Day10.txt");
+            int expectedLineCount = 140;
+            int actual = lines.Length;
+            Assert.Equal(expectedLineCount, actual);
+
+            var sut = new PipeMazeWalker(lines);
+
+            actual = sut.WalkPipeMaze();
+            Assert.Equal(6842, actual);
+        }
+
+    }
+}
---
+++ AdventCode2023/AdventCode2023/Models/PipeMazeWalker.cs
@@ -0,0 +1,143 @@
+﻿namespace AdventCode2023.Models
+{
+    public class PipeMazeWalker
+    {
+        public List<char[]> PipeMaze { get; set; }
+        public Tuple<int, int> Start { get; set; }
+        public Tuple<int, int> Last { get; set; }
+
+        public Dictionary<char, string> LeftChoices { get; set; }
+        public Dictionary<char, string> RightChoices { get; set; }
+        public Dictionary<char, string> UpChoices { get; set; }
+        public Dictionary<char, string> DownChoices { get; set; }
+
+        public PipeMazeWalker(string[] lines)
+        {
+            PipeMaze = new List<char[]>();
+            int idx = 0;
+            int sX = -1;
+            int sY = -1;
+            foreach (var line in lines)
+            {
+                PipeMaze.Add(line.ToCharArray());
+                int x = line.IndexOf('S');
+                if (x >= 0)
+                {
+                    sX = x;
+                    sY = idx;
+                }
+                idx++;
+            }
+            Start = Tuple.Create(sX, sY);
+            Last = Tuple.Create(sX, sY);
+            LeftChoices = new Dictionary<char, string>()
+            {
+                { '-', "-LFS" }, { 'J', "-LFS"}, { '7',"-LFS"}, { 'S', "-LFS"}
+            };
+            RightChoices = new Dictionary<char, string>()
+            {
+                { '-', "-J7S" }, { 'L', "-J7S"}, { 'F',"-J7S"}, { 'S', "-J7S"}
+            };
+            UpChoices = new Dictionary<char, string>()
+            {
+                { '|', "|7FS" }, { 'L', "|7FS"}, { 'J',"|7FS"}, { 'S', "|7FS"}
+            };
+            DownChoices = new Dictionary<char, string>()
+            {
+                { '|', "|LJS" }, { '7', "|LJS"}, { 'F',"|LJS"}, { 'S', "|LJS"}
+            };
+        }
+
+        public int WalkPipeMaze()
+        {
+            var now = Start;
+            int steps = 0;
+            do
+            {
+                now = FindNextStep(now);
+                steps++;
+            } while (!Start.Equals(now));
+            return steps / 2;
+        }
+
+        public Tuple<int, int> FindNextStep(Tuple<int, int> now)
+        {
+            /*
+                | is a vertical pipe connecting north and south.
+                - is a horizontal pipe connecting east and west.
+                L is a 90-degree bend connecting north and east.
+                J is a 90-degree bend connecting north and west.
+                7 is a 90-degree bend connecting south and west.
+                F is a 90-degree bend connecting south and east.
+                . is ground; there is no pipe in this tile.
+                S is the starting position of the animal; there is a pipe on this tile, but your sketch doesn't show what shape the pipe has.
+            */
+
+            // <==
+            char nowCh = PipeMaze[now.Item2][now.Item1];
+            Tuple<int, int> next;
+            if (now.Item1 - 1 >= 0)
+            {
+                next = new Tuple<int, int>(now.Item1 - 1, now.Item2);
+                if (!Last.Equals(next))
+                {
+                    char ch = PipeMaze[next.Item2][next.Item1];
+                    if (LeftChoices.ContainsKey(nowCh) && LeftChoices[nowCh].Contains(ch))
+                    {
+                        // I can move this way
+                        Last = now;
+                        return next;
+                    }
+                }
+            }
+            // up
+            if (now.Item2 - 1 >= 0)
+            {
+                next = new Tuple<int, int>(now.Item1, now.Item2 - 1);
+                if (!Last.Equals(next))
+                {
+                    char ch = PipeMaze[next.Item2][next.Item1];
+                    if (UpChoices.ContainsKey(nowCh) && UpChoices[nowCh].Contains(ch))
+                    {
+                        // I can move this way
+                        Last = now;
+                        return next;
+                    }
+                }
+            }
+            // ==>
+            if (now.Item1 + 1 < PipeMaze[0].Length)
+            {
+                next = new Tuple<int, int>(now.Item1 + 1, now.Item2);
+                if (!Last.Equals(next))
+                {
+                    char ch = PipeMaze[next.Item2][next.Item1];
+                    if (RightChoices.ContainsKey(nowCh) && RightChoices[nowCh].Contains(ch))
+                    {
+                        // I can move this way
+                        Last = now;
+                        return next;
+                    }
+                }
+            }
+
+            // down
+            if (now.Item2 + 1 < PipeMaze.Count)
+            {
+                next = new Tuple<int, int>(now.Item1, now.Item2 + 1);
+                if (!Last.Equals(next))
+                {
+                    char ch = PipeMaze[next.Item2][next.Item1];
+                    if (DownChoices.ContainsKey(nowCh) && DownChoices[nowCh].Contains(ch))
+                    {
+                        // I can move this way
+                        Last = now;
+                        return next;
+                    }
+                }
+            }
+
+            throw new ArgumentOutOfRangeException(nameof(next));
+        }
+    }
+}
