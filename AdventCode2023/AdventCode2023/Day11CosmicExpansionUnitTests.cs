a1012b8c1a2a57cda488969f6c212061726e8a17
David Lund <david.lund@knowit.se>
2023-12-14 23:01:54 +0100

    Day 11 both parts

--- AdventCode2023/AdventCode2023/Day11CosmicExpansionUnitTests.cs
+++ AdventCode2023/AdventCode2023/Day11CosmicExpansionUnitTests.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AdventCode2023.Models;
 
 namespace AdventCode2023
 {
@@ -12,8 +8,8 @@ namespace AdventCode2023
         public void ReadInDataFile_OK()
         {
             var lines = Utils.ReadLinesFromFile("Day11.txt");
-            int expectedLineCount = 140;
-            int actual = lines.Length;
+            long expectedLineCount = 140;
+            long actual = lines.Length;
             Assert.Equal(expectedLineCount, actual);
         }
 
@@ -21,9 +17,85 @@ namespace AdventCode2023
         public void ReadInDataFile_Test_OK()
         {
             var lines = Utils.ReadLinesFromFile("Day11test1.txt");
-            int expectedLineCount = 10;
-            int actual = lines.Length;
+            long expectedLineCount = 10;
+            long actual = lines.Length;
             Assert.Equal(expectedLineCount, actual);
+
+            var cosmos = new CosmicExpansionMapper(lines);
+
+            Assert.Equal(9, cosmos.Stars.Count());
+
+            cosmos.ExpandTheCosmos(1);
+
+            Assert.Equal(9, cosmos.ExpandedStars.Count());
+
+            actual = cosmos.CalcGalaxyLengths();
+
+            Assert.Equal(374, actual);
         }
+
+        [Fact]
+        public void Day11_Part1_CosmicExpansion_OK()
+        {
+            var lines = Utils.ReadLinesFromFile("Day11.txt");
+            long expectedLineCount = 140;
+            long actual = lines.Length;
+            Assert.Equal(expectedLineCount, actual);
+
+            var cosmos = new CosmicExpansionMapper(lines);
+
+            Assert.Equal(461, cosmos.Stars.Count());
+
+            cosmos.ExpandTheCosmos(1);
+
+            Assert.Equal(461, cosmos.ExpandedStars.Count());
+
+            actual = cosmos.CalcGalaxyLengths();
+
+            Assert.Equal(10490062, actual);
+        }
+
+        [Theory]
+        [InlineData(1, 374)]
+        [InlineData(10, 1030)]
+        [InlineData(100, 8410)]
+        public void Factor_10_Test_OK(int factor, long expected)
+        {
+            var lines = Utils.ReadLinesFromFile("Day11test1.txt");
+            long expectedLineCount = 10;
+            long actual = lines.Length;
+            Assert.Equal(expectedLineCount, actual);
+
+            var cosmos = new CosmicExpansionMapper(lines);
+
+            cosmos.ExpandTheCosmos(factor);
+
+            actual = cosmos.CalcGalaxyLengths();
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Day11_Part2_CosmicExpansion_OK()
+        {
+            var lines = Utils.ReadLinesFromFile("Day11.txt");
+            long expectedLineCount = 140;
+            long actual = lines.Length;
+            Assert.Equal(expectedLineCount, actual);
+
+            var cosmos = new CosmicExpansionMapper(lines);
+
+            Assert.Equal(461, cosmos.Stars.Count());
+
+            cosmos.ExpandTheCosmos(1000000);
+
+            Assert.Equal(461, cosmos.ExpandedStars.Count());
+
+            actual = cosmos.CalcGalaxyLengths();
+
+            // too high 382980107092
+            Assert.Equal(382979724122, actual);
+        }
+
     }
 }
--- AdventCode2023/AdventCode2023/Models/CosmicExpansionMapper.cs
+++ AdventCode2023/AdventCode2023/Models/CosmicExpansionMapper.cs
@@ -1,12 +1,82 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AdventCode2023.Models
+﻿namespace AdventCode2023.Models
 {
     public class CosmicExpansionMapper
     {
+        public List<Tuple<long, long>> Stars { get; set; }
+        public List<Tuple<long, long>> ExpandedStars { get; set; }
+
+        public int MaxX { get; set; }
+        public int MaxY { get; set; }
+
+        public CosmicExpansionMapper(string[] lines)
+        {
+            Stars = new List<Tuple<long, long>>();
+            ExpandedStars = new List<Tuple<long, long>>();
+
+            MaxX = lines[0].Length;
+            MaxY = lines.Length;
+
+            for (int y = 0; y < lines.Length; y++)
+            {
+                for (int x = 0; x < lines[y].Length; x++)
+                {
+                    if (lines[y][x] == '#')
+                    {
+                        // star
+                        Stars.Add(new Tuple<long, long>(x, y));
+                    }
+                }
+            }
+        }
+
+        public void ExpandTheCosmos(long factor)
+        {
+            HashSet<long> starXs = new HashSet<long>(Stars.Select(s => s.Item1).Distinct().ToList());
+            HashSet<long> starYs = new HashSet<long>(Stars.Select(s => s.Item2).Distinct().ToList());
+            HashSet<long> emptyXs = new HashSet<long>(Enumerable.Range(0, MaxX).Select(l => (long)l));
+            HashSet<long> emptyYs = new HashSet<long>(Enumerable.Range(0, MaxY).Select(l => (long)l));
+
+            emptyXs.ExceptWith(starXs);
+            emptyYs.ExceptWith(starYs);
+
+            factor = (factor - 1) < 1 ? 1 : factor - 1;
+            foreach (var star in Stars)
+            {
+                long x = star.Item1;
+                long y = star.Item2;
+                foreach (var x2 in emptyXs)
+                {
+                    if (star.Item1 > x2)
+                        x += factor;
+                }
+                foreach (var y2 in emptyYs)
+                {
+                    if (star.Item2 > y2)
+                        y += factor;
+                }
+                ExpandedStars.Add(new Tuple<long, long>(x, y));
+            }
+        }
+
+        public long CalcGalaxyLengths()
+        {
+            Dictionary<Tuple<long, long>, long> dists = new Dictionary<Tuple<long, long>, long>();
+
+            for (int idx = 0; idx < ExpandedStars.Count - 1; idx++)
+            {
+                for (int idx2 = idx + 1; idx2 < ExpandedStars.Count; idx2++)
+                {
+                    long dist = Math.Abs(ExpandedStars[idx].Item1 - ExpandedStars[idx2].Item1) + Math.Abs(ExpandedStars[idx].Item2 - ExpandedStars[idx2].Item2);
+                    var distXY = new Tuple<long, long>(idx, idx2);
+                    if (!dists.ContainsKey(distXY))
+                    {
+                        dists.Add(distXY, dist);
+                    }
+                }
+            }
+
+            long totalLengths = dists.Values.Sum();
+            return totalLengths;
+        }
     }
 }
