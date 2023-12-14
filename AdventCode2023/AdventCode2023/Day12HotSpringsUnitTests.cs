44cf3a66dd982370bc95d56899a9d1582c5717c8
David Lund <david.lund@knowit.se>
2023-12-14 23:19:46 +0100

    Template for Day 12

---
+++ AdventCode2023/AdventCode2023/Day12HotSpringsUnitTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventCode2023
+{
+    public class Day12HotSpringsUnitTests
+    {
+        [Fact]
+        public void ReadInDataFile_OK()
+        {
+            var lines = Utils.ReadLinesFromFile("Day12.txt");
+            long expectedLineCount = 1000;
+            long actual = lines.Length;
+            Assert.Equal(expectedLineCount, actual);
+        }
+
+        [Fact]
+        public void ReadInDataFile_Test_OK()
+        {
+            var lines = Utils.ReadLinesFromFile("Day12test.txt");
+            long expectedLineCount = 6;
+            long actual = lines.Length;
+            Assert.Equal(expectedLineCount, actual);
+        }
+
+    }
+}
