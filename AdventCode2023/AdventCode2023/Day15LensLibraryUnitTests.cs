1b38d47a76b30d41861f76d759e7e429ac8bf219
David Lund <david.lund@knowit.se>
2023-12-15 23:30:05 +0100

    Day 15 part 1 before bed.

---
+++ AdventCode2023/AdventCode2023/Day15LensLibraryUnitTests.cs
@@ -0,0 +1,54 @@
+﻿using AdventCode2023.Models;
+
+namespace AdventCode2023
+{
+    public class Day15LensLibraryUnitTests
+    {
+        [Fact]
+        public void ReadInDataFile_OK()
+        {
+            var lines = Utils.ReadLinesFromFile("Day15.txt");
+            int expected = 1;
+            int actual = lines.Length;
+            Assert.Equal(expected, actual);
+            expected = 22969; // 22970; 
+            Assert.Equal(expected, lines[0].Length);
+        }
+
+        [Fact]
+        public void Hash_Test_OK()
+        {
+            string line = "HASH";
+            var sut = new LensLibraryProcessor(line);
+
+            int actual = sut.HashLine(line);
+            Assert.Equal(52, actual);
+        }
+
+        [Fact]
+        public void Hash_MultilpleLines_OK()
+        {
+            string line = "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7";
+            var sut = new LensLibraryProcessor(line);
+
+            int actual = sut.HashAllLines();
+            Assert.Equal(1320, actual);
+        }
+
+        [Fact]
+        public void Day15_Part1_LensLibrary_OK()
+        {
+            var lines = Utils.ReadLinesFromFile("Day15.txt");
+            int expected = 1;
+            int actual = lines.Length;
+            Assert.Equal(expected, actual);
+            expected = 22969; // 22970; 
+            Assert.Equal(expected, lines[0].Length);
+
+            var sut = new LensLibraryProcessor(lines[0]);
+
+            actual = sut.HashAllLines();
+            Assert.Equal(0, actual);
+        }
+    }
+}
---
+++ AdventCode2023/AdventCode2023/Models/LensLibraryProcessor.cs
@@ -0,0 +1,34 @@
+﻿namespace AdventCode2023.Models
+{
+    public class LensLibraryProcessor
+    {
+        public string[] Lines { get; set; }
+        public LensLibraryProcessor(string line)
+        {
+            Lines = line.Split(',', StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        public int HashAllLines()
+        {
+            int hashTotal = 0;
+
+            foreach (string line in Lines)
+            {
+                hashTotal += HashLine(line);
+            }
+            return hashTotal;
+        }
+
+        public int HashLine(string line)
+        {
+            int hash = 0;
+            foreach (char ch in line)
+            {
+                hash += (int)ch;
+                hash *= 17;
+                hash = hash % 256;
+            }
+            return hash;
+        }
+    }
+}
