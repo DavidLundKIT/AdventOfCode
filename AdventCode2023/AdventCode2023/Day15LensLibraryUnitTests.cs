4ef63fbc306ddb2352a85defaacef17102d6fe13
David Lund <david.lund@knowit.se>
2023-12-16 14:51:21 +0100

    Day 15 part 2 didn't need the long

--- AdventCode2023/AdventCode2023/Day15LensLibraryUnitTests.cs
+++ AdventCode2023/AdventCode2023/Day15LensLibraryUnitTests.cs
@@ -50,5 +50,33 @@ namespace AdventCode2023
             actual = sut.HashAllLines();
             Assert.Equal(498538, actual);
         }
+
+        [Fact]
+        public void ProcessLens_Test_OK()
+        {
+            string line = "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7";
+            var sut = new LensLibraryProcessor(line);
+
+            sut.ProcessLensCmdsToBoxes();
+            long totalFocusPower = sut.CalcFocusingPower();
+            Assert.Equal(145, totalFocusPower);
+        }
+
+        [Fact]
+        public void Day15_Part2_LensLibrary_OK()
+        {
+            var lines = Utils.ReadLinesFromFile("Day15.txt");
+            int expected = 1;
+            int actual = lines.Length;
+            Assert.Equal(expected, actual);
+            expected = 22969; // 22970; 
+            Assert.Equal(expected, lines[0].Length);
+
+            var sut = new LensLibraryProcessor(lines[0]);
+
+            sut.ProcessLensCmdsToBoxes();
+            long totalFocusPower = sut.CalcFocusingPower();
+            Assert.Equal(286278, totalFocusPower);
+        }
     }
 }
--- AdventCode2023/AdventCode2023/Models/LensLibraryProcessor.cs
+++ AdventCode2023/AdventCode2023/Models/LensLibraryProcessor.cs
@@ -3,9 +3,89 @@
     public class LensLibraryProcessor
     {
         public string[] Lines { get; set; }
+        public Dictionary<int, List<Tuple<string, int>>> LensBoxes { get; set; }
+
         public LensLibraryProcessor(string line)
         {
             Lines = line.Split(',', StringSplitOptions.RemoveEmptyEntries);
+            LensBoxes = new Dictionary<int, List<Tuple<string, int>>>();
+        }
+
+        public void ProcessLensCmdsToBoxes()
+        {
+            string lens = string.Empty;
+            int hash = -1;
+            int lensValue = 0;
+            LensBoxes.Clear();
+
+            foreach (string line in Lines)
+            {
+                if (line.Contains("-"))
+                {
+                    lens = line.Substring(0, line.Length - 1);
+                    hash = HashLine(lens);
+                    if (LensBoxes.ContainsKey(hash))
+                    {
+                        var lensTuple = LensBoxes[hash].FirstOrDefault(l => l.Item1.Equals(lens));
+                        if (lensTuple != null)
+                        {
+                            // remove it
+                            LensBoxes[hash].Remove(lensTuple);
+                            // remove LensBox if no Lenses left
+                            if (!LensBoxes[hash].Any())
+                            {
+                                LensBoxes.Remove(hash);
+                            }
+                        }
+                    }
+                    // else no lens box to remove something
+                }
+                else
+                {
+                    // add a lens
+                    var temp = line.Split('=', StringSplitOptions.RemoveEmptyEntries);
+                    lens = temp[0];
+                    lensValue = int.Parse(temp[1]);
+                    hash = HashLine(lens);
+                    if (!LensBoxes.ContainsKey(hash))
+                    {
+                        List<Tuple<string, int>> lensList = new List<Tuple<string, int>>();
+                        lensList.Add(new Tuple<string, int>(lens, lensValue));
+                        LensBoxes.Add(hash, lensList);
+                    }
+                    else
+                    {
+                        var lensTuple = LensBoxes[hash].FirstOrDefault(l => l.Item1.Equals(lens));
+                        if (lensTuple != null)
+                        {
+                            // found it, remove old
+                            int idx = LensBoxes[hash].IndexOf(lensTuple);
+                            LensBoxes[hash].RemoveAt(idx);
+                            // add new one
+                            LensBoxes[hash].Insert(idx, new Tuple<string, int>(lens, lensValue));
+                        }
+                        else
+                        {
+                            LensBoxes[hash].Add(new Tuple<string, int>(lens, lensValue));
+                        }
+                    }
+                }
+            }
+        }
+
+        public long CalcFocusingPower()
+        {
+            long totalFocusingPower = 0;
+
+            foreach (var lensBox in LensBoxes)
+            {
+                for (int i = 0; i < lensBox.Value.Count; i++)
+                {
+                    int focusPower = (lensBox.Key + 1) * (i + 1) * lensBox.Value[i].Item2;
+                    totalFocusingPower += focusPower;
+                }
+            }
+            return totalFocusingPower;
         }
 
         public int HashAllLines()
