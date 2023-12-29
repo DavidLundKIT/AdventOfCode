58d791d24a9f78b0c8ac02fc64f078e33137a0cf
David Lund <david.lund@knowit.se>
2023-12-29 10:51:45 +0100

    Day 14 finally and cleaned up a bit.

--- AdventCode2023/AdventCode2023/Day14ParabolicReflectorDishUnitTests.cs
+++ AdventCode2023/AdventCode2023/Day14ParabolicReflectorDishUnitTests.cs
@@ -22,8 +22,8 @@ namespace AdventCode2023
             Assert.Equal(expected, actual);
 
             var sut = new DishTilterMeter(lines);
-
-            long lactual = sut.TiltNorth();
+            sut.TiltNorth();
+            long lactual = sut.CalculateLoad();
             Assert.Equal(136, lactual);
         }
 
@@ -37,9 +37,57 @@ namespace AdventCode2023
 
             var sut = new DishTilterMeter(lines);
 
-            long lactual = sut.TiltNorth();
+            sut.TiltNorth();
+            long lactual = sut.CalculateLoad();
             Assert.Equal(110677, lactual);
         }
 
+        [Fact]
+        public void Check_Each_Cycle_Test_OK()
+        {
+            var lines = Utils.ReadLinesFromFile("Day14test.txt");
+            int expected = 10;
+            int actual = lines.Length;
+            Assert.Equal(expected, actual);
+
+            var sut = new DishTilterMeter(lines);
+            sut.DoCycle();
+            sut.DoCycle();
+            sut.DoCycle();
+            long lactual = sut.CalculateLoad();
+            Assert.Equal(69, lactual);
+        }
+
+        [Fact]
+        public void Check_For_Repeat_Cycle_Test_OK()
+        {
+            var lines = Utils.ReadLinesFromFile("Day14test.txt");
+            int expected = 10;
+            int actual = lines.Length;
+            Assert.Equal(expected, actual);
+            var sut = new DishTilterMeter(lines);
+            sut.RunCyclesForPattern();
+            long lactual = sut.FindCorrectLoad();
+            Assert.Equal(64, lactual);
+        }
+
+        [Fact]
+        public void Day14_Part2_ParabolicReflectorDish_OK()
+        {
+            var lines = Utils.ReadLinesFromFile("Day14.txt");
+            int expected = 100;
+            int actual = lines.Length;
+            Assert.Equal(expected, actual);
+
+            var sut = new DishTilterMeter(lines);
+            sut.RunCyclesForPattern();
+            /*
+             * It is here that I inspected the loadDict dictionary and saw the repeat pattern after 107 single steps
+             * The pattern had 34 cycle length. I dumped the dictionary to Excel and then looked for the row that 
+             * when (1000000000 - first cycle ) mod 34 = 0.
+             */
+            long lactual = sut.FindCorrectLoad();
+            Assert.Equal(90551, lactual);
+        }
     }
 }
--- AdventCode2023/AdventCode2023/Models/DishTilterMeter.cs
+++ AdventCode2023/AdventCode2023/Models/DishTilterMeter.cs
@@ -4,15 +4,24 @@ namespace AdventCode2023.Models
 {
     public class DishTilterMeter
     {
+        public char[,] DishOneCycle { get; set; }
         public char[,] Dish { get; set; }
         public int MaxX { get; set; }
         public int MaxY { get; set; }
 
+        // The dictionary:
+        // the key is the load calculation.
+        // the tuple is <count, last cycle, first cycle, second cycle>
+        public Dictionary<long, Tuple<int, int, int, int>> LoadDict { get; set; }
+
+
         public DishTilterMeter(string[] lines)
         {
+            LoadDict = new Dictionary<long, Tuple<int, int, int, int>>();
             MaxX = lines[0].Length;
             MaxY = lines.Length;
             Dish = new char[MaxX, MaxY];
+            DishOneCycle = new char[MaxX, MaxY];
             for (int y = 0; y < MaxY; y++)
             {
                 var tch = lines[y].ToCharArray();
@@ -23,9 +32,8 @@ namespace AdventCode2023.Models
             }
         }
 
-        public long TiltNorth()
+        public void TiltNorth()
         {
-            DumpDish();
             for (var k = 0; k < MaxY; k++)
             {
                 for (var x = 0; x < MaxX; x++)
@@ -57,7 +65,196 @@ namespace AdventCode2023.Models
                 }
             }
 
-            DumpDish();
+        }
+
+        public void TiltSouth()
+        {
+            for (var k = MaxY - 1; k >= 0; k--)
+            {
+                for (var x = 0; x < MaxX; x++)
+                {
+                    char ch = Dish[x, k];
+                    if (ch == 'O' || ch == '#')
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        // can something fill this spot?
+                        for (var y = k - 1; y >= 0; y--)
+                        {
+                            char tch = Dish[x, y];
+                            if (tch == 'O')
+                            {
+                                Dish[x, k] = 'O';
+                                Dish[x, y] = '.';
+                                break;
+                            }
+                            if (tch == '#')
+                            {
+                                // unmoveable rock
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public void TiltWest()
+        {
+            for (var k = 0; k < MaxX; k++)
+            {
+                for (var y = 0; y < MaxY; y++)
+                {
+                    char ch = Dish[k, y];
+                    if (ch == 'O' || ch == '#')
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        // can something fill this spot?
+                        for (var x = k + 1; x < MaxX; x++)
+                        {
+                            char tch = Dish[x, y];
+                            if (tch == 'O')
+                            {
+                                Dish[k, y] = 'O';
+                                Dish[x, y] = '.';
+                                break;
+                            }
+                            if (tch == '#')
+                            {
+                                // unmoveable rock
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public void TiltEast()
+        {
+            for (var k = MaxX - 1; k >= 0; k--)
+            {
+                for (var y = 0; y < MaxY; y++)
+                {
+                    char ch = Dish[k, y];
+                    if (ch == 'O' || ch == '#')
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        // can something fill this spot?
+                        for (var x = k - 1; x >= 0; x--)
+                        {
+                            char tch = Dish[x, y];
+                            if (tch == 'O')
+                            {
+                                Dish[k, y] = 'O';
+                                Dish[x, y] = '.';
+                                break;
+                            }
+                            if (tch == '#')
+                            {
+                                // unmoveable rock
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public void DoCycle()
+        {
+            TiltNorth();
+            TiltWest();
+            TiltSouth();
+            TiltEast();
+        }
+
+        public void RunCyclesForPattern()
+        {
+            LoadDict.Clear();
+            int cycles = 1;
+            DoCycle();
+            long lactual = CalculateLoad();
+            LoadDict.Add(lactual, new Tuple<int, int, int, int>(1, cycles, 1, 0));
+            do
+            {
+                DoCycle();
+                cycles++;
+                lactual = CalculateLoad();
+                if (LoadDict.ContainsKey(lactual))
+                {
+                    var tp = LoadDict[lactual];
+                    if (tp.Item1 > 110)
+                    {
+                        Debug.WriteLine($"Load: {lactual}, count: {tp.Item1}, cycle: {cycles}");
+                        break;
+                    }
+                    LoadDict[lactual] = new Tuple<int, int, int, int>(tp.Item1 + 1, cycles, tp.Item3, tp.Item4 == 0 ? cycles : tp.Item4);
+                }
+                else
+                {
+                    LoadDict.Add(lactual, new Tuple<int, int, int, int>(1, cycles, cycles, 0));
+                }
+            } while (cycles < 1000000000);
+        }
+
+        public long FindCorrectLoad()
+        {
+            var listSingleKeys = LoadDict.Where(kvp => kvp.Value.Item1 == 1).Select(kvp => kvp.Key).ToList();
+            foreach (var key in listSingleKeys)
+            {
+                LoadDict.Remove(key);
+            }
+
+            int size = LoadDict.Count + LoadDict.Where(kvp => kvp.Value.Item1 > 100).Count();
+
+            foreach (var kvp in LoadDict)
+            {
+                int temp = (1000000000 - kvp.Value.Item3) % size;
+                if (temp == 0)
+                {
+                    return kvp.Key;
+                }
+            }
+            return -1;
+        }
+
+        public void SaveDishOneCycle()
+        {
+            for (int y = 0; y < MaxY; ++y)
+            {
+                for (var x = 0; x < MaxX; x++)
+                {
+                    DishOneCycle[x, y] = Dish[x, y];
+                }
+            }
+        }
+
+        public bool DishesAreEqual()
+        {
+            for (int y = 0; y < MaxY; ++y)
+            {
+                for (var x = 0; x < MaxX; x++)
+                {
+                    if (DishOneCycle[x, y] != Dish[x, y])
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        public long CalculateLoad()
+        {
             long totalLoad = 0;
             for (var y = 0; y < MaxY; y++)
             {
