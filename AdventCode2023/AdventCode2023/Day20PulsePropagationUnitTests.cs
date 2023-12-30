a22f21ad41ae04d91e8d43207c47338a41c63b1e
David Lund <david.lund@knowit.se>
2023-12-30 22:34:48 +0100

    Just template at the moment.

---
+++ AdventCode2023/AdventCode2023/Day20PulsePropagationUnitTests.cs
@@ -0,0 +1,39 @@
+﻿using AdventCode2023.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventCode2023
+{
+    public class Day20PulsePropagationUnitTests
+    {
+        [Fact]
+        public void ReadInDataFile_OK()
+        {
+            var lines = Utils.ReadLinesFromFile("Day20.txt");
+            int expected = 58;
+            int actual = lines.Length;
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ReadInDataFile_Test1_OK()
+        {
+            var lines = Utils.ReadLinesFromFile("Day20test1.txt");
+            int expected = 5;
+            int actual = lines.Length;
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ReadInDataFile_Test2_OK()
+        {
+            var lines = Utils.ReadLinesFromFile("Day20test2.txt");
+            int expected = 5;
+            int actual = lines.Length;
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ AdventCode2023/AdventCode2023/Day25SnowverloadUnitTests.cs
@@ -0,0 +1,23 @@
+﻿namespace AdventCode2023
+{
+    public class Day25SnowverloadUnitTests
+    {
+        [Fact]
+        public void ReadInDataFile_OK()
+        {
+            var lines = Utils.ReadLinesFromFile("Day25.txt");
+            int expected = 1247;
+            int actual = lines.Length;
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ReadInDataFile_Test1_OK()
+        {
+            var lines = Utils.ReadLinesFromFile("Day25test.txt");
+            int expected = 13;
+            int actual = lines.Length;
+            Assert.Equal(expected, actual);
+        }
+    }
+}
