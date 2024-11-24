03423fcd0f80ddb971eefd8cc577809db5583823
David Lund <david.lund@knowit.se>
2024-11-24 15:22:08 +0100

    Template for 2024

---
+++ AdventCode2024/AdventCode2024/Day01blahblahUnitTests.cs
@@ -0,0 +1,15 @@
+﻿using AdventCode2024.Models;
+
+namespace AdventCode2024
+{
+    public class Day01blahblahUnitTests
+    {
+        [Fact]
+        public void Day01_Step1_Something_OK()
+        {
+            var lines = Utils.ReadLinesFromFile("Day01.txt");
+            int count = 1;
+            Assert.Equal(count, lines.Length);
+        }
+    }
+}
