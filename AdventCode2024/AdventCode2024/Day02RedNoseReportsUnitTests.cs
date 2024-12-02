e13232caf4e608d16572a929774e3ada65cdfc99
David Lund <david.lund@knowit.se>
2024-12-02 08:54:03 +0100

    Day 2 step 1 Red Nose reports

---
+++ AdventCode2024/AdventCode2024/Day02RedNoseReportsUnitTests.cs
@@ -0,0 +1,48 @@
+﻿using AdventCode2024.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventCode2024;
+
+public class Day02RedNoseReportsUnitTests
+{
+    [Fact]
+    public void Day02_Step1_SafeReports_OK()
+    {
+        var lines = Utils.ReadLinesFromFile("Day02.txt");
+        int count = 1000;
+        Assert.Equal(count, lines.Length);
+
+        int safeCount = 0;
+        bool isSafe = false;
+        var sut = new ReportHandler();
+        foreach (var line in lines)
+        {
+            isSafe = sut.IsSafe(line);
+            if (isSafe)
+            {
+                safeCount++;
+            }
+        }
+        Assert.Equal(1000, safeCount);
+    }
+
+    [Theory]
+
+    [InlineData("7 6 4 2 1", true)]
+    [InlineData("1 2 7 8 9", false)]
+    [InlineData("9 7 6 2 1", false)]
+    [InlineData("1 3 2 4 5", false)]
+    [InlineData("8 6 4 4 1", false)]
+    [InlineData("1 3 6 7 9", true)]
+    public void CheckIfRowIsSafe_OK(string line, bool isSafe)
+    {
+        var sut = new ReportHandler();
+        var actual = sut.IsSafe(line);
+
+        Assert.Equal(isSafe, actual);
+    }
+}
---
+++ AdventCode2024/AdventCode2024/Models/ReportHandler.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventCode2024.Models;
+
+public class ReportHandler
+{
+    public ReportHandler()
+    {
+    }
+
+    public bool IsSafe(string row)
+    {
+        var list = ParseRow(row);
+        var differences = LevelDifferences(list);
+        bool isIncreasing = false;
+        bool isDecreasing = false;
+
+        foreach (var diff in differences)
+        {
+            if (Math.Abs(diff) > 3)
+            {
+                return false;
+            }
+            if (diff == 0)
+            {
+                return false;
+            }
+            if (1 <= diff && diff <= 3)
+            {
+                isDecreasing = true;
+            }
+            if (-3 <= diff && diff <= -1)
+            {
+                isIncreasing = true;
+            }
+        }
+
+        return isIncreasing == !isDecreasing;
+    }
+
+    public List<int> ParseRow(string row)
+    {
+        var vals = row.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        var list = vals.Select(s => int.Parse(s)).ToList();
+        return list;
+    }
+
+    public List<int> LevelDifferences(List<int> inputs)
+    {
+        var list = new List<int>();
+        for (int ii = 0; ii < inputs.Count - 1; ii++)
+        {
+            list.Add(inputs[ii] - inputs[ii + 1]);
+        }
+        return list;
+    }
+}
