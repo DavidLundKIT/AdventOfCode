e5b0d73e2d64b2d5e38c7bb138e5f5ce1b58a0ad
David Lund <david.lund@knowit.se>
2024-12-04 20:11:55 +0100

    Day 4 step 1

---
+++ AdventCode2024/AdventCode2024/Day04CeresSearchUnitTests.cs
@@ -0,0 +1,33 @@
+﻿using AdventCode2024.Models;
+
+namespace AdventCode2024;
+
+public class Day04CeresSearchUnitTests
+{
+    [Fact]
+    public void CeresSearchXmas_OK()
+    {
+        var lines = Utils.ReadLinesFromFile("Day04test.txt");
+        int count = 10;
+        Assert.Equal(count, lines.Length);
+
+        var search = new CeresSearch(lines);
+        var actual = search.FindAllXmas();
+
+        Assert.Equal(18, actual);
+    }
+
+    [Fact]
+    public void Day04_Step1_CeresSearchXmas_OK()
+    {
+        var lines = Utils.ReadLinesFromFile("Day04.txt");
+        int count = 140;
+        Assert.Equal(count, lines.Length);
+
+        var search = new CeresSearch(lines);
+        var actual = search.FindAllXmas();
+
+        Assert.Equal(2514, actual);
+    }
+
+}
---
+++ AdventCode2024/AdventCode2024/Models/CeresSearch.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventCode2024.Models;
+
+public class CeresSearch
+{
+    public Dictionary<Point, char> Puzzle { get; set; }
+    public int MaxX { get; set; }
+    public int MaxY { get; set; }
+
+    public CeresSearch(string[] lines)
+    {
+        Puzzle = new Dictionary<Point, char>();
+        MaxY = lines.Length;
+        MaxX = lines[0].Length;
+        for (int y = 0; y < MaxY; y++)
+        {
+            var chArr = lines[y].ToCharArray();
+            for (int x = 0; x < MaxX; x++)
+            {
+                Puzzle.Add(new Point(x, y), chArr[x]);
+            }
+        }
+    }
+
+    public int FindAllXmas()
+    {
+        int count = 0;
+
+        for (int y = 0; y < MaxY; y++)
+        {
+            for (int x = 0; x < MaxX; x++)
+            {
+                var ptNow = new Point(x, y);
+                if (Puzzle[ptNow] == 'X')
+                {
+                    // look for XMAS
+                    int found = FindXmasForPoint(ptNow);
+                    count += found;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    public int FindXmasForPoint(Point ptX)
+    {
+        int count = 0;
+
+        // forwards XMAS
+        count += IsXmas(new Point(ptX.X + 1, ptX.Y), new Point(ptX.X + 2, ptX.Y), new Point(ptX.X + 3, ptX.Y));
+        // down right
+        count += IsXmas(new Point(ptX.X + 1, ptX.Y + 1), new Point(ptX.X + 2, ptX.Y + 2), new Point(ptX.X + 3, ptX.Y + 3));
+        // up right
+        count += IsXmas(new Point(ptX.X + 1, ptX.Y - 1), new Point(ptX.X + 2, ptX.Y - 2), new Point(ptX.X + 3, ptX.Y - 3));
+        // down 
+        count += IsXmas(new Point(ptX.X, ptX.Y + 1), new Point(ptX.X, ptX.Y + 2), new Point(ptX.X, ptX.Y + 3));
+        // up
+        count += IsXmas(new Point(ptX.X, ptX.Y - 1), new Point(ptX.X, ptX.Y - 2), new Point(ptX.X, ptX.Y - 3));
+        // backwards
+        count += IsXmas(new Point(ptX.X - 1, ptX.Y), new Point(ptX.X - 2, ptX.Y), new Point(ptX.X - 3, ptX.Y));
+        // down left
+        count += IsXmas(new Point(ptX.X - 1, ptX.Y + 1), new Point(ptX.X - 2, ptX.Y + 2), new Point(ptX.X - 3, ptX.Y + 3));
+        // up right
+        count += IsXmas(new Point(ptX.X - 1, ptX.Y - 1), new Point(ptX.X - 2, ptX.Y - 2), new Point(ptX.X +- 3, ptX.Y - 3));
+
+        return count;
+    }
+
+    public int IsXmas(Point ptM, Point ptA, Point ptS)
+    {
+        // we start with an X so no need to check
+        if (Puzzle.ContainsKey(ptM) && Puzzle.ContainsKey(ptA) && Puzzle.ContainsKey(ptS))
+        {
+            if (Puzzle[ptM] == 'M' && Puzzle[ptA] == 'A' && Puzzle[ptS] == 'S')
+            {
+                return 1;
+            }
+        }
+        return 0;
+    }
+}
+
+public record Point(int X, int Y);
+
