1747a8bf914000237396901d91d2195202dc75e1
David Lund <david.lund@knowit.se>
2024-12-05 08:23:10 +0100

    Day5 step 1 done

---
+++ AdventCode2024/AdventCode2024/Day05PrintQueueUnitTests.cs
@@ -0,0 +1,34 @@
+﻿using AdventCode2024.Models;
+
+namespace AdventCode2024;
+
+public class Day05PrintQueueUnitTests
+{
+    [Fact]
+    public void ReadInData_OK()
+    {
+        var lines = Utils.ReadLinesFromFile("Day05test.txt");
+        Assert.Equal(28, lines.Length);
+
+        var checker = new PageQueueOrderChecker(lines);
+        Assert.Equal(21, checker.Rules.Count);
+        Assert.Equal(6, checker.Manuals.Count);
+
+        int actual = checker.CheckPageOrderOfManuals();
+        Assert.Equal(143, actual);
+    }
+
+    [Fact]
+    public void Day05_Step1_PrintQueue_OK()
+    {
+        var lines = Utils.ReadLinesFromFile("Day05.txt");
+        Assert.Equal(1375, lines.Length);
+
+        var checker = new PageQueueOrderChecker(lines);
+        Assert.Equal(1176, checker.Rules.Count);
+        Assert.Equal(198, checker.Manuals.Count);
+
+        int actual = checker.CheckPageOrderOfManuals();
+        Assert.Equal(5639, actual);
+    }
+}
---
+++ AdventCode2024/AdventCode2024/Models/PageQueueOrderChecker.cs
@@ -0,0 +1,68 @@
+﻿using System.Data;
+
+namespace AdventCode2024.Models;
+
+public class PageQueueOrderChecker
+{
+    public Dictionary<PageRule, int> Rules { get; set; }
+    public List<List<int>> Manuals { get; set; }
+    public PageQueueOrderChecker(string[] lines)
+    {
+        Rules = new Dictionary<PageRule, int>();
+        Manuals = new List<List<int>>();
+
+        foreach (var line in lines)
+        {
+            ProcessLine(line);
+        }
+    }
+
+    public void ProcessLine(string line)
+    {
+        if (string.IsNullOrWhiteSpace(line))
+            return;
+        if (line.Contains("|"))
+        {
+            var pages = line.Split('|');
+            Rules.Add(new PageRule(int.Parse(pages[0]), int.Parse(pages[1])), 0);
+        }
+        else
+        {
+            var manual = line.Split(",", StringSplitOptions.RemoveEmptyEntries).Select(s => int.Parse(s)).ToList();
+            Manuals.Add(manual);
+        }
+    }
+
+    public int CheckPageOrderOfManuals()
+    {
+        int sum = 0;
+
+        foreach (var manual in Manuals)
+        {
+            if (IsManualOrderedCorrectly(manual))
+            {
+                int middle = manual[manual.Count / 2];
+                sum += middle;
+            }
+        }
+        return sum;
+    }
+
+    public bool IsManualOrderedCorrectly(List<int> manual)
+    {
+        for (int i = 0; i < manual.Count - 1; i++)
+        {
+            for (int j = i + 1; j < manual.Count; j++)
+            {
+                var pageRule = new PageRule(manual[i], manual[j]);
+                if (!Rules.ContainsKey(pageRule))
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
+
+public record PageRule(int a, int b);
