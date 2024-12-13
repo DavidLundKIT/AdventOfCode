73f843d33e49e33d1161e8f52b774ae5d4a8f1a8
David Lund <david.lund@knowit.se>
2024-12-13 06:31:52 +0100

    So far Day 11 not ending yet

--- AdventCode2024/AdventCode2024/Day11PlutonianPebblesUnitTests.cs
+++ AdventCode2024/AdventCode2024/Day11PlutonianPebblesUnitTests.cs
@@ -1,4 +1,5 @@
 ﻿using AdventCode2024.Models;
+using System.Diagnostics;
 
 namespace AdventCode2024;
 
@@ -57,6 +58,7 @@ public class Day11PlutonianPebblesUnitTests
 
         Assert.Equal(55312, ppr.Stones.Count);
     }
+
     [Fact]
     public void Day11_Part1_PlutonianPebbles_OK()
     {
@@ -70,5 +72,86 @@ public class Day11PlutonianPebblesUnitTests
         }
 
         Assert.Equal(199753, ppr.Stones.Count);
+    }
+
+    [Fact(Skip = "Ran forever")]
+    public void Day11_Part2_PlutonianPebbles_OK()
+    {
+        var ppr = new PlutonianPebbleRules(Data);
+        Assert.NotNull(ppr);
+        Assert.Equal(8, ppr.Stones.Count);
+
+        for (int i = 0; i < 75; i++)
+        {
+            ppr.DoBlink();
+        }
+
+        Assert.Equal(199753, ppr.Stones.Count);
+    }
+
+    [Fact]
+    public void Day11_Part2_OnlyOne_PlutonianPebbles_OK()
+    {
+        var oneAtATime = Data.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+        long actual = 0;
+        foreach (var one in oneAtATime)
+        {
+            Debug.WriteLine($"For one: {one}");
+            var ppr = new PlutonianPebbleRules(one);
+
+            for (int i = 0; i < 25; i++)
+            {
+                ppr.DoBlink();
+            }
+            int count = ppr.Stones.Count;
+            Debug.WriteLine($"For one: {one}, count is: {count}");
+            actual += ppr.Stones.Count;
+        }
+
+        Assert.Equal(199753, actual);
+    }
+
+    [Theory]
+    [InlineData(1, 3)]
+    [InlineData(2, 4)]
+    [InlineData(3, 5)]
+    [InlineData(4, 9)]
+    [InlineData(5, 13)]
+    [InlineData(6, 22)]
+    [InlineData(25, 55312)]
+    public void PlutonianPebblesRecursive_TestData2_Blink25_OK(int blinks, long expected)
+    {
+        var ppr = new PlutonianPebblesRecursive(TestData2);
+        Assert.NotNull(ppr);
+        Assert.Equal(2, ppr.StartValues.Count);
+
+        long actual = ppr.DoBlinks(blinks);
+
+       Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Day11_Part1_PlutonianPebblesRecursive_OK()
+    {
+        var ppr = new PlutonianPebblesRecursive(Data);
+        Assert.NotNull(ppr);
+        Assert.Equal(8, ppr.StartValues.Count);
+
+        long actual = ppr.DoBlinks(25);
+
+        Assert.Equal(199753, actual);
+    }
+
+    [Fact]
+    public void Day11_Part2_PlutonianPebblesRecursive_OK()
+    {
+        var ppr = new PlutonianPebblesRecursive(Data);
+        Assert.NotNull(ppr);
+        Assert.Equal(8, ppr.StartValues.Count);
+
+        long actual = ppr.DoBlinks(75);
+
+        Assert.Equal(0, actual);
     }
 }
---
+++ AdventCode2024/AdventCode2024/Models/PlutonianPebblesRecursive.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventCode2024.Models;
+
+public class PlutonianPebblesRecursive
+{
+    public List<long> StartValues { get; set; }
+
+    public PlutonianPebblesRecursive(string line)
+    {
+        StartValues = line.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(s => long.Parse(s)).ToList();
+    }
+
+    public long DoBlinks(int times)
+    {
+        long total = 0;
+
+        foreach (var sv in StartValues)
+        {
+            long result = Blink(sv, times);
+            total += result;
+        }
+
+        return total;
+    }
+
+    public long Blink(long currentValue, int times)
+    {
+        long result = 0;
+
+        if (times == 0)
+        {
+            return 1;
+        }
+
+        string sval = currentValue.ToString();
+        if (currentValue == 0)
+        {
+            currentValue = 1;
+            result = Blink(currentValue, times-1);
+        }
+        else if (sval.Length % 2 == 0)
+        {
+            string sval1 = sval.Substring(0, sval.Length / 2);
+            string sval2 = sval.Substring(sval.Length / 2).TrimStart('0');
+            sval2 = string.IsNullOrWhiteSpace(sval2) ? "0" : sval2;
+            result = Blink(long.Parse(sval2), times - 1);
+            result += Blink(long.Parse(sval1), times - 1);
+        }
+        else
+        {
+            currentValue = currentValue * 2024;
+            result = Blink(currentValue, times - 1);
+        }
+        return result;
+    }
+}
