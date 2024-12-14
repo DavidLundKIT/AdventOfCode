a7ae6fa52b273059ad953c0b963a03023a212910
David Lund <david.lund@knowit.se>
2024-12-14 15:59:23 +0100

    Day 14 both parts

---
+++ AdventCode2024/AdventCode2024/Day14RestroomRedoubtUnitTests.cs
@@ -0,0 +1,123 @@
+﻿using AdventCode2024.Models;
+
+namespace AdventCode2024;
+
+public class Day14RestroomRedoubtUnitTests
+{
+
+    [Fact]
+    public void TestData_GetStartValues_OK()
+    {
+        var lines = Utils.ReadLinesFromFile("Day14test.txt");
+        Assert.NotEmpty(lines);
+        Assert.Equal(12, lines.Count());
+
+        var roboTracker = new RobotTracker(lines, 11, 7);
+        Assert.NotNull(roboTracker);
+        Assert.NotEmpty(roboTracker.StartValues);
+        Assert.Equal(12, roboTracker.StartValues.Count());
+
+        roboTracker.MoveAllRobotsFor(100);
+        Assert.NotEmpty(roboTracker.Positions);
+
+        int actual = roboTracker.Quadrant1Count();
+        int total = actual;
+        Assert.Equal(1, actual);
+
+        actual = roboTracker.Quadrant2Count();
+        total *= actual;
+        Assert.Equal(3, actual);
+        Assert.Equal(3, total);
+
+        actual = roboTracker.Quadrant3Count();
+        total *= actual;
+        Assert.Equal(4, actual);
+        Assert.Equal(12, total);
+
+        actual = roboTracker.Quadrant4Count();
+        total *= actual;
+        Assert.Equal(1, actual);
+        Assert.Equal(12, total);
+
+        total = roboTracker.SafetyFactor();
+        Assert.Equal(12, total);
+    }
+
+    [Theory]
+    [InlineData(1, 4, 1)]
+    [InlineData(2, 6, 5)]
+    [InlineData(3, 8, 2)]
+    [InlineData(4, 10, 6)]
+    [InlineData(5, 1, 3)]
+    public void TestData_CheckMovement_OK(int seconds, int expectedX, int expectedY)
+    {
+        List<string> lines = new List<string>();
+        lines.Add("p=2,4 v=2,-3");
+
+        var roboTracker = new RobotTracker(lines.ToArray(), 11, 7);
+        Assert.NotNull(roboTracker);
+        Assert.NotEmpty(roboTracker.StartValues);
+        Assert.Single(roboTracker.StartValues);
+
+        var expectedPt = new Point(expectedX, expectedY);
+        var actualPt = roboTracker.MoveRobotFor(seconds, roboTracker.StartValues[0]);
+        Assert.Equal(expectedPt, actualPt);
+    }
+
+    [Fact]
+    public void Day14_Part1_RestroomRedoubt_OK()
+    {
+        var lines = Utils.ReadLinesFromFile("Day14.txt");
+        Assert.NotEmpty(lines);
+        Assert.Equal(500, lines.Count());
+
+        var roboTracker = new RobotTracker(lines, 101, 103);
+        Assert.NotNull(roboTracker);
+        Assert.NotEmpty(roboTracker.StartValues);
+        Assert.Equal(500, roboTracker.StartValues.Count());
+
+        roboTracker.MoveAllRobotsFor(100);
+        int actual = roboTracker.SafetyFactor();
+        Assert.Equal(230435667, actual);
+    }
+
+    /// <summary>
+    /// First did this for every second in bunches of 1000
+    /// Then thought I saw a pattern and printed the pattern 
+    /// starting 1220 every 103 seconds.
+    /// Until I found it. I used VS Code to peruse the files.
+    /// The "view" scrollbar on the right was great.
+    /// How anyone solved this quickly is beyond me.
+    /// </summary>
+    [Fact(Skip = "Makes file in c:\\Temp directory")]
+    public void Day14_Part2_RestroomRedoubt_Looking_OK()
+    {
+        var lines = Utils.ReadLinesFromFile("Day14.txt");
+        Assert.NotEmpty(lines);
+        Assert.Equal(500, lines.Count());
+
+        var roboTracker = new RobotTracker(lines, 101, 103);
+
+        int secs = 1220;
+        do
+        {
+            roboTracker.MoveAllRobotsFor(secs);
+            roboTracker.PlotRobotPositions(secs);
+            secs += 103;
+        } while (secs < 300000);
+    }
+
+    [Fact]
+    public void Day14_Part2_RestroomRedoubt_Found_OK()
+    {
+        var lines = Utils.ReadLinesFromFile("Day14.txt");
+        Assert.NotEmpty(lines);
+        Assert.Equal(500, lines.Count());
+
+        var roboTracker = new RobotTracker(lines, 101, 103);
+
+        int secs = 7709;
+        roboTracker.MoveAllRobotsFor(secs);
+        roboTracker.PlotRobotPositionWindow(secs);
+    }
+}
---
+++ AdventCode2024/AdventCode2024/Models/RobotTracker.cs
@@ -0,0 +1,162 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+namespace AdventCode2024.Models;
+
+public record RobotStartValues(Point Position, Point Velocity);
+
+/// <summary>
+/// Day 14 RestRoomredoubt
+/// </summary>
+public class RobotTracker
+{
+    public List<RobotStartValues> StartValues { get; set; }
+    public Dictionary<Point, int> Positions { get; set; }
+    public int MaxX { get; set; }
+    public int MaxY { get; set; }
+
+    public RobotTracker(string[] lines, int maxX, int maxY)
+    {
+        StartValues = new List<RobotStartValues>();
+        Positions = new Dictionary<Point, int>();
+        MaxX = maxX;
+        MaxY = maxY;
+
+        foreach (string line in lines)
+        {
+            StartValues.Add(ParseRobotStartValues(line));
+        }
+    }
+
+    public RobotStartValues ParseRobotStartValues(string line)
+    {
+        var vals = line.Split(new string[] { "p=", "v=", ",", " " }, StringSplitOptions.RemoveEmptyEntries);
+        var startValues = new RobotStartValues(new Point(int.Parse(vals[0]), int.Parse(vals[1])), new Point(int.Parse(vals[2]), int.Parse(vals[3])));
+        return startValues;
+    }
+
+    public Point MoveRobotFor(int secs, RobotStartValues rsv)
+    {
+        int x = ((rsv.Position.X + (secs * rsv.Velocity.X)) % MaxX);
+        x = x < 0 ? MaxX + x : x;
+        int y = ((rsv.Position.Y + (secs * rsv.Velocity.Y)) % MaxY);
+        y = y < 0 ? MaxY + y : y;
+
+        return new Point(x, y);
+    }
+
+    public void MoveAllRobotsFor(int secs)
+    {
+        Positions.Clear();
+        foreach (var rsv in StartValues)
+        {
+            var pos = MoveRobotFor(secs, rsv);
+            if (Positions.ContainsKey(pos))
+            {
+                Positions[pos]++;
+            }
+            else
+            {
+                Positions.Add(pos, 1);
+            }
+        }
+    }
+
+    public int Quadrant1Count()
+    {
+        int midX = MaxX / 2;
+        int midY = MaxY / 2;
+
+        var positionsInQuadrant = Positions.Where(pv => pv.Key.X >= 0 && pv.Key.X < midX && pv.Key.Y >= 0 && pv.Key.Y < midY).ToList();
+        int count = positionsInQuadrant.Select(kv => kv.Value).Sum();
+        return count;
+    }
+
+    public int Quadrant2Count()
+    {
+        int midX = MaxX / 2;
+        int midY = MaxY / 2;
+
+        var positionsInQuadrant = Positions.Where(pv => pv.Key.X > midX && pv.Key.X < MaxX && pv.Key.Y >= 0 && pv.Key.Y < midY).ToList();
+        int count = positionsInQuadrant.Select(kv => kv.Value).Sum();
+        return count;
+    }
+
+    public int Quadrant3Count()
+    {
+        int midX = MaxX / 2;
+        int midY = MaxY / 2;
+
+        var positionsInQuadrant = Positions.Where(pv => pv.Key.X >= 0 && pv.Key.X < midX && pv.Key.Y > midY && pv.Key.Y < MaxY).ToList();
+        int count = positionsInQuadrant.Select(kv => kv.Value).Sum();
+        return count;
+    }
+
+    public int Quadrant4Count()
+    {
+        int midX = MaxX / 2;
+        int midY = MaxY / 2;
+
+        var positionsInQuadrant = Positions.Where(pv => pv.Key.X > midX && pv.Key.X < MaxX && pv.Key.Y > midY && pv.Key.Y < MaxY).ToList();
+        int count = positionsInQuadrant.Select(kv => kv.Value).Sum();
+        return count;
+    }
+    public int SafetyFactor()
+    {
+        int total = Quadrant1Count();
+        total *= Quadrant2Count();
+        total *= Quadrant3Count();
+        total *= Quadrant4Count();
+        return total;
+    }
+
+    public void PlotRobotPositions(int secs)
+    {
+        int[,] screen = new int[MaxX, MaxY];
+        foreach (var pos in Positions)
+        {
+            screen[pos.Key.X, pos.Key.Y] = pos.Value;
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int y = 0; y < MaxY; y++)
+        {
+            for (int x = 0; x < MaxX; x++)
+            {
+                if (0 == screen[x, y])
+
+                    sb.Append(" ");
+                else
+                    sb.Append("#");
+            }
+            sb.AppendLine();
+        }
+        sb.AppendLine($"--- Seconds {secs}");
+        sb.AppendLine("-----");
+        File.AppendAllText(@"C:\temp\Day14Tree4.txt", sb.ToString());
+    }
+
+    public void PlotRobotPositionWindow(int secs)
+    {
+        int[,] screen = new int[MaxX, MaxY];
+        foreach (var pos in Positions)
+        {
+            screen[pos.Key.X, pos.Key.Y] = pos.Value;
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int y = 0; y < MaxY; y++)
+        {
+            for (int x = 0; x < MaxX; x++)
+            {
+                if (0 == screen[x, y])
+
+                    sb.Append(" ");
+                else
+                    sb.Append("#");
+            }
+            sb.AppendLine();
+        }
+        sb.AppendLine($"--- Seconds {secs}");
+        sb.AppendLine("-----");
+        Debug.WriteLine(sb.ToString());
+    }
+}
