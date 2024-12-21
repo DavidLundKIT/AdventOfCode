bee2c794edcc8a0a9cbde38a4c39d2047944779f
David Lund <david.lund@knowit.se>
2024-12-22 00:04:00 +0100

    Prep Day 18 no solutions

---
+++ AdventCode2024/AdventCode2024/Day18RamRunUnitTest.cs
@@ -0,0 +1,22 @@
+﻿using AdventCode2024.Models;
+
+namespace AdventCode2024;
+
+public class Day18RamRunUnitTest
+{
+    [Fact]
+    public void RamRun_TestData_OK()
+    {
+        var lines = Utils.ReadLinesFromFile("Day18test.txt");
+        Assert.NotEmpty(lines);
+        Assert.Equal(25, lines.Count());
+    }
+
+    [Fact]
+    public void Day18_Part1_RamRun_OK()
+    {
+        var lines = Utils.ReadLinesFromFile("Day18.txt");
+        Assert.NotEmpty(lines);
+        Assert.Equal(3450, lines.Count());
+    }
+}
