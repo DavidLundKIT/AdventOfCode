cc138ef95938035cbf6e3ac3745af4f211878fd6
David Lund <david.lund@knowit.se>
2025-11-30 21:58:28 +0100

    Prepared for tomorrow! Advent of Code 2025.

---
+++ AdventCode2025/AdventCode2025/Day01UnitTests.cs
@@ -0,0 +1,16 @@
+﻿using AdventCode2025.Models;
+
+namespace AdventCode2025
+{
+    public class Day01UnitTests
+    {
+        [Fact]
+        public void Day01_Part1_OK()
+        {
+            var lines = Utils.ReadLinesFromFile("Day01.txt");
+            Assert.NotNull(lines);
+            Assert.NotEmpty(lines);
+            Assert.Equal("Hello world!", lines[0]);
+        }
+    }
+}
