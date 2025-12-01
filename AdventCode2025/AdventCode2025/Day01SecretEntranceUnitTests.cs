80f9c017b3c1c55743f535d1d157420b165afe49
David Lund <david.lund@knowit.se>
2025-12-01 07:08:46 +0100

    Day 1 Secret Entrance part 1 OK

---
+++ AdventCode2025/AdventCode2025/Day01SecretEntranceUnitTests.cs
@@ -0,0 +1,35 @@
+﻿using AdventCode2025.Models;
+
+namespace AdventCode2025;
+
+public class Day01SecretEntranceUnitTests
+{
+    [Fact]
+    public void SecretEntrance_TestData_OK()
+    {             
+        //var secretEntrance = new Day01SecretEntrance();
+        var lines = Utils.ReadLinesFromFile("Day01test.txt");
+        Assert.NotNull(lines);
+        Assert.NotEmpty(lines);
+        Assert.Equal(10, lines.Length);
+        Assert.Equal("L68", lines[0]);
+        var sut = new SecretEntranceMapper(lines);
+        var actual = sut.TimesMovedToZero(50);
+
+        Assert.Equal(3, actual);
+    }
+
+    [Fact]
+    public void Day01_Part1_OK()
+    {
+        var lines = Utils.ReadLinesFromFile("Day01.txt");
+        Assert.NotNull(lines);
+        Assert.NotEmpty(lines);
+        Assert.Equal(4779, lines.Length);
+        Assert.Equal("L50", lines[0]);
+        var sut = new SecretEntranceMapper(lines);
+        var actual = sut.TimesMovedToZero(50);
+
+        Assert.Equal(3, actual);
+    }
+}
---
+++ AdventCode2025/AdventCode2025/Models/SecretEntranceMapper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventCode2025.Models;
+
+public class SecretEntranceMapper
+{
+    public List<string> Instructions { get; set; }
+    
+    public SecretEntranceMapper(string[] lines)
+	{
+        Instructions = new List<string>(lines);
+    }
+
+    public int TimesMovedToZero(int startPos)
+    {
+        int zeroed = 0;
+
+        foreach (var instruction in Instructions)
+        {
+            var operation = instruction[0];
+
+            bool isLeft = operation == 'L';
+            var value = int.Parse(instruction[1..]);
+            if (isLeft)
+            {
+                startPos -= value;
+            }
+            else
+            {
+                startPos += value;
+            }
+            // wrap values
+            startPos = startPos % 100;
+            if (startPos < 0)
+            {
+                startPos += 100;
+            }
+
+            if (startPos == 0)
+            {
+                zeroed++;
+            }
+        }
+        return zeroed;
+    }
+}
