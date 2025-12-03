9fbb5aa406460e946182041654f9d0790024fd30
David Lund <david.lund@knowit.se>
2025-12-03 23:47:10 +0100

    Not done yet Day03 part 2

--- AdventCode2025/AdventCode2025/Day03LobbyUnitTests.cs
+++ AdventCode2025/AdventCode2025/Day03LobbyUnitTests.cs
@@ -48,4 +48,46 @@ public class Day03LobbyUnitTests
         }
         Assert.Equal(17694, sumJoltage);
     }
+
+    [Theory]
+    [InlineData("987654321111111", 98)]
+    [InlineData("811111111111119", 89)]
+    [InlineData("234234234234278", 78)]
+    [InlineData("818181911112111", 92)]
+    public void TestJoltageCalculator_Top12_OK(string battery, int joltage)
+    {
+        var calculator = new JoltageCalculator();
+        var calculatedJoltage = calculator.CalculateJoltage(battery);
+        Assert.Equal(joltage, calculatedJoltage);
+    }
+
+    [Fact]
+    public void TestJoltageCalculator_Top12_Sum_OK()
+    {
+        var lines = Utils.ReadLinesFromFile("Day03test.txt");
+        Assert.Equal(4, lines.Length);
+
+        var calculator = new JoltageCalculator();
+        int sumJoltage = 0;
+        foreach (var line in lines)
+        {
+            sumJoltage += calculator.CalculateJoltage(line);
+        }
+        Assert.Equal(357, sumJoltage);
+    }
+
+    [Fact]
+    public void Day03_Part2_Solution_OK()
+    {
+        var lines = Utils.ReadLinesFromFile("Day03.txt");
+        Assert.Equal(200, lines.Length);
+
+        var calculator = new JoltageCalculator();
+        int sumJoltage = 0;
+        foreach (var line in lines)
+        {
+            sumJoltage += calculator.CalculateJoltage(line);
+        }
+        Assert.Equal(17694, sumJoltage);
+    }
 }
--- AdventCode2025/AdventCode2025/Models/JoltageCalculator.cs
+++ AdventCode2025/AdventCode2025/Models/JoltageCalculator.cs
@@ -33,4 +33,35 @@ public  class JoltageCalculator
         }
         return joltage;
     }
+
+    /// <summary>
+    /// Remember the order of the 12 must be preserved
+    /// </summary>
+    /// <param name="battery"></param>
+    /// <returns></returns>
+    public long CalculateTop12Joltage(string battery)
+    {
+        long joltage = 0;
+        // TODO David must make this work
+        var batteryJoltage = battery.ToCharArray();
+        var digitChars = (new String(' ', batteryJoltage.Length)).ToCharArray();
+
+        // find highest digit with at least 11 other after it
+        int firstHighest = 0;
+        for (int i = 0; i < batteryJoltage.Length - 1; i++)
+        {
+            int iJolt = int.Parse(batteryJoltage[i].ToString()) * 10;
+            for (int j = i + 1; j < batteryJoltage.Length; j++)
+            {
+                int jJolt = int.Parse(batteryJoltage[j].ToString());
+
+                int testJolt = jJolt + iJolt;
+                if (testJolt > joltage)
+                {
+                    joltage = testJolt;
+                }
+            }
+        }
+        return joltage;
+    }
 }
