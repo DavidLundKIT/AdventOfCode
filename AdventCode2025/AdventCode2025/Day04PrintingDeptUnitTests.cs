4862429830ccd0053f02bfe6a75d960dac8eae80
David Lund <david.lund@knowit.se>
2025-12-04 19:37:38 +0100

    Day 4 Part 1 and 2 Done!

---
+++ AdventCode2025/AdventCode2025/Day04PrintingDeptUnitTests.cs
@@ -0,0 +1,56 @@
+﻿using AdventCode2025.Models;
+
+namespace AdventCode2025;
+
+public class Day04PrintingDeptUnitTests
+{
+    [Fact]
+    public void Day04_TestData_Find13_OK()
+    {
+        var lines = Utils.ReadLinesFromFile("Day04test.txt");
+        Assert.Equal(10, lines.Length);
+
+        var sut = new PrinterDeptFloorMapper(lines);
+
+        //sut.PrintWarehouse();
+        var tiles = sut.CountAccessiblePaperRolls();
+        Assert.Equal(13, tiles);
+    }
+
+    [Fact]
+    public void Day04_Part1_Solution_OK()
+    {
+        var lines = Utils.ReadLinesFromFile("Day04.txt");
+        Assert.Equal(136, lines.Length);
+        var sut = new PrinterDeptFloorMapper(lines);
+
+        //sut.PrintWarehouse();
+        var tiles = sut.CountAccessiblePaperRolls();
+        Assert.Equal(1397, tiles);
+    }
+
+    [Fact]
+    public void Day04_TestData_RemoveRolls_OK()
+    {
+        var lines = Utils.ReadLinesFromFile("Day04test.txt");
+        Assert.Equal(10, lines.Length);
+
+        var sut = new PrinterDeptFloorMapper(lines);
+
+        //sut.PrintWarehouse();
+        var tiles = sut.RemoveWarehousePaperRolls();
+        Assert.Equal(43, tiles);
+    }
+
+    [Fact]
+    public void Day04_Part2_Solution_OK()
+    {
+        var lines = Utils.ReadLinesFromFile("Day04.txt");
+        Assert.Equal(136, lines.Length);
+        var sut = new PrinterDeptFloorMapper(lines);
+
+        //sut.PrintWarehouse();
+        var tiles = sut.RemoveWarehousePaperRolls();
+        Assert.Equal(8758, tiles);
+    }
+}
---
+++ AdventCode2025/AdventCode2025/Models/PrinterDeptFloorMapper.cs
@@ -0,0 +1,104 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+namespace AdventCode2025.Models;
+
+public class PrinterDeptFloorMapper
+{
+    public Dictionary<Tile, char> WarehouseDict { get; set; }
+    public PrinterDeptFloorMapper(string[] lines)
+    {
+        WarehouseDict = new Dictionary<Tile, char>();
+
+        for (int y = 0; y < lines.Length; y++)
+        {
+            var line = lines[y];
+            for (int x = 0; x < line.Length; x++)
+            {
+                if (line[x] == '@')
+                    WarehouseDict.Add(new Tile(x, y), line[x]);
+            }
+        }
+    }
+
+    public void PrintWarehouse()
+    {
+        Debug.WriteLine("--- Done ---");
+        int maxX = WarehouseDict.Keys.Select(k => k.X).Max();
+        int maxY = WarehouseDict.Keys.Select(k => k.Y).Max();
+
+        for (int y = 0; y <= maxY; y++)
+        {
+            StringBuilder lineBuilder = new StringBuilder();
+            for (int x = 0; x <= maxX; x++)
+            {
+                var tile = new Tile(x, y);
+                if (WarehouseDict.ContainsKey(tile))
+                {
+                    lineBuilder.Append(WarehouseDict[tile]);
+                }
+                else
+                {
+                    lineBuilder.Append('.');
+                }
+            }
+            Debug.WriteLine(lineBuilder.ToString());
+        }
+        Debug.WriteLine("--- Done ---");
+    }
+
+    public int CountAccessiblePaperRolls()
+    {
+        int freeRolls = 0;
+        foreach (var tile in WarehouseDict.Keys)
+        {
+            int isBlockedSpace = 0;
+            for (int x = -1; x <= 1; x++)
+            {
+                for (int y = -1; y <= 1; y++)
+                {
+                    if (x == 0 && y == 0)
+                        continue;
+                    var adjacentTile = new Tile(tile.X + x, tile.Y + y);
+                    if (WarehouseDict.ContainsKey(adjacentTile))
+                    {
+                        isBlockedSpace++;
+                    }
+                }
+            }
+            if (isBlockedSpace <= 3)
+            {
+                freeRolls++;
+                WarehouseDict[tile] = 'x';
+            }
+        }
+
+        return freeRolls;
+    }
+
+    public int RemoveWarehousePaperRolls()
+    {
+        int totalRolls = WarehouseDict.Count;
+
+        int totalMovedRolls = 0;
+        int movedRolls = 0;
+        do
+        {
+            movedRolls = CountAccessiblePaperRolls();
+            totalMovedRolls += movedRolls;
+            if (movedRolls > 0)
+            {
+                var tilesToRemove = WarehouseDict.Where(li => li.Value == 'x').Select(li => li.Key);
+                foreach (var tile in tilesToRemove)
+                {
+                    WarehouseDict.Remove(tile);
+                }
+            }
+        } while (movedRolls > 0);
+
+        var temp = totalRolls - WarehouseDict.Count;
+        return totalMovedRolls;
+    }
+}
+
+public record Tile(int X, int Y);
