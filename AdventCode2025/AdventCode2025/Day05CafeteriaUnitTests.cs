4f156f6fadeadd16c9d521abe9b9ebb8fbf53d00
David Lund <david.lund@knowit.se>
2025-12-06 09:18:47 +0100

    Day 5 part 1 ok and part 2 not yet.

---
+++ AdventCode2025/AdventCode2025/Day05CafeteriaUnitTests.cs
@@ -0,0 +1,62 @@
+﻿using AdventCode2025.Models;
+
+namespace AdventCode2025;
+
+public class Day05CafeteriaUnitTests
+{
+    [Fact]
+    public void Day05_TestData_Find13_OK()
+    {
+        var lines = Utils.ReadLinesFromFile("Day05test.txt");
+        Assert.Equal(11, lines.Length);
+
+        var sut = new FoodChecker(lines);
+        Assert.Equal(6, sut.Ingredients.Count);
+        Assert.Equal(4, sut.IngredientRanges.Count);
+
+        var freshCount = sut.FindFreshIngredients();
+        Assert.Equal(3, freshCount);
+    }
+
+    [Fact]
+    public void Day05_Part1_Solution_OK()
+    {
+        var lines = Utils.ReadLinesFromFile("Day05.txt");
+        Assert.Equal(1178, lines.Length);
+
+        var sut = new FoodChecker(lines);
+        Assert.Equal(167, sut.IngredientRanges.Count);
+        Assert.Equal(1000, sut.Ingredients.Count);
+
+        var freshCount = sut.FindFreshIngredients();
+        Assert.Equal(707, freshCount);
+    }
+
+    [Fact]
+    public void Day05_TestData_FindValidIngedients_OK()
+    {
+        var lines = Utils.ReadLinesFromFile("Day05test.txt");
+        Assert.Equal(11, lines.Length);
+
+        var sut = new FoodChecker(lines);
+        Assert.Equal(6, sut.Ingredients.Count);
+        Assert.Equal(4, sut.IngredientRanges.Count);
+
+        var freshCount = sut.FindValidIngredients();
+        Assert.Equal(14, freshCount);
+    }
+
+    [Fact]
+    public void Day05_Part2_Solution_OK()
+    {
+        var lines = Utils.ReadLinesFromFile("Day05.txt");
+        Assert.Equal(1178, lines.Length);
+
+        var sut = new FoodChecker(lines);
+        Assert.Equal(167, sut.IngredientRanges.Count);
+        Assert.Equal(1000, sut.Ingredients.Count);
+
+        var freshCount = sut.FindValidIngredients();
+        Assert.Equal(0, freshCount);
+    }
+}
---
+++ AdventCode2025/AdventCode2025/Models/FoodChecker.cs
@@ -0,0 +1,96 @@
+﻿namespace AdventCode2025.Models;
+
+public class FoodChecker
+{
+    public Dictionary<IngredientRange, long> IngredientRanges { get; set; }
+    public SortedList<long, long> Ingredients { get; set; }
+
+    public FoodChecker(string[] lines)
+    {
+        bool inRangesSection = true;
+        IngredientRanges = new Dictionary<IngredientRange, long>();
+        Ingredients = new SortedList<long, long>();
+
+        foreach (var line in lines)
+        {
+            if (string.IsNullOrWhiteSpace(line))
+            {
+                inRangesSection = false;
+                continue;
+            }
+            if (inRangesSection)
+            {
+                // Parse ingredient range
+                var parts = line.Split('-');
+                var startId = long.Parse(parts[0]);
+                var endId = long.Parse(parts[1]);
+                if (endId < startId)
+                {
+                    // Wrap around case
+                    var temp = startId;
+                    startId = endId;
+                    endId = temp;
+                }
+                var foodRange = new IngredientRange(startId, endId);
+                if (IngredientRanges.ContainsKey(foodRange))
+                {
+                    IngredientRanges[foodRange]++;
+                }
+                else
+                {
+                    IngredientRanges.Add(new IngredientRange(startId, endId), 0);
+                }
+            }
+            else
+            {
+                var foodId = long.Parse(line);
+                Ingredients.Add(foodId, 1);
+            }
+        }
+    }
+
+    public int FindFreshIngredients()
+    {
+        var FreshIngredients = new Dictionary<long, int>();
+
+        foreach (var range in IngredientRanges.Keys)
+        {
+            var freshFoodsInRange = Ingredients.Where(ingredient => ingredient.Key >= range.StartFoodId && ingredient.Key <= range.EndFoodId).Select(ingredient => ingredient.Key)
+                .ToList();
+            foreach (var foodId in freshFoodsInRange)
+            {
+                if (FreshIngredients.ContainsKey(foodId))
+                {
+                    FreshIngredients[foodId]++;
+                }
+                else
+                {
+                    FreshIngredients[foodId] = 1;
+                }
+            }
+        }
+        return FreshIngredients.Count;
+    }
+
+    public long FindValidIngredients()
+    {
+        Dictionary<long, long> ValidIngredients = new Dictionary<long, long>();
+        foreach (var range in IngredientRanges.Keys)
+        {
+            for (long foodId = range.StartFoodId; foodId <= range.EndFoodId; foodId++)
+            {
+                if (ValidIngredients.ContainsKey(foodId))
+                {
+                    ValidIngredients[foodId]++;
+                }
+                else
+                {
+                    ValidIngredients[foodId] = 1;
+                }
+            }
+        }
+        return ValidIngredients.Count;
+    }
+}
+
+public record IngredientRange(long StartFoodId, long EndFoodId);
