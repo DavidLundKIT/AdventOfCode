1effa3e17d25946ad5870eac4bb9e67f28892be0
David Lund <david.lund@knowit.se>
2025-12-07 12:42:22 +0100

    Day 7 part 1 okay but part 2 too low

---
+++ AdventCode2025/AdventCode2025/Day07LaboratoriesUnitTests.cs
@@ -0,0 +1,50 @@
+﻿using AdventCode2025.Models;
+
+namespace AdventCode2025;
+
+public class Day07LaboratoriesUnitTests
+{
+    [Fact]
+    public void TachyonTracer_TestSplits_OK()
+    {
+        var lines = Utils.ReadLinesFromFile("Day07test.txt");
+        Assert.Equal(16, lines.Length);
+
+        var tracer = new TachyonTracer(lines);
+        var actual = tracer.TotalSplits();
+        Assert.Equal(21, actual);
+    }
+
+    [Fact]
+    public void Day07_Part1_Solution_OK()
+    {
+        var lines = Utils.ReadLinesFromFile("Day07.txt");
+        Assert.Equal(142, lines.Length);
+
+        var tracer = new TachyonTracer(lines);
+        var actual = tracer.TotalSplits();
+        Assert.Equal(1537, actual);
+    }
+
+    [Fact]
+    public void TachyonTracer_TestTimelines_OK()
+    {
+        var lines = Utils.ReadLinesFromFile("Day07test.txt");
+        Assert.Equal(16, lines.Length);
+
+        var tracer = new TachyonTracer(lines);
+        var actual = tracer.TotalTimelines();
+        Assert.Equal(40, actual);
+    }
+
+    [Fact]
+    public void Day07_Part2_Solution_OK()
+    {
+        var lines = Utils.ReadLinesFromFile("Day07.txt");
+        Assert.Equal(142, lines.Length);
+
+        var tracer = new TachyonTracer(lines);
+        var actual = tracer.TotalTimelines();
+        Assert.Equal(0, actual);
+    }
+}
---
+++ AdventCode2025/AdventCode2025/Models/TachyonTracer.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventCode2025.Models;
+
+public class TachyonTracer
+{
+    public List<List<char>> Manifold { get; set; }
+
+    public TachyonTracer(string[] lines)
+    {
+        Manifold = new List<List<char>>();
+
+        foreach (var line in lines)
+        {
+            Manifold.Add(line.ToCharArray().ToList());
+        }
+    }
+
+    public long TotalSplits()
+    {
+        long totalSplits = 0;
+        for (int rowIdx = 0; rowIdx < Manifold.Count; rowIdx++)
+        {
+            var row = Manifold[rowIdx];
+            var prevIdx = rowIdx - 1;
+            if (prevIdx < 0)
+                continue;
+
+            var prevRow = Manifold[prevIdx];
+
+            for (int colIdx = 0; colIdx < row.Count; colIdx++)
+            {
+                var prevCh = prevRow[colIdx];
+
+                switch (prevCh)
+                {
+                    case 'S':
+                        row[colIdx] = '|';
+                        break;
+                    case '|':
+                        if (row[colIdx] == '^')
+                        {
+                            // beam splitter
+                            totalSplits++;
+                            if (colIdx - 1 >= 0)
+                                row[colIdx - 1] = '|';
+                            if (colIdx + 1 < row.Count)
+                                row[colIdx + 1] = '|';
+                        }
+                        else
+                            row[colIdx] = '|';
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+        return totalSplits;
+    }
+
+    public long TotalTimelines()
+    {
+        long totalTimelines = 0;
+        for (int rowIdx = 0; rowIdx < Manifold.Count; rowIdx++)
+        {
+            var row = Manifold[rowIdx];
+            var prevIdx = rowIdx - 1;
+            if (prevIdx < 0)
+                continue;
+
+            var prevRow = Manifold[prevIdx];
+            bool splits = false;
+            for (int colIdx = 0; colIdx < row.Count; colIdx++)
+            {
+                var prevCh = prevRow[colIdx];
+
+                switch (prevCh)
+                {
+                    case 'S':
+                        row[colIdx] = '|';
+                        break;
+                    case '|':
+                        if (row[colIdx] == '^')
+                        {
+                            // beam splitter
+                            splits = true;
+                            if (colIdx - 1 >= 0)
+                                row[colIdx - 1] = '|';
+                            if (colIdx + 1 < row.Count)
+                                row[colIdx + 1] = '|';
+                        }
+                        else
+                            row[colIdx] = '|';
+                        break;
+                    default:
+                        break;
+                }
+            }
+            if (splits)
+            {
+                totalTimelines += row.Where(c => c == '|').Count();
+            }
+        }
+        return totalTimelines;
+    }
+}
