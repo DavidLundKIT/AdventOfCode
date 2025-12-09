ea6211a67a3c3e07f3f3129a9a810eac86e8b0e1
David Lund <david.lund@knowit.se>
2025-12-09 23:20:31 +0100

    No part 2 for day 9 yet.

--- AdventCode2025/AdventCode2025/Day09MovieTheaterUnitTests.cs
+++ AdventCode2025/AdventCode2025/Day09MovieTheaterUnitTests.cs
@@ -25,4 +25,28 @@ public class Day09MovieTheaterUnitTests
         var result = sut.FindLargestRectangleArea();
         Assert.Equal(4764078684, result);
     }
+
+    [Fact]
+    public void Day09_TestData_RedGreen_OK()
+    {
+        var lines = Utils.ReadLinesFromFile("Day09test.txt");
+        Assert.Equal(8, lines.Length);
+
+        var sut = new RectangleCalculator(lines);
+        //sut.PrintFloor(@"C:\temp\Day09TheaterFloorTest.txt");
+        var result = sut.FindLargestContainedRectangleArea();
+        Assert.Equal(24, result);
+    }
+
+    [Fact]
+    public void Day09_Part2_Solution_OK()
+    {
+        var lines = Utils.ReadLinesFromFile("Day09.txt");
+        Assert.Equal(496, lines.Length);
+
+        var sut = new RectangleCalculator(lines);
+        //sut.PrintFloor(@"C:\temp\Day09TheaterFloor.txt");
+        var result = sut.FindLargestContainedRectangleArea();
+        Assert.Equal(0, result);
+    }
 }
--- AdventCode2025/AdventCode2025/Models/RectangleCalculator.cs
+++ AdventCode2025/AdventCode2025/Models/RectangleCalculator.cs
@@ -1,8 +1,17 @@
-﻿namespace AdventCode2025.Models;
+﻿using System.Diagnostics;
+using System.Text;
+
+namespace AdventCode2025.Models;
 
 public class RectangleCalculator
 {
     public List<Tile> Tiles { get; set; }
+    public Dictionary<Tile, string> FloorMap { get; set; }
+
+    public int MinX { get; set; } = int.MaxValue;
+    public int MaxX { get; set; } = int.MinValue;
+    public int MinY { get; set; } = int.MaxValue;
+    public int MaxY { get; set; } = int.MinValue;
 
     public RectangleCalculator(string[] lines)
     {
@@ -11,7 +20,50 @@ public class RectangleCalculator
         foreach (var line in lines)
         {
             var parts = line.Split(',');
-            Tiles.Add(new Tile(int.Parse(parts[0]), int.Parse(parts[1])));
+            int x = int.Parse(parts[0]);
+            MaxX = Math.Max(MaxX, x);
+            MinX = Math.Min(MinX, x);
+            int y = int.Parse(parts[1]);
+            MaxY = Math.Max(MaxY, y);
+            MinY = Math.Min(MinY, y);
+            Tiles.Add(new Tile(x, y));
+        }
+
+        FloorMap = new Dictionary<Tile, string>();
+        for (int idx = 0; idx < Tiles.Count; idx++)
+        {
+            var tile0 = Tiles[idx];
+            int next = (idx + 1) % Tiles.Count;
+            var tile1 = Tiles[next];
+            FillGreenTileLines(tile0, tile1);
+        }
+    }
+
+    public void FillGreenTileLines(Tile tile0, Tile tile1)
+    {
+        FloorMap[tile0]= "#";
+        FloorMap[tile1]= "#";
+        if (tile0.X == tile1.X)
+        {
+            int startY = Math.Min(tile0.Y, tile1.Y);
+            int endY = Math.Max(tile0.Y, tile1.Y);
+            for (int dy = startY + 1; dy < endY; dy++)
+            {
+                FloorMap.Add(new Tile(tile0.X, dy), "x");
+            }
+        }
+        else if (tile0.Y == tile1.Y)
+        {
+            int startX = Math.Min(tile0.X, tile1.X);
+            int endX = Math.Max(tile0.X, tile1.X);
+            for (int dx = startX + 1; dx < endX; dx++)
+            {
+                FloorMap.Add(new Tile(dx, tile0.Y), "x");
+            }
+        }
+        else        
+        {
+            throw new ArgumentException("Tiles must be aligned either horizontally or vertically.");
         }
     }
 
@@ -34,6 +86,59 @@ public class RectangleCalculator
             }
         }
         return maxArea;
+    }
+
+    public long FindLargestContainedRectangleArea()
+    {
+        long maxArea = 0;
+        for (int i = 0; i < Tiles.Count; i++)
+        {
+            for (int j = i + 1; j < Tiles.Count; j++)
+            {
+                var tile1 = Tiles[i];
+                var tile2 = Tiles[j];
+                long width = Math.Abs(tile2.X - tile1.X) + 1;
+                long height = Math.Abs(tile2.Y - tile1.Y) + 1;
+                long area = width * height;
+                if (area > maxArea)
+                {
+                    maxArea = area;
+                }
+            }
+        }
+        return maxArea;
+    }
+
+    public void PrintFloor(string filePath)
+    {
+        using var fs = File.CreateText(filePath);
+        fs.WriteLine("--- Floor ---");
+        int minX = FloorMap.Keys.Select(k => k.X).Min();
+        int maxX = FloorMap.Keys.Select(k => k.X).Max();
+        int minY = FloorMap.Keys.Select(k => k.Y).Min();
+        int maxY = FloorMap.Keys.Select(k => k.Y).Max();
+
+        StringBuilder sb = new StringBuilder();
+        for (int y = minY; y <= maxY; y++)
+        {
+            for (int x = minX; x <= maxX; x++)
+            {
+                var tile = new Tile(x, y);
+                if (FloorMap.ContainsKey(tile))
+                {
+                    sb.Append(FloorMap[tile]);
+                }
+                else
+                {
+                    sb.Append('.');
+                }
+            }
+            fs.WriteLine(sb.ToString());
+            sb.Clear();
+        }
+        fs.WriteLine("--- Done ---");
+        fs.Flush();
+        fs.Close();
     }
 }
 
