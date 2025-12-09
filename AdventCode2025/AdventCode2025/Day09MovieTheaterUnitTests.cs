d359dcba30d7769100c0a30cae39982487d555c9
David Lund <david.lund@knowit.se>
2025-12-09 21:06:30 +0100

    Day 9 part 1 ok.

---
+++ AdventCode2025/AdventCode2025/Day09MovieTheaterUnitTests.cs
@@ -0,0 +1,28 @@
+﻿using AdventCode2025.Models;
+
+namespace AdventCode2025;
+
+public class Day09MovieTheaterUnitTests
+{
+    [Fact]
+    public void Day09_TestData_OK()
+    {
+        var lines = Utils.ReadLinesFromFile("Day09test.txt");
+        Assert.Equal(8, lines.Length);
+
+        var sut = new RectangleCalculator(lines);
+        var result = sut.FindLargestRectangleArea();
+        Assert.Equal(50, result);
+    }
+
+    [Fact]
+    public void Day09_Part1_Solution_OK()
+    {
+        var lines = Utils.ReadLinesFromFile("Day09.txt");
+        Assert.Equal(496, lines.Length);
+
+        var sut = new RectangleCalculator(lines);
+        var result = sut.FindLargestRectangleArea();
+        Assert.Equal(4764078684, result);
+    }
+}
---
+++ AdventCode2025/AdventCode2025/Models/RectangleCalculator.cs
@@ -0,0 +1,39 @@
+﻿namespace AdventCode2025.Models;
+
+public class RectangleCalculator
+{
+    public List<Tile> Tiles { get; set; }
+
+    public RectangleCalculator(string[] lines)
+    {
+        Tiles = new List<Tile>();
+
+        foreach (var line in lines)
+        {
+            var parts = line.Split(',');
+            Tiles.Add(new Tile(int.Parse(parts[0]), int.Parse(parts[1])));
+        }
+    }
+
+    public long FindLargestRectangleArea()
+    {
+        long maxArea = 0;
+        for (int i = 0; i < Tiles.Count; i++)
+        {
+            for (int j = i + 1; j < Tiles.Count; j++)
+            {
+                var tile1 = Tiles[i];
+                var tile2 = Tiles[j];
+                long width = Math.Abs(tile2.X - tile1.X) + 1;
+                long height = Math.Abs(tile2.Y - tile1.Y) + 1;
+                long area = width * height;
+                if (area > maxArea)
+                {
+                    maxArea = area;
+                }
+            }
+        }
+        return maxArea;
+    }
+}
+
