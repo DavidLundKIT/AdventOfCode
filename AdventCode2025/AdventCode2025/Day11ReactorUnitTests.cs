2afdc1f07aa1156d187bc861546aabe13be2d37a
David Lund <david.lund@knowit.se>
2025-12-11 21:07:54 +0100

    Day 11 part 1 ok but part 2 does not end

---
+++ AdventCode2025/AdventCode2025/Day11ReactorUnitTests.cs
@@ -0,0 +1,62 @@
+﻿using AdventCode2025.Models;
+
+namespace AdventCode2025;
+
+public class Day11ReactorUnitTests
+{
+    [Fact]
+    public void Day11_TestData_OK()
+    {
+        var lines = Utils.ReadLinesFromFile("Day11test.txt");
+        Assert.Equal(10, lines.Length);
+
+        var sut = new DataPathMapper(lines);
+        Assert.Equal(10, sut.DeviceOutputs.Count);
+        Assert.True(sut.DeviceOutputs.ContainsKey("you"));
+
+        var actual = sut.CountAllDataPathsFromDevice("you");
+        Assert.Equal(5, actual);
+    }
+
+    [Fact]
+    public void Day11_Part1_Solution_OK()
+    {
+        var lines = Utils.ReadLinesFromFile("Day11.txt");
+        Assert.Equal(647, lines.Length);
+
+        var sut = new DataPathMapper(lines);
+        Assert.Equal(647, sut.DeviceOutputs.Count);
+        Assert.True(sut.DeviceOutputs.ContainsKey("you"));
+
+        var actual = sut.CountAllDataPathsFromDevice("you");
+        Assert.Equal(555, actual);
+    }
+
+    [Fact]
+    public void Day11_TestData2_Svr_OK()
+    {
+        var lines = Utils.ReadLinesFromFile("Day11test2.txt");
+        Assert.Equal(13, lines.Length);
+
+        var sut = new DataPathMapper(lines);
+        Assert.Equal(13, sut.DeviceOutputs.Count);
+        Assert.True(sut.DeviceOutputs.ContainsKey("svr"));
+
+        var actual = sut.CountPathsViaDacFftFromDevice("svr", false, false);
+        Assert.Equal(2, actual);
+    }
+
+    [Fact(Skip = "Doesn't end")]
+    public void Day11_Part2_Solution_OK()
+    {
+        var lines = Utils.ReadLinesFromFile("Day11.txt");
+        Assert.Equal(647, lines.Length);
+
+        var sut = new DataPathMapper(lines);
+        Assert.Equal(647, sut.DeviceOutputs.Count);
+        Assert.True(sut.DeviceOutputs.ContainsKey("svr"));
+
+        var actual = sut.CountPathsViaDacFftFromDevice("svr", false, false);
+        Assert.Equal(5, actual);
+    }
+}
---
+++ AdventCode2025/AdventCode2025/Models/DataPathMapper.cs
@@ -0,0 +1,62 @@
+﻿namespace AdventCode2025.Models;
+
+public class DataPathMapper
+{
+    public Dictionary<string, List<string>> DeviceOutputs { get; set; }
+
+    public DataPathMapper(string[] lines)
+    {
+        DeviceOutputs = new Dictionary<string, List<string>>();
+        foreach (var line in lines)
+        {
+            var parts = line.Split(':', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+            var device = parts[0];
+            var outputs = parts[1].Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).ToList();
+            DeviceOutputs.Add(device, outputs);
+        }
+    }
+
+    public long CountAllDataPathsFromDevice(string device)
+    {
+        if (!DeviceOutputs.ContainsKey(device))
+        {
+            return 0;
+        }
+        long totalPaths = 0;
+        var outputs = DeviceOutputs[device];
+        if (outputs.Count == 1 && string.Equals(outputs[0], "out"))
+        {
+            return 1;
+        }
+        foreach (var output in outputs)
+        {
+            totalPaths += CountAllDataPathsFromDevice(output);
+        }
+        return totalPaths;
+    }
+
+    public long CountPathsViaDacFftFromDevice(string device, bool foundDac, bool foundFft)
+    {
+        if (!DeviceOutputs.ContainsKey(device))
+        {
+            return 0;
+        }
+        long totalPaths = 0;
+        var outputs = DeviceOutputs[device];
+        if (outputs.Count == 1 && string.Equals(outputs[0], "out"))
+        {
+            if (foundDac && foundFft)
+                return 1;
+            return 0;
+        }
+        if (string.Equals(device, "dac"))
+            foundDac = true;
+        if (string.Equals(device, "fft"))
+            foundFft = true;
+        foreach (var output in outputs)
+        {
+            totalPaths += CountPathsViaDacFftFromDevice(output, foundDac, foundFft);
+        }
+        return totalPaths;
+    }
+}
