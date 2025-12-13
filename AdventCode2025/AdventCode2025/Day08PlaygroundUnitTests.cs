6b77c580d1348c6df3597a83a064a953a73f74b6
David Lund <david.lund@knowit.se>
2025-12-13 19:27:24 +0100

    No progress

---
+++ AdventCode2025/AdventCode2025/Day08PlaygroundUnitTests.cs
@@ -0,0 +1,23 @@
+﻿using AdventCode2025.Models;
+
+namespace AdventCode2025;
+
+public class Day08PlaygroundUnitTests
+{
+    [Fact]
+    public void Day08_TestData_OK()
+    {
+        var lines = Utils.ReadLinesFromFile("Day08test.txt");
+        Assert.Equal(20, lines.Length);
+
+
+    }
+
+    [Fact]
+    public void Day08_Part1_Solution_OK()
+    {
+        var lines = Utils.ReadLinesFromFile("Day08.txt");
+        Assert.Equal(1000, lines.Length);
+
+    }
+}
