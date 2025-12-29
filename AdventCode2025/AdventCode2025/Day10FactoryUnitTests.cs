c2b81bde88e91d692415e4d842a352009b79c99a
David Lund <david.lund@knowit.se>
2025-12-29 11:15:53 +0100

    giving up on Part 2 Day 10

--- AdventCode2025/AdventCode2025/Day10FactoryUnitTests.cs
+++ AdventCode2025/AdventCode2025/Day10FactoryUnitTests.cs
@@ -64,6 +64,7 @@ public class Day10FactoryUnitTests
     [InlineData("[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}", 6, 10)]
     [InlineData("[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}", 5, 12)]
     [InlineData("[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}", 4, 11)]
+    //[InlineData("[##....#.##] (0,1,2,4,5,6,7,9) (0,1,2,3,4,6,7,9) (0,1,6) (0,5,8) (1,3,4,5,9) (0,1,3,8,9) (0,2,4,5) (0,1,2,5,8,9) (1,2,4,5,6,8,9) (0,2,3,5,7,8,9) (0,2,3,4,7,8,9) (1,2,3,5,6,7,8) {97,74,93,57,65,79,46,51,84,79}", 12, 100)]
     public void Day10_testdata_line_jolt_OK(string line, int buttons, int expectedSteps)
     {
         var sut = new MachineConfigurator(line);
@@ -98,7 +99,7 @@ public class Day10FactoryUnitTests
         Assert.Equal(33, total);
     }
 
-    [Fact]
+    [Fact(Skip ="Not ending after 21 hours")]
     public void Day10_Part2_Solution_OK()
     {
         var lines = Utils.ReadLinesFromFile("Day10.txt");
